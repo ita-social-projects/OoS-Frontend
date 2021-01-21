4862e02aa21944936f527346cede785cdd0a67c3
Alexandr Nabokin <sashkai@ukr.net>
2021-01-21 13:07:21 +0200

    Added components header, shell, main & routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ShellModule } from './shell/shell.module';
+import { ShellComponent } from './shell/shell.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShellComponent
+    
   ],
   imports: [
-    BrowserModule,
+  BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ShellModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
