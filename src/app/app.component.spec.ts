c1ab20ec12a8fd887d693f20bff7b3c5b043a470
LenkVskv <olenkavaskiv4215@gmail.com>
2021-01-25 14:14:30 +0200

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('out-of-school');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('span').textContent).toContain('out-of-school app is running!');
-  });
 });
