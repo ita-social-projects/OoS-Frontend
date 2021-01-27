a12ac486082fb4e08230ebbff7b0fdb5cb63ad20
Alexandr Nabokin <sashkai@ukr.net>
2021-01-27 11:58:55 +0200

    created result component

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -4,11 +4,17 @@ import { MainComponent } from './main/main.component';
 import { RouterModule } from '@angular/router';
 import { ShellRoutingModule } from './shell-routing.module';
 import { ShellComponent } from './shell.component';
+import { ResultComponent } from './result/result.component';
+
+const routes = [
+  { path: 'result', component: ResultComponent }
+]
 
 @NgModule({
   declarations: [
     MainComponent,
-    ShellComponent
+    ShellComponent,
+    ResultComponent
   ],
   imports: [
     CommonModule,
