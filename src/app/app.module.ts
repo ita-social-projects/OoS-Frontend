578c4f50488619ae2793cd8466dc572793e99c07
Oleksandra Lytvynets <alitvinets7@gmail.com>
2021-02-01 13:02:50 +0200

    conflict resolfing in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ShellModule } from './shell/shell.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
-
 import { AppState } from './shared/store/app.state';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { environment } from 'src/environments/environment';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShellModule } from './shell/shell.module';
 import { ShellComponent } from './shell/shell.component';
 import { MetaDataState } from './shared/store/meta-data.state';
 
