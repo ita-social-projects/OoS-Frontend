87db5f23e3c369dbaf14c3dca8c6bbd5420f376c
Art-mimiron <71485120+Art-mimiron@users.noreply.github.com>
2021-02-02 14:05:28 +0200

    fixed conflicts (#29)

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -23,7 +23,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'
 import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 
