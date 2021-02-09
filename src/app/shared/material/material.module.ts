ad183504644332d6d253b53ce55ef6778fcaefc3
Yura Simonyan <tushe.s.y@gmail.com>
2021-02-09 22:33:58 +0200

    Restored imports.

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
-const materialComponent = [];
+const materialComponent = [
+  MatButtonModule,
+  MatDialogModule,
+];
 
 @NgModule({
   declarations: [],
