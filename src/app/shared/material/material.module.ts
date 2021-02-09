86f7a9cb0af9f503979ac2c59e5eeb9b0fcc13b2
Yura Simonyan <tushe.s.y@gmail.com>
2021-02-09 20:18:11 +0200

    Added modal authentication.

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,19 +1,6 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 
-const materialComponent = [
-  MatButtonModule,
-  MatDialogModule,
-  MatDividerModule,
-  MatInputModule,
-  MatFormFieldModule,
-  MatIconModule
-];
+const materialComponent = [];
 
 @NgModule({
   declarations: [],
