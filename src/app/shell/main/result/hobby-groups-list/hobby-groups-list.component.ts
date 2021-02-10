4420ff6aed755278a8d778c48f53f2f4c8253bdb
Artem Miroschnichenko <artmimiron@gmail.com>
2021-02-10 20:38:50 +0200

    +generation of cards with mocked data

--- src/app/shell/main/result/hobby-groups-list/hobby-groups-list.component.ts
+++ src/app/shell/main/result/hobby-groups-list/hobby-groups-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Store } from '@ngxs/store';
+import { orgCard, ORGCARDS } from 'src/app/mock-org-cards';
 
 @Component({
   selector: 'app-hobby-groups-list',
@@ -8,16 +9,21 @@ import { Store } from '@ngxs/store';
   styleUrls: ['./hobby-groups-list.component.scss']
 })
 export class HobbyGroupsListComponent implements OnInit {
-  stateCards: Observable<Object[]>;
-  public cards: Array<Object> = [];
+  /* stateCards: Observable<Object[]>; */
+  public cards: orgCard[];
 
-  constructor(private store: Store) {
-    this.stateCards = this.store.select(state => state.ShowData);
+  constructor(/* private store: Store */) {
+    /* this.stateCards = this.store.select(state => state.ShowData); */
     // To do: change "state.ShowData" when state will be configured
   }
   
   ngOnInit(): void {
-    this.stateCards.subscribe((data) => this.cards = data);
+    /* this.stateCards.subscribe((data) => this.cards = data); */
+    this.getCards().subscribe(cards => this.cards = cards);
+  }
+
+  getCards(): Observable<orgCard[]> {
+    return of(ORGCARDS);
   }
 
 }
