3b2edb2f4a7f057a3bae1a30d4886f0a095b1ff8
Alexandr Nabokin <sashkai@ukr.net>
2021-02-12 15:22:34 +0200

    added div on map for hobby group card

--- src/app/shell/main/result/map/map.component.ts
+++ src/app/shell/main/result/map/map.component.ts
@@ -19,8 +19,7 @@ export class MapComponent implements OnInit {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
 
-  // Marker for the top of Mt. Ranier
-  summit = marker([ 50.408333, 30.556072 ], {
+  testMarker1 = marker([ 50.408333, 30.556072 ], {
     icon: icon({
       iconSize: [ 25, 41 ],
       iconAnchor: [ 13, 41 ],
@@ -29,8 +28,7 @@ export class MapComponent implements OnInit {
     })
   });
 
-  // Marker for the parking lot at the base of Mt. Ranier trails
-  paradise = marker([ 50.484972, 30.586922 ], {
+  testMarker2 = marker([ 50.484972, 30.586922 ], {
     icon: icon({
       iconSize: [ 25, 41 ],
       iconAnchor: [ 13, 41 ],
@@ -40,22 +38,8 @@ export class MapComponent implements OnInit {
     })
   });
 
-  // Path from paradise to summit - most points omitted from this example for brevity
-  route = polyline([[ 46.78465227596462,-121.74141269177198 ],
-    [ 46.80047278292477, -121.73470708541572 ],
-    [ 46.815471360459924, -121.72521826811135 ],
-    [ 46.8360239546746, -121.7323131300509 ],
-    [ 46.844306448474526, -121.73327445052564 ],
-    [ 46.84979408048093, -121.74325201660395 ],
-    [ 46.853193528950214, -121.74823296256363 ],
-    [ 46.85322881676257, -121.74843915738165 ],
-    [ 46.85119913890958, -121.7519719619304 ],
-    [ 46.85103829018772, -121.7542376741767 ],
-    [ 46.85101557523012, -121.75431755371392 ],
-    [ 46.85140013694763, -121.75727385096252 ],
-    [ 46.8525277543813, -121.75995212048292 ],
-    [ 46.85290292836726, -121.76049157977104 ],
-    [ 46.8528160918504, -121.76042997278273 ]]);
+  // You can make path from testMarker2 to testMarker1
+  route = polyline([]);
 
   // Layers control object with our two base layers and the three overlay layers
   layersControl = {
@@ -64,13 +48,13 @@ export class MapComponent implements OnInit {
       'Wikimedia Maps': this.wMaps
     },
     overlays: {
-      'Mt. Rainier Summit': this.summit,
-      'Mt. Rainier Paradise Start': this.paradise,
+      'Mt. Rainier testMarker1': this.testMarker1,
+      'Mt. Rainier testMarker2 Start': this.testMarker2,
       'Mt. Rainier Climb Route': this.route
     }
   };
   options = {
-    layers: [ this.streetMaps, this.route, this.summit, this.paradise ],
+    layers: [ this.streetMaps, this.route, this.testMarker1, this.testMarker2 ],
     zoom: 11,
     center: latLng([ 50.462235, 30.545131 ])
   };
