ed908bd1a526079cbd8170db28819041583b59a1
Oleksandra Lytvynets <alitvinets7@gmail.com>
2021-02-19 13:36:39 +0200

    header contains action AuthFail handler+snackbar+callApi is deleted (#64)

--- src/app/shared/store/user-registration.state.ts
+++ src/app/shared/store/user-registration.state.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { State, Action, StateContext, Selector } from '@ngxs/store';
-import { CallApi, Login, Logout, CheckAuth, CheckAuthFail } from './user-registration.actions';
+import { Login, Logout, CheckAuth, AuthFail } from './user-registration.actions';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { MatSnackBar } from '@angular/material/snack-bar';
 
 export interface UserRegistrationStateModel {
   isAuthorized: boolean;
@@ -20,9 +19,7 @@ export interface UserRegistrationStateModel {
 export class UserRegistrationState {
   constructor(
     public oidcSecurityService: OidcSecurityService,
-    public http: HttpClient,
-    public snackBar: MatSnackBar
-    ) {}
+    public http: HttpClient) {}
 
   @Selector()
     static isAuthorized(state: UserRegistrationStateModel) {
@@ -30,36 +27,15 @@ export class UserRegistrationState {
   }
 
   @Action(Login)
-    Login({  dispatch }: StateContext<UserRegistrationStateModel>): void {
-      this.oidcSecurityService.authorize();
-      dispatch(new CheckAuth()); 
+  Login({  dispatch }: StateContext<UserRegistrationStateModel>): void {
+    this.oidcSecurityService.authorize();
+    dispatch(new CheckAuth()); 
   }
   @Action(Logout)
-    Logout({  dispatch  }: StateContext<UserRegistrationStateModel>): void {
-      let isAuth: boolean;
-      this.oidcSecurityService.checkAuth().subscribe(value => {
-        isAuth = value;
-      });
-      if (isAuth) {
-        this.oidcSecurityService.logoff();
-        dispatch(new CheckAuth());
-      }
+  Logout({  dispatch  }: StateContext<UserRegistrationStateModel>): void {
+    this.oidcSecurityService.logoff();
+    dispatch(new CheckAuth());
   }
-  @Action(CallApi)
-    CallApi(): void {
-      const token = this.oidcSecurityService.getToken();
-
-      this.http.get('http://localhost:5000/Organization/TestOk', {
-        headers: new HttpHeaders({
-          Authorization: 'Bearer ' + token,
-        }),
-        responseType: 'text'
-      })
-        .subscribe((data: any) => {
-          console.log('api result:', data);
-        });
-  }
-  
   @Action(CheckAuth)
   CheckAuth({ patchState }: StateContext<UserRegistrationStateModel>): void {
     this.oidcSecurityService
@@ -69,11 +45,8 @@ export class UserRegistrationState {
         patchState({ isAuthorized: auth});
       });
   }
-  @Action(CheckAuthFail)
-  CheckAuthFail({ payload }: CheckAuthFail): void {
-    this.snackBar.open('Check your connection', "Try again!", {
-      duration: 5000,
-      panelClass: ['red-snackbar'],
-      });
+  @Action(AuthFail)
+  AuthFail(): void {
+      console.log("Authorization failed");
+    }
   }
-}
