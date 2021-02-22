bbdbce4ad103ef1875bd04777848a22e4e2a473d
Yura Simonyan <tushe.s.y@gmail.com>
2021-02-22 15:23:23 +0200

    fixed some errors

--- src/app/shared/store/user-registration.state.ts
+++ src/app/shared/store/user-registration.state.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { State, Action, StateContext, Selector } from '@ngxs/store';
-import {Login, Logout, CheckAuth, AuthFail, UserName, Role} from './user-registration.actions';
+import {Login, Logout, CheckAuth, AuthFail} from './user-registration.actions';
 
 import { HttpClient } from '@angular/common/http';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
@@ -49,24 +49,20 @@ export class UserRegistrationState {
     dispatch(new CheckAuth());
   }
   @Action(CheckAuth)
-  CheckAuth({ patchState }: StateContext<UserRegistrationStateModel>): void {
+  CheckAuth({ patchState }: StateContext<UserRegistrationStateModel>, {  dispatch  }: StateContext<UserRegistrationStateModel>): void {
     this.oidcSecurityService
       .checkAuth()
       .subscribe((auth) => {
         console.log('is authenticated', auth);
         patchState({ isAuthorized: auth});
+        if (auth) {
+          patchState({role: jwt_decode(this.oidcSecurityService.getToken()).role});
+          patchState({userName: jwt_decode(this.oidcSecurityService.getToken()).name});
+        }
       });
   }
   @Action(AuthFail)
   AuthFail(): void {
       console.log('Authorization failed');
     }
-  @Action(UserName)
-  UserName({  patchState }: StateContext<UserRegistrationStateModel>): void {
-    patchState({userName: jwt_decode(this.oidcSecurityService.getToken())['name']});
-  }
-  @Action(Role)
-  Role({  patchState }: StateContext<UserRegistrationStateModel>): void {
-    patchState({role: jwt_decode(this.oidcSecurityService.getToken())['role']});
-  }
   }
