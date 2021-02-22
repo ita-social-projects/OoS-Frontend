6bcbd352c627923850f1c1ddb9d33f4cccd1345d
YuraSimonyan <73949887+YuraSimonyan@users.noreply.github.com>
2021-02-22 15:31:40 +0200

    added user name (#66)
    * added user name
    
    * added roles
    
    * fixed some errors
    
    * fixed some errors
    
    * removed subscription from component

--- src/app/shared/store/user-registration.actions.ts
+++ src/app/shared/store/user-registration.actions.ts
@@ -14,3 +14,11 @@ export class AuthFail {
   static readonly type = '[user] has auth failed';
   constructor() {}
 }
+export class UserName {
+  static readonly type = '[user] name added';
+  constructor() {}
+}
+export class Role {
+  static readonly type = '[role] added';
+  constructor() {}
+}
--- src/app/shared/store/user-registration.state.ts
+++ src/app/shared/store/user-registration.state.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { State, Action, StateContext, Selector } from '@ngxs/store';
-import { Login, Logout, CheckAuth, AuthFail } from './user-registration.actions';
+import {Login, Logout, CheckAuth, AuthFail} from './user-registration.actions';
 
 import { HttpClient } from '@angular/common/http';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
+import jwt_decode from 'jwt-decode';
 
 export interface UserRegistrationStateModel {
   isAuthorized: boolean;
+  userName: string;
+  role: string;
 }
 
 @State<UserRegistrationStateModel>({
   name: 'user',
   defaults: {
     isAuthorized: false,
+    userName: '',
+    role: ''
   }
 })
 @Injectable()
@@ -22,8 +27,16 @@ export class UserRegistrationState {
     public http: HttpClient) {}
 
   @Selector()
-    static isAuthorized(state: UserRegistrationStateModel) {
+    static isAuthorized(state: UserRegistrationStateModel): boolean {
     return state.isAuthorized;
+  }
+  @Selector()
+  static userName(state: UserRegistrationStateModel): string {
+    return state.userName;
+  }
+  @Selector()
+  static role(state: UserRegistrationStateModel): string {
+    return state.role;
   }
 
   @Action(Login)
@@ -36,16 +49,20 @@ export class UserRegistrationState {
     dispatch(new CheckAuth());
   }
   @Action(CheckAuth)
-  CheckAuth({ patchState }: StateContext<UserRegistrationStateModel>): void {
+  CheckAuth({ patchState }: StateContext<UserRegistrationStateModel>, {  dispatch  }: StateContext<UserRegistrationStateModel>): void {
     this.oidcSecurityService
       .checkAuth()
       .subscribe((auth) => {
-        console.log('is authenticated', auth)
+        console.log('is authenticated', auth);
         patchState({ isAuthorized: auth});
+        if (auth) {
+          patchState({role: jwt_decode(this.oidcSecurityService.getToken()).role});
+          patchState({userName: jwt_decode(this.oidcSecurityService.getToken()).name});
+        }
       });
   }
   @Action(AuthFail)
   AuthFail(): void {
-      console.log("Authorization failed");
+      console.log('Authorization failed');
     }
   }
