77961d6aa5a2035fb42ea5d61ecc059b1cc3560c
Oleksandra Lytvynets <alitvinets7@gmail.com>
2021-02-24 17:25:49 +0200

    first view (without cards)

--- src/app/shell/provider/provider.module.ts
+++ src/app/shell/provider/provider.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ProviderActivitiesComponent } from './provider-activities/provider-activities.component';
 import { ProviderConfigComponent } from './provider-config/provider-config.component';
 import { ProviderRoutingModule } from './provider-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { ProviderRoutingModule } from './provider-routing.module';
     ProviderConfigComponent],
   imports: [
     CommonModule,
-    ProviderRoutingModule
+    ProviderRoutingModule,
+    FlexLayoutModule,
+    MatButtonModule
   ]
 })
 export class ProviderModule { }
