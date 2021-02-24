170824854086362810d3fb619a7528668daca00c
Yura Simonyan <tushe.s.y@gmail.com>
2021-02-24 22:00:17 +0200

    added registration-provider comp

---
+++ src/app/shell/provider-registration/provider-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-provider-registration',
+  templateUrl: './provider-registration.component.html',
+  styleUrls: ['./provider-registration.component.scss']
+})
+export class ProviderRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
