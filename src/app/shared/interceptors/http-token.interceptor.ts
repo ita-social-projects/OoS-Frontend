ac5445a4542194808001a32d139919f486fed7d8
Oleksandra Lytvynets <alitvinets7@gmail.com>
2021-03-02 23:33:02 +0200

    Revert "auth service. This version doesnt work"
    This reverts commit 5cd269214a782a2e9b38d61e97708ccb7ca8a64b.

--- src/app/shared/interceptors/http-token.interceptor.ts
+++ src/app/shared/interceptors/http-token.interceptor.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-
-import { AuthService } from '../services/org-cards/auth.service';
-import { Select, Store } from '@ngxs/store';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { Select } from '@ngxs/store';
 import { UserRegistrationState } from '../store/user-registration.state';
 
 ​
@@ -12,33 +11,31 @@ import { UserRegistrationState } from '../store/user-registration.state';
 export class HttpTokenInterceptor implements HttpInterceptor {
 ​
 constructor(
-  public auth: AuthService,
-  public store: Store) {}
+    private oidcSecurityService: OidcSecurityService,
+    private http: HttpClient) {}
 
-    @Select(UserRegistrationState.role)
-    role$: Observable<string>;
+@Select(UserRegistrationState.role)
+role$: Observable<string>;
+​
+public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+​
+    let token = this.oidcSecurityService.getToken();
+    const role = this.oidcSecurityService.getToken()['role'];
+    const tokenTitle = (role === this.role$) ? 'Authorization' : 'token';
 
-    public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-      ​
-          let token = this.auth.getToken();
-          const role = this.store.selectSnapshot<string>(UserRegistrationState.role);
-      ​
-          const tokenTitle = (role) ?  'token': 'Authorization';
-          // 'token !== null' added to avoid pass condition when "typeof token === 'string'" and token is set to 'Bearer null'
-          if (role && token !== null) {
-            token = `Bearer ${token}`;
-            console.log('Http token interceptor works!')
-          }
-      ​
-          if (typeof token === 'string') {
-            return next.handle(request.clone({
-              setHeaders: { [tokenTitle]: token }
-            })).pipe(catchError((error) => {
-              return throwError(error);
-            }));
-          }
-          return next.handle(request).pipe(catchError((error) => {
-            return throwError(error);
-          }));
-        }
-      }
+    if (this.role$ === role && token !== null) {
+      token = `Bearer ${token}`;
+      console.log('This thing works')
+    }
+    if (typeof token === 'string') {
+      return next.handle(request.clone({
+        setHeaders: { [tokenTitle]: token }
+      })).pipe(catchError((error) => {
+        return throwError(error);
+      }));
+    }
+    return next.handle(request).pipe(catchError((error) => {
+      return throwError(error);
+    }));
+  }
+}
