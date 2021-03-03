d930f81f37a1aace77b7d3fbd6e15f80fff37ddf
Oleksandra Lytvynets <alitvinets7@gmail.com>
2021-03-03 10:46:14 +0200

    works!

--- src/app/shared/interceptors/http-token.interceptor.ts
+++ src/app/shared/interceptors/http-token.interceptor.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
-import { Select } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { UserRegistrationState } from '../store/user-registration.state';
 
 ​
@@ -11,6 +11,7 @@ import { UserRegistrationState } from '../store/user-registration.state';
 export class HttpTokenInterceptor implements HttpInterceptor {
 ​
 constructor(
+    public store: Store,
     private oidcSecurityService: OidcSecurityService,
     private http: HttpClient) {}
 
@@ -20,17 +21,14 @@ role$: Observable<string>;
 public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 ​
     let token = this.oidcSecurityService.getToken();
-    const role = this.oidcSecurityService.getToken()['role'];
-    const tokenTitle = (role === this.role$) ? 'Authorization' : 'token';
 
-    if (this.role$ === role && token !== null) {
-      token = `Bearer ${token}`;
-      console.log('This thing works')
-    }
-    if (typeof token === 'string') {
+    let tokenTitle= (token) ? `Bearer ${token}`: null;
+    
+    if (typeof(token) === 'string') {
       return next.handle(request.clone({
-        setHeaders: { [tokenTitle]: token }
-      })).pipe(catchError((error) => {
+        setHeaders: { Authorization: tokenTitle }
+      }))
+        .pipe(catchError((error) => {
         return throwError(error);
       }));
     }
