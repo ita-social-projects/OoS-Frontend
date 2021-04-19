bd640cda6b50ba65eabbe19b94b06662adfcaa19
Sasha <alitvinets7@gmail.com>
2021-04-19 17:21:46 +0300

    Update min-max.directive.ts

--- src/app/shared/directives/min-max.directive.ts
+++ src/app/shared/directives/min-max.directive.ts
@@ -5,23 +5,18 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class MinMaxDirective {
 
-  @Input()
-  public min: number;
-
-  @Input()
-  public max: number;
+  @Input() public min: number;
+  @Input() public max: number;
 
   constructor(private ref: ElementRef) { }
 
-  @HostListener('input', [ '$event' ])
-    public onInput(a_Event: InputEvent): void {
-      let val = parseInt(this.ref.nativeElement.value);
-      if(this.max !== null && this.max !== undefined  && val >= this.max)
-        this.ref.nativeElement.value = this.max.toString();
-      else if (this.min !== null && this.min !== undefined  && val <= this.min)
-        this.ref.nativeElement.value = this.min.toString();
-    }
+  @HostListener('input', ['$event'])
+  public onInput(a_Event: InputEvent): void {
+    let val = parseInt(this.ref.nativeElement.value);
+    if (this.max !== null && this.max !== undefined && val >= this.max)
+      this.ref.nativeElement.value = this.max.toString();
+    else if (this.min !== null && this.min !== undefined && val <= this.min)
+      this.ref.nativeElement.value = this.min.toString();
+  }
 
 }
-
-
