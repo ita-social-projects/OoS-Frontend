99beeb2ec34d92d7bf7d816e1e8cefa86101da14
Yura Simonyan <tushe.s.y@gmail.com>
2021-04-22 14:11:39 +0300

    fixed some bugs

--- src/app/shell/provider/provider-routing.module.ts
+++ src/app/shell/provider/provider-routing.module.ts
@@ -1,40 +1,42 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PersonalCabinetComponent } from './personal-cabinet/personal-cabinet.component';
-import { CreateActivityComponent } from './provider-activities/create-activity/create-activity.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {PersonalCabinetComponent} from './personal-cabinet/personal-cabinet.component';
+import {CreateActivityComponent} from './provider-activities/create-activity/create-activity.component';
 
-import { ProviderActivitiesComponent } from './provider-activities/provider-activities.component';
-import { ProviderConfigComponent } from './provider-config/provider-config.component';
-import { ProviderOrgInfoComponent } from './provider-org-info/provider-org-info.component';
-import { ProviderRequestsComponent } from './provider-requests/provider-requests.component';
+import {ProviderActivitiesComponent} from './provider-activities/provider-activities.component';
+import {ProviderConfigComponent} from './provider-config/provider-config.component';
+import {ProviderOrgInfoComponent} from './provider-org-info/provider-org-info.component';
+import {ProviderRequestsComponent} from './provider-requests/provider-requests.component';
 
 const routes: Routes = [
-  { path: 'cabinet', component: PersonalCabinetComponent,
-  children: [
-    {
-      path: 'config', 
-      component: ProviderConfigComponent, 
-    },
-    {
-      path: 'org-info', 
-      component: ProviderOrgInfoComponent, 
-    },
-    {
-      path: 'activities', 
-      component: ProviderActivitiesComponent, 
-    },
-    {
-      path: 'requests', 
-      component: ProviderRequestsComponent, 
-    },
-    
-  ]
- },
- {path: 'create-activity/:id', component: CreateActivityComponent} 
+  {
+    path: 'cabinet', component: PersonalCabinetComponent,
+    children: [
+      {
+        path: 'config',
+        component: ProviderConfigComponent,
+      },
+      {
+        path: 'org-info',
+        component: ProviderOrgInfoComponent,
+      },
+      {
+        path: 'activities',
+        component: ProviderActivitiesComponent,
+      },
+      {
+        path: 'requests',
+        component: ProviderRequestsComponent,
+      },
+
+    ]
+  },
+  {path: 'create-activity/:id', component: CreateActivityComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ProviderRoutingModule { }
+export class ProviderRoutingModule {
+}
