4bfb39837c6b643db0ed97b3645fbd74a4c2676e
Sasha <alitvinets7@gmail.com>
2021-05-27 11:42:00 +0300

    mock links are replaced with the real ones

--- src/app/shared/models/category.model.ts
+++ src/app/shared/models/category.model.ts
@@ -1,5 +1,18 @@
 export interface Category {
   id: number;
   title: string;
-  picture: string;
+  description: string;
+}
+export interface Subcategory {
+  id: number;
+  categoryId: number;
+  title: string;
+  description: string;
+}
+
+export interface Subsubcategory {
+  id: number;
+  title: string;
+  description: string;
+  subcategoryId: number;
 }
--- src/app/shared/services/categories/categories.service.ts
+++ src/app/shared/services/categories/categories.service.ts
@@ -10,10 +10,11 @@ export class CategoriesService {
 
   dataUrlCategories = '/assets/mock-categories-cards.json';
   dataUrlIcons = '/assets/categories-icons.json';
+  dataUrl = '/Category/Get';
 
   constructor(private http: HttpClient) { }
 
   getCategories(): Observable<Category[]> {
-    return this.http.get<Category[]>(this.dataUrlCategories);
+    return this.http.get<Category[]>(this.dataUrl);
   }
 }
