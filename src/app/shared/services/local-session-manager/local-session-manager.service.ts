a44e809110b88dd5d1e38fc8a57a87779e076c93
Oleksandra Lytvynets <alitvinets7@gmail.com>
2021-05-28 17:57:47 +0300

    LocalSessionManagerService (#208)

---
+++ src/app/shared/services/local-session-manager/local-session-manager.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { AbstractSecurityStorage } from 'angular-auth-oidc-client';
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalSessionManagerService implements AbstractSecurityStorage {
+  constructor() { }
+  remove(key: string): void {
+    localStorage.removeItem(key);
+  }
+  read(key: string) {
+    let item = localStorage.getItem(key);
+    if (!!item) {
+      return JSON.parse(item);
+    }
+    else {
+      return null;
+    }
+  }
+  write(key: string, value: any) {
+    value = value || null;
+    localStorage.setItem(key, JSON.stringify(value));
+    return true;
+  }
+}
