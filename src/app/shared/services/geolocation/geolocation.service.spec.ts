e764ebc6b2a0f397c861d407c33bc76308233e0f
Sasha <alitvinets7@gmail.com>
2021-06-07 10:43:44 +0300

    address fix

--- src/app/shared/services/geolocation/geolocation.service.spec.ts
+++ src/app/shared/services/geolocation/geolocation.service.spec.ts
@@ -1,9 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-
 import { GeolocationService } from './geolocation.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import Geocoder from 'leaflet-control-geocoder';
-
+import { MapComponent } from '../../../shell/personal-cabinet/provider/create-workshop/create-address/map/map.component';
 
 describe('GeolocationService', () => {
   let service: GeolocationService;
