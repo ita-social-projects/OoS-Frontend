a72d3d78729e940c10cc81817d80354d285ed085
Sasha <alitvinets7@gmail.com>
2021-06-09 11:02:04 +0300

    comments fix

--- src/app/shared/components/workshop-card/workshop-card.component.spec.ts
+++ src/app/shared/components/workshop-card/workshop-card.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { WorkshopCardComponent } from './workshop-card.component';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { NgxsModule, Store } from '@ngxs/store';
+import { NgxsModule } from '@ngxs/store';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Workshop } from '../../models/workshop.model';
