7a74843314211a73f1b4ef4d441ca16af195f90d
unknown <zim21den21@gmail.com>
2021-06-10 12:47:47 +0300

    fix button

--- src/app/shell/workshop-details/side-menu/actions/actions.component.ts
+++ src/app/shell/workshop-details/side-menu/actions/actions.component.ts
@@ -1,5 +1,9 @@
+import { Store } from '@ngxs/store';
 import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/user.model';
 import { Workshop } from 'src/app/shared/models/workshop.model';
+import { RegistrationState } from 'src/app/shared/store/registration.state';
+import { Role } from 'src/app/shared/enum/role';
 
 @Component({
   selector: 'app-actions',
@@ -7,12 +11,13 @@ import { Workshop } from 'src/app/shared/models/workshop.model';
   styleUrls: ['./actions.component.scss']
 })
 export class ActionsComponent implements OnInit {
-
+  isDisplayed: boolean;
   @Input() workshop: Workshop;
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
+    this.isDisplayed = this.store.selectSnapshot<User>(RegistrationState.user).role !== Role.provider;
   }
 
 }
