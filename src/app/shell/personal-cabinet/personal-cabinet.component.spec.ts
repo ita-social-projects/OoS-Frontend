ce743fc42df58f1b108f8b5f29e2368d9871a888
Sasha <alitvinets7@gmail.com>
2021-06-12 15:03:03 +0300

    Update personal-cabinet.component.spec.ts

--- src/app/shell/personal-cabinet/personal-cabinet.component.spec.ts
+++ src/app/shell/personal-cabinet/personal-cabinet.component.spec.ts
@@ -2,10 +2,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PersonalCabinetComponent } from './personal-cabinet.component';
 import { NgxsModule, Store } from '@ngxs/store';
 import { RouterTestingModule } from '@angular/router/testing';
+import { User } from 'src/app/shared/models/user.model';
 
 describe('PersonalCabinetComponent', () => {
   let component: PersonalCabinetComponent;
   let fixture: ComponentFixture<PersonalCabinetComponent>;
+  let store: Store;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,12 +16,15 @@ describe('PersonalCabinetComponent', () => {
         NgxsModule.forRoot([]),
         RouterTestingModule
       ],
-      declarations: [ PersonalCabinetComponent ],
+      declarations: [PersonalCabinetComponent],
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
+    store = TestBed.inject(Store);
+    spyOn(store, 'selectSnapshot').and.returnValue({ role: '' } as User);
+
     fixture = TestBed.createComponent(PersonalCabinetComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
