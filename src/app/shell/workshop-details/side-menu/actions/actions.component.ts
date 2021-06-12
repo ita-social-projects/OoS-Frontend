82ba4820dc07e90bb7c5325531e8aa787ee1fc65
DenysIvashchenko <71029756+DenysIvashchenko@users.noreply.github.com>
2021-06-12 11:11:57 +0300

    fix button (#253)
    * fix button
    
    * Removed Create Request Button for user under provider profile
    
    * fixed test for ActionsComponent

--- src/app/shell/workshop-details/side-menu/actions/actions.component.ts
+++ src/app/shell/workshop-details/side-menu/actions/actions.component.ts
@@ -1,5 +1,9 @@
+import { Store } from '@ngxs/store';
 import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/user.model';
 import { Workshop } from 'src/app/shared/models/workshop.model';
+import { RegistrationState } from 'src/app/shared/store/registration.state';
+import { Role } from 'src/app/shared/enum/role';
 
 @Component({
   selector: 'app-actions',
@@ -7,12 +11,26 @@ import { Workshop } from 'src/app/shared/models/workshop.model';
   styleUrls: ['./actions.component.scss']
 })
 export class ActionsComponent implements OnInit {
-
+  displayed: boolean = this.isDisplayed();
   @Input() workshop: Workshop;
 
-  constructor() { }
+  constructor(private store: Store) { }
+
+  /**
+  * This method takes user Role and return boolean type for "displayed"
+  * to display button 
+  */
+
+  isDisplayed(): boolean {
+    const user: User = this.store.selectSnapshot<User>(RegistrationState.user);
+    if(user && user.role === Role.provider) {
+      return false
+    }
+    return true
+  }
 
   ngOnInit(): void {
+    this.isDisplayed();
   }
 
 }
