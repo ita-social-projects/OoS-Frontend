2a0e3ea1b5c9d1fd19be86dfd448917474dfe655
Sasha <alitvinets7@gmail.com>
2021-06-12 18:40:07 +0300

    created child cards

--- src/app/shared/components/child-card/child-card.component.ts
+++ src/app/shared/components/child-card/child-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Child } from '../../models/child.model';
+import { Workshop } from '../../models/workshop.model';
 
 @Component({
   selector: 'app-child-card',
@@ -10,7 +11,33 @@ export class ChildCardComponent implements OnInit {
 
   @Input() child: Child;
 
+  workshops: Workshop[];
+
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  /**
+   * This method get of the child
+   * @param string birth date
+  * @returns number Age
+  */
+  getAge(): number {
+    const today = new Date();
+    const birthDate = new Date(this.child.dateOfBirth);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    let m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
+  onEdit(): void {
+    console.log('I edit it!');//TODO: link the real Api when child creation will be fixed
+  }
+
+  onDelete(): void {
+    console.log('I delete it!'); //TODO: link the real Api when child creation will be fixed
+  }
 }
