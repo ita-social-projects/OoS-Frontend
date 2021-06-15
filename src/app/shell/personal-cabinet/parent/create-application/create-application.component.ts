e792a492206bbcbaf8a5525d6a3f85d3a7c430c3
Sasha <alitvinets7@gmail.com>
2021-06-15 11:26:22 +0300

    linked workshop  card to the application

--- src/app/shell/personal-cabinet/parent/create-application/create-application.component.ts
+++ src/app/shell/personal-cabinet/parent/create-application/create-application.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { cardType } from 'src/app/shared/enum/role';
@@ -10,7 +11,7 @@ import { User } from 'src/app/shared/models/user.model';
 import { Workshop } from 'src/app/shared/models/workshop.model';
 
 import { RegistrationState } from 'src/app/shared/store/registration.state';
-import { CreateApplication, GetChildren } from 'src/app/shared/store/user.actions';
+import { CreateApplication, GetChildren, GetWorkshopsById } from 'src/app/shared/store/user.actions';
 import { UserState } from 'src/app/shared/store/user.state';
 
 
@@ -27,20 +28,23 @@ export class CreateApplicationComponent implements OnInit {
   @Select(RegistrationState.parent) parent$: Observable<Parent>;
   @Select(RegistrationState.user) user$: Observable<User>;
 
-
   children: Child[] = [];
   selectedChild: Child;
-  // workshop: Workshop;
-  workshop = {
-    id: 3
-  }
+
+  @Select(UserState.selectedWorkshop) workshop$: Observable<Workshop>;
+  workshop: Workshop;
 
   ChildFormControl = new FormControl('');
 
-  constructor(private store: Store) { }
+  constructor(
+    private store: Store,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.store.dispatch(new GetChildren());
+    const workshopId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+    this.store.dispatch(new GetWorkshopsById(workshopId));
+    this.workshop$.subscribe(workshop => this.workshop = workshop);
   }
 
   /**
