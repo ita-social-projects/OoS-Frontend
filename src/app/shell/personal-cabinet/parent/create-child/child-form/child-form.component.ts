ac2f7797a511a9da4482dcda9d2f9b7270a14585
Oleksandra Lytvynets <alitvinets7@gmail.com>
2021-07-05 11:22:29 +0300

    Calendar (#325)
    * date picker style fix + disable to pick the future date
    
    * styled calendar
    
    * final styling
    
    * space-between
    
    * Update shell.module.ts

--- src/app/shell/personal-cabinet/parent/create-child/child-form/child-form.component.ts
+++ src/app/shell/personal-cabinet/parent/create-child/child-form/child-form.component.ts
@@ -1,11 +1,12 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { SocialGroup } from 'src/app/shared/models/socialGroup.model';
 
 @Component({
   selector: 'app-child-form',
   templateUrl: './child-form.component.html',
-  styleUrls: ['./child-form.component.scss']
+  styleUrls: ['./child-form.component.scss'],
 })
 export class ChildFormComponent implements OnInit {
 
@@ -16,6 +17,8 @@ export class ChildFormComponent implements OnInit {
 
   @Output() deleteForm = new EventEmitter();
 
+  today: Date = new Date();
+
   constructor() { }
 
   ngOnInit(): void { }
@@ -23,5 +26,4 @@ export class ChildFormComponent implements OnInit {
   delete(): void {
     this.deleteForm.emit(this.index);
   }
-
 }
