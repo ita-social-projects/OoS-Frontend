01dbfa22d706756b558c9dc43d594c614c01ba8c
unknown <zim21den21@gmail.com>
2021-07-09 16:04:12 +0300

    test fixed

--- src/app/shared/components/navigation-mobile-bar/navigation-mobile-bar.component.spec.ts
+++ src/app/shared/components/navigation-mobile-bar/navigation-mobile-bar.component.spec.ts
@@ -1,19 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxsModule, Store } from '@ngxs/store';
+import { Navigation } from '../../models/navigation.model';
 import { NavigationMobileBarComponent } from './navigation-mobile-bar.component';
 
 describe('NavigationMobileBarComponent', () => {
   let component: NavigationMobileBarComponent;
   let fixture: ComponentFixture<NavigationMobileBarComponent>;
-
+  let store: Store;
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[
+        RouterTestingModule,
+        MatIconModule,
+        NgxsModule.forRoot([]),
+      ],
       declarations: [ NavigationMobileBarComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    store = TestBed.inject(Store);
+    spyOn(store,'selectSnapshot').and.returnValue({} as Navigation);
     fixture = TestBed.createComponent(NavigationMobileBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
