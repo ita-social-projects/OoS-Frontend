59ddf12be46f6b3b75aa94ff08ea514e3dea7030
unknown <zim21den21@gmail.com>
2021-07-13 15:55:10 +0300

    final fix test

--- src/app/shell/personal-cabinet/parent/favorite-workshops/favorite-workshops.component.spec.ts
+++ src/app/shell/personal-cabinet/parent/favorite-workshops/favorite-workshops.component.spec.ts
@@ -1,5 +1,5 @@
 import { Workshop } from './../../../../shared/models/workshop.model';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxsModule, Store } from '@ngxs/store';
@@ -40,4 +40,8 @@ describe('FavoriteWorkshopsComponent', () => {
   selector: 'app-workshop-card',
   template: ''
 })
-class MockWorkshopCardComponent{ }
+class MockWorkshopCardComponent{
+  @Input() workshop: Workshop;
+  @Input() isMainPage: boolean;
+  @Input() userRole: string;
+ }
