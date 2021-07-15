587c92b68ae837882bb0378ed14fef7444ec47cc
Julia Sivokon <sivxkxn@gmail.com>
2021-07-15 12:18:31 +0300

    in side-menu

--- src/app/shell/workshop-details/side-menu/actions/actions.component.ts
+++ src/app/shell/workshop-details/side-menu/actions/actions.component.ts
@@ -1,10 +1,8 @@
 import { Store } from '@ngxs/store';
-import { Component, Input, OnInit, Provider } from '@angular/core';
-import { User } from 'src/app/shared/models/user.model';
+import { Component, Input, OnInit } from '@angular/core';
 import { Workshop } from 'src/app/shared/models/workshop.model';
-import { RegistrationState } from 'src/app/shared/store/registration.state';
 import {  Login } from 'src/app/shared/store/registration.actions';
-import { Role } from 'src/app/shared/enum/role';
+
 
 @Component({
   selector: 'app-actions',
@@ -12,32 +10,18 @@ import { Role } from 'src/app/shared/enum/role';
   styleUrls: ['./actions.component.scss']
 })
 export class ActionsComponent implements OnInit {
-  displayed: boolean = this.isDisplayed();
-  registered:boolean = false;
   @Input() workshop: Workshop;
+  @Input() isRegistered: boolean;
+  
 
   constructor(private store: Store) { }
 
-  /**
-  * This method takes user Role and return boolean type for "displayed"
-  * to display button
-  */
-
-   isDisplayed(): boolean {
-    const user: User = this.store.selectSnapshot<User>(RegistrationState.user);
-    if (user) {
-      this.registered=true;
-      if(user.role === Role.provider){
-      return false
-      }
-    }
-    return true
-  }
-  login(): void {
+  login(isReg: boolean): void {
+    if(!isReg){
     this.store.dispatch(new Login());
+    }
   }
   ngOnInit(): void {
-    this.isDisplayed();
   }
 
 }
--- src/app/shell/workshop-details/side-menu/side-menu.component.ts
+++ src/app/shell/workshop-details/side-menu/side-menu.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
 import { Workshop } from 'src/app/shared/models/workshop.model';
+import { Component, Input, OnInit, Provider } from '@angular/core';
+import { User } from 'src/app/shared/models/user.model';
+import { RegistrationState } from 'src/app/shared/store/registration.state';
+import { Role } from 'src/app/shared/enum/role';
 
 @Component({
   selector: 'app-side-menu',
@@ -8,11 +12,24 @@ import { Workshop } from 'src/app/shared/models/workshop.model';
 })
 export class SideMenuComponent implements OnInit {
 
+  isDisplayed: boolean = false;
+  isRegistered: boolean = false;
+
   @Input() workshop: Workshop;
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
-  }
+    const user = this.store.selectSnapshot<User>(RegistrationState.user);
+    if (user) {
+      this.isRegistered = true;
+      if (user.role !== Role.provider) {
+        this.isDisplayed = true;
+      }
+    }
+    else{
+      this.isDisplayed = true;
+    }
 
+  }
 }
