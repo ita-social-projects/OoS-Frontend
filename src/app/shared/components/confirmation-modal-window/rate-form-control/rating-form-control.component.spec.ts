e69ccf8d8e66dd964645af727f9c0a6aefd6264b
Sasha <alitvinets7@gmail.com>
2021-07-23 09:36:21 +0300

    done!!

--- src/app/shared/components/confirmation-modal-window/rate-form-control/rating-form-control.component.spec.ts
+++ src/app/shared/components/confirmation-modal-window/rate-form-control/rating-form-control.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { RatingFormControlComponent } from './rating-form-control.component';
 
@@ -8,6 +13,13 @@ describe('RateFormControlComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        MatButtonToggleModule,
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+      ],
       declarations: [RatingFormControlComponent]
     })
       .compileComponents();
--- src/app/shell/workshop-details/workshop-page/reviews/reviews.component.spec.ts
+++ src/app/shell/workshop-details/workshop-page/reviews/reviews.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgxsModule } from '@ngxs/store';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxsModule, Store } from '@ngxs/store';
+import { Parent } from 'src/app/shared/models/parent.model';
+import { Workshop } from 'src/app/shared/models/workshop.model';
 import { ReviewsComponent } from './reviews.component';
 
 describe('ReviewsComponent', () => {
   let component: ReviewsComponent;
   let fixture: ComponentFixture<ReviewsComponent>;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         NgxsModule.forRoot([]),
+        MatDialogModule,
+        MatIconModule
       ],
       declarations: [ReviewsComponent]
     })
@@ -17,8 +24,11 @@ describe('ReviewsComponent', () => {
   });
 
   beforeEach(() => {
+    store = TestBed.inject(Store);
+    spyOn(store, 'selectSnapshot').and.returnValue({} as Parent);
     fixture = TestBed.createComponent(ReviewsComponent);
     component = fixture.componentInstance;
+    component.workshop = {} as Workshop;
     fixture.detectChanges();
   });
 
