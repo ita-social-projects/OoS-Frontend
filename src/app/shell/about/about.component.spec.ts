3059a85027616d8595b917542fafa1cd0e5edd4d
Julia Sivokon <sivxkxn@gmail.com>
2021-08-05 09:46:36 +0300

    page about

---
+++ src/app/shell/about/about.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxsModule } from '@ngxs/store';
+
+import { AboutComponent } from './about.component';
+
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        NgxsModule.forRoot([]),
+      ],
+      declarations: [ AboutComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shell/about/about.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { NavBarName } from 'src/app/shared/enum/navigation-bar';
+import { NavigationBarService } from 'src/app/shared/services/navigation-bar/navigation-bar.service';
+import { AddNavPath, DeleteNavPath } from 'src/app/shared/store/navigation.actions';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor(private store: Store, public navigationBarService: NavigationBarService,) {
+
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(
+      new AddNavPath(this.navigationBarService.creatOneNavPath(
+        { name: NavBarName.About, isActive: false, disable: true }
+      )),
+    );
+  }
+  ngOnDestroy(): void {
+    this.store.dispatch(new DeleteNavPath());
+  }
+
+}
