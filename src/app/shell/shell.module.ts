bdd2c86d3fea62af34cbbfcf7b54949a44a18e10
Oleksandra Lytvynets <alitvinets7@gmail.com>
2021-09-14 19:29:47 +0300

    Date picker (#463)
    * install moment
    
    * localization
    
    * update

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -19,10 +19,12 @@ import { ProviderGuard } from './personal-cabinet/provider/provider.guard';
 import { ParentGuard } from './personal-cabinet/parent/parent.guard';
 import { CreateProviderGuard } from './personal-cabinet/provider/create-provider/create-provider.guard';
 import { WorkshopMapViewListComponent } from './result/workshop-map-view-list/workshop-map-view-list.component';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { AllCategoriesComponent } from './all-categories/all-categories.component';
 import { AboutComponent } from './about/about.component';
 import { SupportComponent } from './support/support.component';
+import { MOMENT_DATE_FORMATS } from '../shared/constants/constants';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
 @NgModule({
   declarations: [
     MainComponent,
@@ -53,6 +55,8 @@ import { SupportComponent } from './support/support.component';
     CreateProviderGuard,
     { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
     { provide: MAT_DATE_LOCALE, useValue: 'uk-UA' },
+    { provide: MAT_DATE_FORMATS, useValue: MOMENT_DATE_FORMATS },
+    { provide: DateAdapter, useClass: MomentDateAdapter },
   ]
 })
 export class ShellModule { }
