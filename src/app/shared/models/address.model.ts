2aac6693cbede31cbd4d3988eac8311fe6be769c
Sasha <alitvinets7@gmail.com>
2021-09-15 20:24:01 +0300

    handling of actual address null

--- src/app/shared/models/address.model.ts
+++ src/app/shared/models/address.model.ts
@@ -8,13 +8,15 @@ export class Address {
   latitude?: number;
   longitude?: number;
 
-  constructor(info) {
+  constructor(info, id?: number) {
     this.city = info.city;
     this.street = info.street;
     this.buildingNumber = info.buildingNumber;
     this.region = info.region;
     this.district = info.district;
     this.buildingNumber = info.buildingNumber;
-
+    if (id) {
+      this.id = id;
+    }
   }
 }
