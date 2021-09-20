764f84a42c2d3dbafb8540b7a6b9804dc3c275bf
Sasha <alitvinets7@gmail.com>
2021-09-20 12:15:35 +0300

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  stsServer: 'http://auth.oos.dmytrominochkin.cloud',
-  serverUrl: 'http://api.oos.dmytrominochkin.cloud',
+  stsServer: 'https://auth.oos.dmytrominochkin.cloud',
+  serverUrl: 'https://api.oos.dmytrominochkin.cloud',
 }
