03d3539b20c0ee5faef98b605e5ea601661ee57f
Sasha <alitvinets7@gmail.com>
2021-09-30 14:52:14 +0300

    statuses

--- src/app/shared/enum/applications.ts
+++ src/app/shared/enum/applications.ts
@@ -1,16 +1,13 @@
 export enum ApplicationStatus {
-
-  Pending,
-  AcceptedForSelection,
-  Approved,
-  Rejected,
-  Left
+  Pending = 'Pending',
+  AcceptedForSelection = 'Pending',
+  Approved = 'Approved',
+  Rejected = 'Rejected',
+  Left = 'Left'
 }
 export enum ApplicationIcons {
-
-  "fas fa-user-clock",
-  "fas fa-user-plus",
-  "fas fa-user-check",
-  "fas fa-user-times",
-  "fas fa-user-times "
+  'fas fa-user-clock',
+  'fas fa-user-plus',
+  'fas fa-user-check',
+  'fas fa-user-times',
 }
