b63044f79cee92fccfb49776c7a645226ae6e82f
Sasha <alitvinets7@gmail.com>
2021-10-25 20:56:22 +0300

    final

--- src/app/shell/personal-cabinet/parent/favorite-workshops/favorite-workshops.component.ts
+++ src/app/shell/personal-cabinet/parent/favorite-workshops/favorite-workshops.component.ts
@@ -1,14 +1,13 @@
 import { UserState } from 'src/app/shared/store/user.state';
-import { Component, ElementRef, OnDestroy, OnInit, ViewChild, Pipe } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { WorkshopCard } from 'src/app/shared/models/workshop.model';
 import { Observable, Subject } from 'rxjs';
-import { RegistrationState } from 'src/app/shared/store/registration.state';
-import { map, scan, takeUntil, count } from 'rxjs/operators';
 import { PaginationElement } from 'src/app/shared/models/paginationElement.model';
 import { PageChange } from 'src/app/shared/store/filter.actions';
 import { Util } from 'src/app/shared/utils/utils';
 import { Constants } from 'src/app/shared/constants/constants';
+import { Role } from 'src/app/shared/enum/role';
 
 
 @Component({
@@ -17,17 +16,16 @@ import { Constants } from 'src/app/shared/constants/constants';
   styleUrls: ['./favorite-workshops.component.scss']
 })
 export class FavoriteWorkshopsComponent implements OnInit, OnDestroy {
-
-  @Select(UserState.favoriteWorkshopsCard)
-  favoriteWorkshopsCard$: Observable<WorkshopCard[]>;
-  @Select(RegistrationState.role)
-  role$: Observable<string>;
-  destroy$: Subject<boolean> = new Subject<boolean>();
+  readonly Role: typeof Role = Role;
   widthOfWorkshopCard = Constants.WIDTH_OF_WORKSHOP_CARD;
   currentPage: PaginationElement = {
     element: 1,
     isActive: true
   };
+
+  @Select(UserState.favoriteWorkshopsCard)
+  favoriteWorkshopsCard$: Observable<WorkshopCard[]>;
+  destroy$: Subject<boolean> = new Subject<boolean>();
   @ViewChild('WorkshopsWrap') workshopsWrap: ElementRef;
   getEmptyCards = Util.getEmptyCards;
 
