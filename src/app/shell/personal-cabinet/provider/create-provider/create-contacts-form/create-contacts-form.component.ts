460f2e8ca7ec6a915ec375af835d177546539675
Sasha <alitvinets7@gmail.com>
2021-10-26 20:55:14 +0300

    first view

--- src/app/shell/personal-cabinet/provider/create-provider/create-contacts-form/create-contacts-form.component.ts
+++ src/app/shell/personal-cabinet/provider/create-provider/create-contacts-form/create-contacts-form.component.ts
@@ -23,36 +23,45 @@ export class CreateContactsFormComponent implements OnInit {
       buildingNumber: new FormControl('', [Validators.required, Validators.pattern(TEXT_WITH_DIGITS_REGEX)]),
       city: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)]),
       district: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)]),
-      region: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)])
+      region: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)]),
     });
 
     this.ActualAddressFormGroup = this.formBuilder.group({
-      street: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)]),
-      buildingNumber: new FormControl('', [Validators.required, Validators.pattern(TEXT_WITH_DIGITS_REGEX)]),
-      city: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)]),
-      district: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)]),
-      region: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)])
+      street: new FormControl(''),
+      buildingNumber: new FormControl(''),
+      city: new FormControl(''),
+      district: new FormControl(''),
+      region: new FormControl('')
     });
   }
 
   ngOnInit(): void {
     this.passActualAddressFormGroup.emit(this.ActualAddressFormGroup);
     this.passLegalAddressFormGroup.emit(this.LegalAddressFormGroup);
-    this.isSameAddressControl.valueChanges.subscribe((isSame: boolean) => {
-      (isSame) ? this.ActualAddressFormGroup.patchValue(this.LegalAddressFormGroup.value) : this.ActualAddressFormGroup.reset();
-    });
+    this.onDisableIsSameAddressControlInit();
 
     this.provider && this.activateEditMode();
   }
 
   activateEditMode(): void {
     this.LegalAddressFormGroup.patchValue(this.provider.legalAddress, { emitEvent: false });
+    this.isSameAddressControl.setValue(!Boolean(this.provider.actualAddress));
+    this.provider.actualAddress && this.ActualAddressFormGroup.patchValue(this.provider.actualAddress, { emitEvent: false });
+  }
 
-    if (this.provider?.actualAddress) {
-      this.ActualAddressFormGroup.patchValue(this.provider.actualAddress, { emitEvent: false });
-    } else {
-      this.ActualAddressFormGroup.patchValue(this.provider.legalAddress, { emitEvent: false });
-    }
-
+  /**
+   * This method makes input enable if radiobutton value is true and sets the value to teh formgroup
+   */
+  onDisableIsSameAddressControlInit(): void {
+    this.isSameAddressControl.valueChanges.subscribe((isSame: boolean) => {
+      this.ActualAddressFormGroup.reset();
+      if (isSame) {
+        this.ActualAddressFormGroup.disable();
+        this.ActualAddressFormGroup.clearValidators();
+      } else {
+        this.ActualAddressFormGroup.enable();
+        this.ActualAddressFormGroup.setValidators([Validators.required, Validators.pattern(TEXT_REGEX)]);
+      }
+    });
   }
 }
