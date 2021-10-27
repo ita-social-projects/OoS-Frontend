ba65a1a483e7e7eb47aee50dad4e67fea07fbc0e
Grygorii Makieiev <gmakan55@gmail.com>
2021-10-27 11:34:10 +0300

    Task 578. Full name of children is shown into tooltip. 'Скасувати' button has been added to user-config-edit.component.

--- src/app/shared/components/child-card/child-card.component.ts
+++ src/app/shared/components/child-card/child-card.component.ts
@@ -9,6 +9,9 @@ import { Child } from '../../models/child.model';
 })
 export class ChildCardComponent implements OnInit {
 
+  public below = 'below';
+  public childFullName: string;
+
   @Input() child: Child;
   @Input() applications: Array<Application>;
   @Output() deleteChild = new EventEmitter<Child>();
@@ -17,6 +20,10 @@ export class ChildCardComponent implements OnInit {
 
   onDelete(): void {
     this.deleteChild.emit(this.child);
+  }
+
+  getChildFullName(id: number): string {
+    return this.childFullName = `${this.child.lastName} ${this.child.firstName} ${this.child.middleName}`;
   }
 
 }
