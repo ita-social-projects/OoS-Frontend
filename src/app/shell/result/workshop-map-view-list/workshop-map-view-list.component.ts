03340448d4f0d2288fe0c5d7c2b81693c87f7c15
roman <roman.divonchuk@gmail.com>
2021-10-28 10:33:44 +0300

    add buttons

--- src/app/shell/result/workshop-map-view-list/workshop-map-view-list.component.ts
+++ src/app/shell/result/workshop-map-view-list/workshop-map-view-list.component.ts
@@ -48,26 +48,38 @@ export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
   private swipeCoord?: [number, number];
   private swipeTime?: number;
   public currentWorkShopIndex = 0;
+  public selectedWorkshopsLength = this.selectedWorkshops.length;
+
 
   swipe(e: TouchEvent, when: string): void {
     const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
-    const time = new Date().getTime();      if (when === 'start') {       this.swipeCoord = coord;
-    this.swipeTime = time;
-  } else if (when === 'end') {
-    const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
-    const duration = time - this.swipeTime;
-    if (duration < 1000 && Math.abs(direction[0]) > 30 && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
-      const swipe = direction[0] < 0 ? 'next' : 'previous';
+    const time = new Date().getTime();
+    if (when === 'start') {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === 'end') {
+      const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
+      const duration = time - this.swipeTime;
+      if (duration < 1000 && Math.abs(direction[0]) > 30 && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
+        const swipe = direction[0] < 0 ? 'next' : 'previous';
         if (swipe === 'next') {
           (this.selectedWorkshops.length-1) > this.currentWorkShopIndex && this.currentWorkShopIndex++
         } else {
           this.currentWorkShopIndex >= 1 && this.currentWorkShopIndex--
         }
-      console.log('SWIPE',swipe)
+        console.log('SWIPE',swipe)
     }
     }
-
   }
+
+  onClickScroll(text) {
+    if (text === 'next') {
+      this.currentWorkShopIndex++
+    } else {
+      this.currentWorkShopIndex--
+    }
+  }
+
   ngOnInit(): void {
     this.filteredWorkshops$
       .pipe(takeUntil(this.destroy$), filter((filteredWorkshops)=> !!filteredWorkshops))
@@ -103,6 +115,7 @@ export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
     else {
       this.selectedWorkshops = [];
     }
+    this.selectedWorkshopsLength = this.selectedWorkshops.length;
   }
 
   public fadeAnimationDone(): void {
