63e95848ec83133d625c43f5c53c580ed383c3e7
roman <roman.divonchuk@gmail.com>
2021-11-01 18:45:12 +0200

    a

--- src/app/shell/result/workshop-map-view-list/workshop-map-view-list.component.ts
+++ src/app/shell/result/workshop-map-view-list/workshop-map-view-list.component.ts
@@ -1,5 +1,5 @@
-import { animate, style, transition, trigger } from '@angular/animations';
-import { Component, ElementRef, Input, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, ElementRef, Input, OnInit, ViewChild, OnDestroy, Renderer2 } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
@@ -20,12 +20,16 @@ import { RegistrationState } from 'src/app/shared/store/registration.state';
         style({ bottom: '-25px', opacity: 0 }),
         animate('200ms ease-in-out')
       ])
-    ])
+    ]),
+    trigger('triggerName', [
+      transition(':decrement', [animate('0.5s',style({ transform: "translateX(+92vw)"}))]),
+      transition(':increment', [animate('0.5s',style({ transform: "translateX(-92vw)" }) )]),
+    ]),
   ]
 })
 export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
 
-  constructor(private store: Store) { }
+  constructor(private store: Store, private renderer: Renderer2) { }
   destroy$: Subject<boolean> = new Subject<boolean>();
   @Input() public filteredWorkshops$: Observable<WorkshopFilterCard>;
   @Input() resetFilter$: Observable<void>;
@@ -47,11 +51,13 @@ export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
 
   private swipeCoord?: [number, number];
   private swipeTime?: number;
-  public currentWorkShopIndex = 0;
+  public currentWorkShopIndex: number = 0;
   public selectedWorkshopsLength = this.selectedWorkshops.length;
-
-
+  public direct: string
+  public left: number = 0
   swipe(e: TouchEvent, when: string): void {
+
+
     const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
     const time = new Date().getTime();
     if (when === 'start') {
@@ -61,24 +67,28 @@ export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
       const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
       const duration = time - this.swipeTime;
       if (duration < 1000 && Math.abs(direction[0]) > 30 && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
+
         const swipe = direction[0] < 0 ? 'next' : 'previous';
+        this.direct = swipe
+
         if (swipe === 'next') {
           (this.selectedWorkshops.length-1) > this.currentWorkShopIndex && this.currentWorkShopIndex++
         } else {
           this.currentWorkShopIndex >= 1 && this.currentWorkShopIndex--
         }
-        console.log('SWIPE',swipe)
     }
     }
   }
 
-  onClickScroll(text) {
-    if (text === 'next') {
-      this.currentWorkShopIndex++
-    } else {
-      this.currentWorkShopIndex--
+  triggerNameDone(e) {
+    if (this.direct === 'next' && this.selectedWorkshops.length > 1 ) {
+      this.left =parseInt(this.curSelectedWorkshop.nativeElement.style.left) - 92
+    }
+    if (this.direct === 'previous' && this.selectedWorkshops.length > 1) {
+      this.left =parseInt(this.curSelectedWorkshop.nativeElement.style.left) + 92
     }
   }
+
 
   ngOnInit(): void {
     this.filteredWorkshops$
@@ -99,12 +109,17 @@ export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
   }
 
   onSelectedAddress(address: Address): void {
-    this.currentWorkShopIndex = 0;
     this.isSelectedMarker = Boolean(address);
+    this.left = 0;
+    this.currentWorkShopIndex = 0;
+    this.direct = null
 
     if (this.isSelectedMarker) {
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> db3517c2 (add amimation and scroll workshoplist, when active halfmap)
       this.selectedWorkshops = this.workshops.filter((workshop: WorkshopCard) =>
         address.city === workshop.address.city &&
         address.street === workshop.address.street &&
@@ -116,6 +131,7 @@ export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
     else {
       this.selectedWorkshops = [];
     }
+
     this.selectedWorkshopsLength = this.selectedWorkshops.length;
   }
 
