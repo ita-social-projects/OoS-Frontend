7e93b94769c0c6575d5e5e6beb126e70505afd96
romandivonchuk <roman.divonchuk@gmail.com>
2021-11-02 17:09:59 +0200

    resolve conversetion

--- src/app/shell/result/workshop-map-view-list/workshop-map-view-list.component.ts
+++ src/app/shell/result/workshop-map-view-list/workshop-map-view-list.component.ts
@@ -23,8 +23,8 @@ import { RegistrationState } from 'src/app/shared/store/registration.state';
       ])
     ]),
     trigger('triggerName', [
-      transition(':decrement', [animate('0.4s ease-in-out',style({ transform: "translateX(+92vw)"}))]),
-      transition(':increment', [animate('0.4s ease-in-out',style({ transform: "translateX(-92vw)" }) )]),
+      transition(':decrement', [animate('0.2s ease-in-out',style({ transform: "translateX(+92vw)"}))]),
+      transition(':increment', [animate('0.2s ease-in-out',style({ transform: "translateX(-92vw)" }) )]),
     ]),
   ]
 })
@@ -55,21 +55,23 @@ export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
   private swipeCoord?: [number, number];
   private swipeTime?: number;
   public currentWorkShopIndex: number = 0;
-  public selectedWorkshopsLength = this.selectedWorkshops.length;
   public direct: string
   public left: number = 0
   swipe(e: TouchEvent, when: string): void {
-
-
     const coord: [number, number] = [e.changedTouches[0].clientX, e.changedTouches[0].clientY];
-    const time = new Date().getTime();
+    let time = new Date().getTime();
+
+    if (when === 'start' && (time - this.swipeTime) < 300 ) {
+      time -= 1000
+    }
+
     if (when === 'start') {
       this.swipeCoord = coord;
       this.swipeTime = time;
     } else if (when === 'end') {
       const direction = [coord[0] - this.swipeCoord[0], coord[1] - this.swipeCoord[1]];
       const duration = time - this.swipeTime;
-      if (duration < 1000 && Math.abs(direction[0]) > 30 && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
+      if ( duration < 1000 && Math.abs(direction[0]) > 30 && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
         const swipe = direction[0] < 0 ? 'next' : 'previous';
         this.direct = swipe
         if (swipe === 'next') {
@@ -127,8 +129,6 @@ export class WorkshopMapViewListComponent implements OnInit, OnDestroy {
     else {
       this.selectedWorkshops = [];
     }
-
-    this.selectedWorkshopsLength = this.selectedWorkshops.length;
   }
 
   public fadeAnimationDone(): void {
