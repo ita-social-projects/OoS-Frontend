ac24bdbe3de9b99e48834111dc250472fd4abe69
Denys Ivashchenko <deivas@softserveinc.com>
2021-11-10 11:30:25 +0200

    fixed coment

--- src/app/shell/personal-cabinet/user-config/user-config.component.ts
+++ src/app/shell/personal-cabinet/user-config/user-config.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/shared/models/user.model';
@@ -10,8 +10,8 @@ import { environment } from 'src/environments/environment';
   templateUrl: './user-config.component.html',
   styleUrls: ['./user-config.component.scss']
 })
-export class UserConfigComponent implements OnInit {
-  public culture: string;
+export class UserConfigComponent {
+  public culture: string = localStorage.getItem('ui-culture');;
 
   @Select(RegistrationState.user)
   user$: Observable<User>;
@@ -19,10 +19,6 @@ export class UserConfigComponent implements OnInit {
   link: string;
 
   constructor() { }
-
-  ngOnInit(): void {
-    this.culture = localStorage.getItem('ui-culture'); 
-  }
 
   onRedirect(link: string): void {
     window.open(`${this.authServer + link}?culture=${this.culture}&ui-culture=${this.culture}`, link, 'height=500,width=500');
