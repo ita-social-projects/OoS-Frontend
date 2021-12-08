184646e9e2aecd2905aae5f490c0f94ed4e82f70
Tetiana Skazhenyk <tianati01@gmail.com>
2021-12-08 16:32:03 +0200

    added feature handling

---
+++ src/app/shared/services/feature-management/feature-management.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FeatureManagementService } from './feature-management.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('FeatureManagementService', () => {
+  let service: FeatureManagementService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(FeatureManagementService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ src/app/shared/services/feature-management/feature-management.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FeaturesList } from '../../models/featuresList.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FeatureManagementService {
+
+  constructor(private http: HttpClient) { }
+
+    /**
+   * This method get features flags depending on releases
+   */
+     getFeaturesList(): Observable<FeaturesList> {
+      return this.http.get<FeaturesList>('/api/v1/FeatureManagement/Get');
+    }
+
+}
