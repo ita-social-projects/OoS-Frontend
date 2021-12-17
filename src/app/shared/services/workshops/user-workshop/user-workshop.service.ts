a1546d378568d4f8d748c156691fe78f5e28f466
Sasha <alitvinets7@gmail.com>
2021-12-17 19:38:38 +0200

    Update user-workshop.service.ts

--- src/app/shared/services/workshops/user-workshop/user-workshop.service.ts
+++ src/app/shared/services/workshops/user-workshop/user-workshop.service.ts
@@ -35,7 +35,7 @@ export class UserWorkshopService {
   createWorkshop(workshop: Workshop): Observable<object> {
     const formData = new FormData();
     Object.keys(workshop).forEach((key: string) => {
-      formData.append(key, JSON.stringify(workshop.[key]));
+      formData.append(key, JSON.stringify(workshop[key]));
     });
 
     return this.http.post('/api/v2/Workshop/Create', formData)
