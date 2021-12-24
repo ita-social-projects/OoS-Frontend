162c571e9707ea068091e8b9b220c0ea706571ee
Sasha <alitvinets7@gmail.com>
2021-12-24 11:52:26 +0200

    Update images.service.ts

--- src/app/shared/services/images/images.service.ts
+++ src/app/shared/services/images/images.service.ts
@@ -8,5 +8,13 @@ export class ImagesService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * This method get workshops by Provider id
+   * @param id: string
+   */
+  getWorkshopsByProviderId(id: string): Observable<any[]> {
+    return this.http.get<any[]>(`/api/v1/PublicImage/${id}`);
+  }
+
 
 }
