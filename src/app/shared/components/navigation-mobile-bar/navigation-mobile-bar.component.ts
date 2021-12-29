a5701fe09afe131fa5d74eba4dcd4c9e26e61e1f
Denys Ivashchenko <deivas@softserveinc.com>
2021-12-29 15:29:44 +0200

    fixed error NG0100

--- src/app/shared/components/navigation-mobile-bar/navigation-mobile-bar.component.ts
+++ src/app/shared/components/navigation-mobile-bar/navigation-mobile-bar.component.ts
@@ -1,23 +1,37 @@
 import { Navigation } from './../../models/navigation.model';
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Select } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { NavigationState } from '../../store/navigation.state';
 import { Location } from '@angular/common';
+import { delay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation-mobile-bar',
   templateUrl: './navigation-mobile-bar.component.html',
   styleUrls: ['./navigation-mobile-bar.component.scss']
 })
-export class NavigationMobileBarComponent {
+export class NavigationMobileBarComponent implements OnInit, OnDestroy {
 
   @Select(NavigationState.navigationPathsMobile)
   navigationPathsMobile$: Observable<Navigation[]>;
+  navigationPathsMobile: Navigation[] = [];
+
+  private navigation: Subscription;
+
   constructor(private location: Location) { }
 
   goBack(): void {
     this.location.back();
   }
 
+  ngOnInit(): void {
+    this.navigation = this.navigationPathsMobile$
+    .pipe(delay(0))
+    .subscribe((navigation) => this.navigationPathsMobile = navigation)
+  }
+  
+  ngOnDestroy(): void {
+     this.navigation.unsubscribe(); 
+  }
 }
