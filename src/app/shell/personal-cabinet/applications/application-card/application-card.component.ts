b7a510be1253624663ac3b3c70e354595d8b3bdd
perepichai <o.perepichai@gmail.com>
2022-01-12 22:14:40 +0200

    feat: update onReject method

--- src/app/shell/personal-cabinet/applications/application-card/application-card.component.ts
+++ src/app/shell/personal-cabinet/applications/application-card/application-card.component.ts
@@ -6,11 +6,23 @@ import { ApplicationTitles, ApplicationStatusDescription } from 'src/app/shared/
 import { Role } from 'src/app/shared/enum/role';
 import { Application } from 'src/app/shared/models/application.model';
 import { Util } from 'src/app/shared/utils/utils';
+
+
+import { MatDialog } from '@angular/material/dialog';
+// import { ConfirmationModalWindowComponent } from 'src/app/shared/components/confirmation-modal-window/confirmation-modal-window.component';
+import { RejectModalWindowComponent } from 'src/app/shared/components/reject-modal-window/reject-modal-window.component';
+// import { ModalConfirmationType } from 'src/app/shared/enum/modal-confirmation';
+// import { Workshop } from 'src/app/shared/models/workshop.model';
+import { Select, Store } from '@ngxs/store';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
 @Component({
   selector: 'app-application-card',
   templateUrl: './application-card.component.html',
   styleUrls: ['./application-card.component.scss']
 })
+
 
 export class ApplicationCardComponent implements OnInit {
 
@@ -23,6 +35,8 @@ export class ApplicationCardComponent implements OnInit {
   childAge: string;
   deviceToogle: boolean;
   infoShowToggle: boolean = false;
+  ReasonFormGroup: FormGroup;
+  reason: string;
 
   @Input() application: Application;
   @Input() userRole: string;
@@ -32,7 +46,11 @@ export class ApplicationCardComponent implements OnInit {
   @Output() infoShow = new EventEmitter();
   @Output() infoHide = new EventEmitter();
 
-  constructor(private detectedDevice: DetectedDeviceService) { }
+  constructor(
+    private detectedDevice: DetectedDeviceService,
+    private store: Store,
+    private matDialog: MatDialog
+    ) {}
 
   ngOnInit(): void {
     this.childAge = Util.getChildAge(this.application.child);
@@ -58,9 +76,9 @@ export class ApplicationCardComponent implements OnInit {
    * This method emit on deny action
    * @param Application application
    */
-  onReject(application: Application): void {
-    this.rejected.emit(application);
-  }
+  // onReject(application: Application): void {
+  //   this.rejected.emit(application);
+  // }
 
   /**
    * This method emit on deny action
@@ -94,5 +112,30 @@ export class ApplicationCardComponent implements OnInit {
     this.infoHide.emit();
     this.deviceToogle && document.removeEventListener('click', this.onClick.bind(this));
   }
+
+    /**
+   * This method receives a form from reject-modal-window child component and assigns to the Reason
+   * @param FormGroup form
+   */
+    //  onReceiveReasonFormGroup(form: FormGroup): void {
+    //   this.ReasonFormGroup = form;
+    // }
+
+    /**
+   * This method reject Application
+   */
+     onReject(application: Application): void {
+      const dialogRef = this.matDialog.open(RejectModalWindowComponent, {
+        width: '330px',
+        data: {
+          test: this.reason
+        }
+      });
+  
+      dialogRef.afterClosed().subscribe((result: string) => {        
+          application.reason = result;
+          this.rejected.emit(application);        
+      });
+    }
 
 }
