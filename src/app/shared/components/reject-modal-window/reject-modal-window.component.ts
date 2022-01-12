296f439a123904cbbde779b86607b26cd31fc9aa
perepichai <o.perepichai@gmail.com>
2022-01-12 22:13:50 +0200

    feat: add textarea to reject modal component

--- src/app/shared/components/reject-modal-window/reject-modal-window.component.ts
+++ src/app/shared/components/reject-modal-window/reject-modal-window.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { Constants } from 'src/app/shared/constants/constants';
+import { Application } from 'src/app/shared/models/application.model';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngxs/store';
+
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+export interface DialogData {
+  test: string
+}
 
 @Component({
   selector: 'app-reject-modal-window',
@@ -6,14 +16,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reject-modal-window.component.scss']
 })
 export class RejectModalWindowComponent implements OnInit {
+
+  readonly constants: typeof Constants = Constants;
+
   modalTitle: string;
   modalDescription: string
 
-  constructor() { }
+  ReasonFormGroup: FormGroup;
+
+  @Input() application: Application;
+  @Output() passReasonFormGroup = new EventEmitter();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private store: Store,
+    public dialogRef: MatDialogRef<RejectModalWindowComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    this.ReasonFormGroup = this.formBuilder.group({
+      description: new FormControl('', Validators.required)
+    });
+   }
 
   ngOnInit(): void {
-    this.modalTitle = 'Відмінити',
-    this.modalDescription = 'тут поле для вводу причини'
+    this.modalTitle = 'ВІДМОВИТИ',
+    this.modalDescription = 'Ви впевнені, що хочете перевести заяву у статус ”Відмовлено”?'
+    this.passReasonFormGroup.emit(this.ReasonFormGroup);
+  }
+
+  onCancel(): void {
+    this.dialogRef.close();
   }
 
 }
