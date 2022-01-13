b9266d6f2fe514671e257a4373cf9bcc11c91760
perepichai <o.perepichai@gmail.com>
2022-01-13 21:46:38 +0200

    feat: small component updates

--- src/app/shared/components/reject-modal-window/reject-modal-window.component.ts
+++ src/app/shared/components/reject-modal-window/reject-modal-window.component.ts
@@ -1,10 +1,9 @@
-import { Component, Inject, EventEmitter, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Constants } from 'src/app/shared/constants/constants';
 import { Application } from 'src/app/shared/models/application.model';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Store } from '@ngxs/store';
 
-import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
 
 export interface DialogData {
   test: string
@@ -25,13 +24,10 @@ export class RejectModalWindowComponent implements OnInit {
   ReasonFormGroup: FormGroup;
 
   @Input() application: Application;
-  @Output() passReasonFormGroup = new EventEmitter();
 
   constructor(
     private formBuilder: FormBuilder,
-    private store: Store,
-    public dialogRef: MatDialogRef<RejectModalWindowComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+    public dialogRef: MatDialogRef<RejectModalWindowComponent>) {
     this.ReasonFormGroup = this.formBuilder.group({
       description: new FormControl('', Validators.required)
     });
@@ -40,7 +36,6 @@ export class RejectModalWindowComponent implements OnInit {
   ngOnInit(): void {
     this.modalTitle = 'ВІДМОВИТИ',
     this.modalDescription = 'Ви впевнені, що хочете перевести заяву у статус ”Відмовлено”?'
-    this.passReasonFormGroup.emit(this.ReasonFormGroup);
   }
 
   onCancel(): void {
