5d74bdfa156755b838efd9ca44ec6e9333def0e0
perepichai <o.perepichai@gmail.com>
2022-01-13 21:47:09 +0200

    feat: fixing test

--- src/app/shared/components/reject-modal-window/reject-modal-window.component.spec.ts
+++ src/app/shared/components/reject-modal-window/reject-modal-window.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RejectModalWindowComponent } from './reject-modal-window.component';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Component, Input } from '@angular/core';
 
 describe('RejectModalWindowComponent', () => {
   let component: RejectModalWindowComponent;
@@ -8,7 +13,17 @@ describe('RejectModalWindowComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RejectModalWindowComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatInputModule
+      ],
+      declarations: [ 
+        RejectModalWindowComponent,
+        MockValidationHintForInputComponent
+       ]
     })
     .compileComponents();
   });
@@ -23,3 +38,18 @@ describe('RejectModalWindowComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  selector: 'app-validation-hint-for-input',
+  template: ''
+})
+
+class MockValidationHintForInputComponent {
+  @Input() type: string;
+  @Input() invalid: boolean;
+  @Input() isEmailCheck: boolean;
+  @Input() isEmptyCheck: boolean;
+  @Input() minLength: boolean;
+  @Input() minCharachters: number;
+  @Input() forbiddenCharacter: string;
+}
