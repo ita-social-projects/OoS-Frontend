39f10d1beb43b0d4da18d16692db6dc7b65b8605
LeraKornienko <Makarenkolera460@gmail.com>
2022-01-19 16:44:58 +0200

    age done

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -22,7 +22,11 @@ export class Util {
       age--;
     }
 
-    return age ? age.toString() + ' ' + this.getDeclensionYear(age) : month.toString() + ' ' + this.getDeclensionMonth(month);
+    if (age < 1 ) {
+      age = "< 1";
+    }
+
+    return age.toString() + ' ' + this.getDeclensionYear(age);
   }
 
   /**
@@ -33,18 +37,7 @@ export class Util {
   public static getDeclensionYear(year: number): string {
     let ageString;
     let lastDigit = year % 10;
-    (lastDigit === 1 && year !== 11) ? ageString = 'рік' : (lastDigit > 1 && lastDigit < 5) ? ageString = 'роки' : ageString = 'років';
-    return ageString;
-  }
-
-  /**
-   * This method returns declension for Child's age in months
-   * @param month Child's age in months
-   * @returns string
-   */
-  public static getDeclensionMonth(month: number): string {
-    let ageString;
-    (month === 1) ? ageString = 'місяць' : (month > 1 && month < 5) ? ageString = 'місяці' : ageString = 'місяців';
+    (lastDigit === 1 && year !== 11) ? ageString = 'рік' : (year == "< 1") ? ageString = 'року' : (lastDigit > 1 && lastDigit < 5) ? ageString = 'роки' : ageString = 'років';
     return ageString;
   }
 }
