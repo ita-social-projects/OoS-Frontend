bb77aea04d8296ed19368b70626da9c2df3fa376
Olya <ola.shy@yahoo.com>
2022-01-21 14:43:13 +0200

    fix

--- src/app/shared/store/meta-data.state.ts
+++ src/app/shared/store/meta-data.state.ts
@@ -239,7 +239,7 @@ export class MetaDataState {
       .getFeaturesList()
       .pipe(
         tap((featuresList: FeaturesList) => 
-        patchState(environment.production ? {featuresList: featuresList} :
+        patchState(environment.production ? { featuresList: featuresList } :
         { featuresList: { release1: true, release2: true, release3: false } })
         ))
   }
