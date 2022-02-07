98bfbd6499b1795515d95e3bc016e77935d0b8e6
perepichai <o.perepichai@gmail.com>
2022-02-07 15:30:45 +0200

    feat: add DATE_REGEX constant

--- src/app/shared/constants/regex-constants.ts
+++ src/app/shared/constants/regex-constants.ts
@@ -4,3 +4,4 @@
 
 export const TEXT_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁё'’\s-]*$/;
 export const TEXT_WITH_DIGITS_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁё'’.,\s\d\/-]*$/;
+export const DATE_REGEX: RegExp = /[^0-9./-]*/g;
