ce2398df8045de9f6241ef538f4bcf3ebb993524
Kokole <kokole110@gmail.com>
2022-02-11 10:30:31 +0200

    add input validation for birth cert number

--- src/app/shared/constants/regex-constants.ts
+++ src/app/shared/constants/regex-constants.ts
@@ -6,3 +6,4 @@ export const TEXT_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁё'’\s-]*$/;
 export const TEXT_WITH_DIGITS_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁё'’.,\s\d\/-]*$/;
 export const DATE_REGEX: RegExp = /[^0-9./-]*/g;
 export const NAME_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁё'’-]*$/;
+export const BIRTH_CERTIFICATE_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁё'’\s\d-№]*$/;
