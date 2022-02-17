af82cae1c3cbe60d34a8caf4933a7f4d0d13005e
perepichai <o.perepichai@gmail.com>
2022-02-17 13:47:33 +0200

    fix test

--- src/app/shared/services/provider-admins/provider-admin.service.spec.ts
+++ src/app/shared/services/provider-admins/provider-admin.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ProviderAdminService } from './provider-admin.service';
@@ -6,7 +7,9 @@ describe('ProviderAdminService', () => {
   let service: ProviderAdminService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ProviderAdminService);
   });
 
