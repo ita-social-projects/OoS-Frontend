c1bca2bf66e0b8e328f77c7d3faef1e42f46ff23
Aleksandr Perepichai <o.perepichai@gmail.com>
2022-02-17 22:13:39 +0200

    Service provider/get providerAdmins from the backend (#943)
    * feat: add service provider-admin
    
    * feat: add GetAllProviderAdmins
    
    * feat: add GetAllProviderAdmins
    
    * update columns name
    
    * update columns name
    
    * enum for the labels
    
    * add enum for the provider admin role
    
    * add new interface for the updated provider admins
    
    * add updateStructureForTheTable method  to updating data
    
    * fix indentantions
    
    * fix test

--- src/app/shell/personal-cabinet/users/users.component.ts
+++ src/app/shell/personal-cabinet/users/users.component.ts
@@ -1,23 +1,14 @@
-
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatTabChangeEvent } from '@angular/material/tabs/tab-group';
-import { Subject } from 'rxjs';
+import { Select, Store } from '@ngxs/store';
+import { Observable, Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { providerAdminRole } from 'src/app/shared/enum/enumUA/provider-admin';
+import { ProviderAdmin, ProviderAdminTable } from 'src/app/shared/models/providerAdmin.model';
+import { GetAllProviderAdmins } from 'src/app/shared/store/user.actions';
+import { UserState } from 'src/app/shared/store/user.state';
 
-export interface ProviderAdmins {
-  name: string;
-  email: string;
-  phone: string;
-  role: string;
-}
-
-const DATA_MOCK: ProviderAdmins[] = [
-  {name: 'Тестян Тест Тестович', email: 'test@gmail.com', phone: '1234567890', role: 'Адміністратор'}, 
-  {name: 'Тестян3 Тест Тестович', email: 'test3@gmail.com', phone: '1234567890', role: 'Адміністратор'},
-  {name: 'Тестян4 Тест Тестович', email: 'test4@gmail.com', phone: '1234567890', role: 'Заступник'},
-  {name: 'Тестян5 Тест Тестович', email: 'test5@gmail.com', phone: '1234567890', role: 'Адміністратор'},
-];
 
 @Component({
   selector: 'app-users',
@@ -26,27 +17,53 @@ const DATA_MOCK: ProviderAdmins[] = [
 })
 export class UsersComponent implements OnInit, OnDestroy {
 
-  providerAdmins = DATA_MOCK;
+  readonly providerAdminRole = providerAdminRole;
+
+  @Select(UserState.providerAdmins)
+  providerAdmins$: Observable<ProviderAdmin[]>;
+  providerAdmins: ProviderAdminTable[];
   filterProviderAdmins: Array<object> = [];
   filter = new FormControl('');
   filterValue: string;
   destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() {}
+  constructor(public store: Store) {}
 
   ngOnInit(): void {
     this.filter.valueChanges
-      .pipe(
-        takeUntil(this.destroy$),
-        debounceTime(200),
-        distinctUntilChanged()
-      ).subscribe((val) => {
+      .pipe(takeUntil(this.destroy$), debounceTime(200), distinctUntilChanged())
+      .subscribe((val) => {
         if (val) {
           this.filterValue = val;
         } else {
           this.filterValue = '';
         }
       });
+    this.getAllProviderAdmins();
+
+    this.providerAdmins$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((providerAdmins: ProviderAdmin[]) => {
+        this.providerAdmins = this.updateStructureForTheTable(providerAdmins);
+      });
+  }
+
+  getAllProviderAdmins(): void {
+    this.store.dispatch(new GetAllProviderAdmins());
+  }
+
+  updateStructureForTheTable(admins: ProviderAdmin[]): ProviderAdminTable[] {
+    let updatedAdmins = [];
+    admins.forEach((admin) => {
+      updatedAdmins.push({
+        pib: `${admin.lastName} ${admin.firstName} ${admin.middleName}`,
+        email: admin.email,
+        phone: admin.phone,
+        deputy: (admin.isDeputy) ? providerAdminRole.isDeputy : providerAdminRole.isNotDeputy
+      });
+      
+    });
+    return updatedAdmins;
   }
 
   /**
@@ -54,7 +71,9 @@ export class UsersComponent implements OnInit, OnDestroy {
    * @param event: MatTabChangeEvent
    */
   onTabChange(event: MatTabChangeEvent): void {
-    this.filterProviderAdmins = this.providerAdmins.filter(user => user.role === event.tab.textLabel);
+    this.filterProviderAdmins = this.providerAdmins.filter(
+      (user) => user.deputy === event.tab.textLabel
+    );
     this.filter.reset();
   }
 
