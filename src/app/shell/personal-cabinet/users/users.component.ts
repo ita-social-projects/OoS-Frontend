f2e7a58f307052f8fa6e91557fad85276f875b12
perepichai <o.perepichai@gmail.com>
2022-02-17 13:34:54 +0200

    add updateStructureForTheTable method  to updating data

--- src/app/shell/personal-cabinet/users/users.component.ts
+++ src/app/shell/personal-cabinet/users/users.component.ts
@@ -1,26 +1,14 @@
-
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatTabChangeEvent } from '@angular/material/tabs/tab-group';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subject } from 'rxjs';
 import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { providerAdminRole } from 'src/app/shared/enum/enumUA/provider-admin';
+import { ProviderAdmin, ProviderAdminTable } from 'src/app/shared/models/providerAdmin.model';
 import { GetAllProviderAdmins } from 'src/app/shared/store/user.actions';
 import { UserState } from 'src/app/shared/store/user.state';
 
-export interface ProviderAdmins {
-  name: string;
-  email: string;
-  phone: string;
-  role: string;
-}
-
-const DATA_MOCK: ProviderAdmins[] = [
-  {name: 'Тестян Тест Тестович', email: 'test@gmail.com', phone: '1234567890', role: 'Адміністратор'}, 
-  {name: 'Тестян3 Тест Тестович', email: 'test3@gmail.com', phone: '1234567890', role: 'Адміністратор'},
-  {name: 'Тестян4 Тест Тестович', email: 'test4@gmail.com', phone: '1234567890', role: 'Заступник'},
-  {name: 'Тестян5 Тест Тестович', email: 'test5@gmail.com', phone: '1234567890', role: 'Адміністратор'},
-];
 
 @Component({
   selector: 'app-users',
@@ -29,11 +17,11 @@ const DATA_MOCK: ProviderAdmins[] = [
 })
 export class UsersComponent implements OnInit, OnDestroy {
 
-  @Select(UserState.providerAdmins)
-  providerAdmins$: Observable<ProviderAdmins[]>;
-  testAdmins: ProviderAdmins[];
+  readonly providerAdminRole = providerAdminRole;
 
-  providerAdmins = DATA_MOCK;
+  @Select(UserState.providerAdmins)
+  providerAdmins$: Observable<ProviderAdmin[]>;
+  providerAdmins: ProviderAdminTable[];
   filterProviderAdmins: Array<object> = [];
   filter = new FormControl('');
   filterValue: string;
@@ -43,11 +31,8 @@ export class UsersComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.filter.valueChanges
-      .pipe(
-        takeUntil(this.destroy$),
-        debounceTime(200),
-        distinctUntilChanged()
-      ).subscribe((val) => {
+      .pipe(takeUntil(this.destroy$), debounceTime(200), distinctUntilChanged())
+      .subscribe((val) => {
         if (val) {
           this.filterValue = val;
         } else {
@@ -55,24 +40,41 @@ export class UsersComponent implements OnInit, OnDestroy {
         }
       });
     this.getAllProviderAdmins();
-    
-    this.providerAdmins$.pipe(
-      takeUntil(this.destroy$)
-    ).subscribe((providerAdmins: ProviderAdmins[]) => this.testAdmins = providerAdmins);
+
+    this.providerAdmins$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((providerAdmins: ProviderAdmin[]) => {
+        this.providerAdmins = this.updateStructureForTheTable(providerAdmins);
+      });
   }
 
   getAllProviderAdmins(): void {
     this.store.dispatch(new GetAllProviderAdmins());
   }
-  
+
+  updateStructureForTheTable(admins: ProviderAdmin[]): ProviderAdminTable[] {
+    let updatedAdmins = [];
+    admins.forEach((admin) => {
+      updatedAdmins.push({
+        pib: `${admin.lastName} ${admin.firstName} ${admin.middleName}`,
+        email: admin.email,
+        phone: admin.phone,
+        deputy: (admin.isDeputy) ? providerAdminRole.isDeputy : providerAdminRole.isNotDeputy
+      });
+      
+    });
+    return updatedAdmins;
+  }
+
   /**
    * This method filter users according to selected tab
    * @param event: MatTabChangeEvent
    */
   onTabChange(event: MatTabChangeEvent): void {
-    this.filterProviderAdmins = this.providerAdmins.filter(user => user.role === event.tab.textLabel);
+    this.filterProviderAdmins = this.providerAdmins.filter(
+      (user) => user.deputy === event.tab.textLabel
+    );
     this.filter.reset();
-    console.log(this.testAdmins[0]);
   }
 
   ngOnDestroy(): void {
