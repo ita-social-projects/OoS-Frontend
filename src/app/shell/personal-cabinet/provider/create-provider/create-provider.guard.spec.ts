0d191c91a828c5256142a043f098c13d7478006d
Olya <ola.shy@yahoo.com>
2022-02-18 16:35:14 +0200

    fix

--- src/app/shell/personal-cabinet/provider/create-provider/create-provider.guard.spec.ts
+++ src/app/shell/personal-cabinet/provider/create-provider/create-provider.guard.spec.ts
@@ -1,26 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
 import { NgxsModule } from '@ngxs/store';
-import { CreateInfoFormComponent } from './create-info-form/create-info-form.component';
 import { CreateProviderGuard } from './create-provider.guard';
 
 describe('CreateProviderGuard', () => {
   let guard: CreateProviderGuard;
-  let component: CreateInfoFormComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
         NgxsModule.forRoot([]),
       ],
-      declarations: [
-        FormsModule
-      ]
     });
     guard = TestBed.inject(CreateProviderGuard);
   });
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
+    expect(guard).toBeTruthy();
   });
 });
