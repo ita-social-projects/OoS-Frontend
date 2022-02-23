1a3e809c6e15dd635ecfa4c14a803d59466d2ec1
perepichai <o.perepichai@gmail.com>
2022-02-23 11:22:34 +0200

    feat: add createProviderAdmin method

--- src/app/shared/services/provider-admins/provider-admin.service.ts
+++ src/app/shared/services/provider-admins/provider-admin.service.ts
@@ -1,21 +1,21 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ProviderAdmin } from '../../models/providerAdmin.model';
+import { ProviderAdmin, ProviderAdminBackend } from '../../models/providerAdmin.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProviderAdminService {
-
   constructor(private http: HttpClient) { }
 
   /**
    * This method get all provider Admins
    */
-  getAllProviderAdmins(): Observable<ProviderAdmin[]> {
-
-    return this.http.get<ProviderAdmin[]>(`/api/v1/ProviderAdmin/GetRelatedProviderAdmins`);
+  getAllProviderAdmins(): Observable<ProviderAdminBackend[]> {
+    return this.http.get<ProviderAdminBackend[]>(
+      `/api/v1/ProviderAdmin/GetRelatedProviderAdmins`
+    );
   }
 
   /**
@@ -23,11 +23,25 @@ export class ProviderAdminService {
    * @param deputyOnly: boolean
    * @param assistantsOnly: boolean
    */
-  getFilteredProviderAdmins(deputyOnly: boolean, assistantsOnly: boolean): Observable<ProviderAdmin> {
+  getFilteredProviderAdmins(
+    deputyOnly: boolean,
+    assistantsOnly: boolean
+  ): Observable<ProviderAdminBackend> {
     let params = new HttpParams();
     params = params.set('deputyOnly', `${deputyOnly}`);
     params = params.set('assistantsOnly', `${assistantsOnly}`);
 
-    return this.http.get<ProviderAdmin>(`/api/v1/ProviderAdmin/GetFilteredProviderAdmins`, { params });
+    return this.http.get<ProviderAdminBackend>(
+      `/api/v1/ProviderAdmin/GetFilteredProviderAdmins`,
+      { params }
+    );
+  }
+
+  /**
+   * This method create Provider Admin
+   * @param providerAdmin: ProviderAdmin
+   */
+  createProviderAdmin(providerAdmin: ProviderAdmin): Observable<object> {
+    return this.http.post('/api/v1/ProviderAdmin/Create', providerAdmin);
   }
 }
