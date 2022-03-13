1992648e28b957bcb91317908919e6f9a591940d
Olya <ola.shy@yahoo.com>
2022-03-13 21:15:56 +0200

    fix

--- src/app/shell/personal-cabinet/parent/create-child/create-child.component.ts
+++ src/app/shell/personal-cabinet/parent/create-child/create-child.component.ts
@@ -72,10 +72,7 @@ export class CreateChildComponent extends CreateFormComponent implements OnInit,
 
     this.ChildrenFormArray.valueChanges.pipe(
       takeUntil(this.destroy$),
-    ).subscribe((val: boolean) => {      
-      this.isEmpty = !val[0].lastName || !val[0].firstName || !val[0].middleName;
-    });
-
+    ).subscribe((val: boolean) => this.isEmpty = !val[0].lastName || !val[0].firstName || !val[0].middleName);
   }
 
   addNavPath(): void {
