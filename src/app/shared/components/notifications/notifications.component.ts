9b4d2cf6afb83d0c269e487028b9ba0fcba6ed89
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-03-16 17:58:07 +0200

    notifications webSocket (#997)
    * notifications
    
    * fix
    
    * fix
    
    * yarn
    
    * Update signal-r.service.spec.ts
    
    * Update signal-r.service.spec.ts
    
    * Update signal-r.service.ts

--- src/app/shared/components/notifications/notifications.component.ts
+++ src/app/shared/components/notifications/notifications.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
-import { MatMenu } from '@angular/material/menu';
+import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
-import { Observable, Subject } from 'rxjs';
-import { filter, takeUntil } from 'rxjs/operators';
-import { NotificationType } from '../../enum/notifications';
-import { NotificationsAmount, Notifications, NotificationGrouped, Notification } from '../../models/notifications.model';
+import { Observable } from 'rxjs';
+import { NotificationsAmount } from '../../models/notifications.model';
 import { AppState } from '../../store/app.state';
-import { GetAllUsersNotificationsGrouped, GetAmountOfNewUsersNotifications, ReadUsersNotificationById, ReadUsersNotificationsByType } from '../../store/notifications.actions';
+import { GetAmountOfNewUsersNotifications } from '../../store/notifications.actions';
 import { NotificationsState } from '../../store/notifications.state';
-import { Util } from '../../utils/utils';
 
 @Component({
   selector: 'app-notifications',
---
+++ src/app/shared/services/signalR/signal-r.service.spec.ts
@@ -0,0 +1,26 @@
+import { TestBed } from '@angular/core/testing';
+import { NgxsModule } from '@ngxs/store';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { MockOidcSecurityService } from '../../mocks/mock-services';
+
+import { SignalRService } from './signal-r.service';
+
+describe('SignalRService', () => {
+  let service: SignalRService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        NgxsModule.forRoot([]),
+      ],
+      providers: [
+        { provide: OidcSecurityService, useValue: MockOidcSecurityService },
+      ]
+    });
+    service = TestBed.inject(SignalRService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
