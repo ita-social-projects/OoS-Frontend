f0299a9f2eac2863b38ee14452861dd8c50634d5
Olya <ola.shy@yahoo.com>
2022-03-16 21:02:46 +0200

    fix

--- src/app/shell/personal-cabinet/parent/create-child/create-child.component.ts
+++ src/app/shell/personal-cabinet/parent/create-child/create-child.component.ts
@@ -77,7 +77,7 @@ export class CreateChildComponent extends CreateFormComponent implements OnInit,
         
         this.ChildrenFormArray.valueChanges.pipe(
           takeUntil(this.destroy$),
-          ).subscribe((val: FormGroup) => {      
+          ).subscribe((val: boolean) => {      
             this.isEmpty = !val[0].lastName || !val[0].firstName || !val[0].middleName;
           });          
         }
