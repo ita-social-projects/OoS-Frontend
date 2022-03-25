7e4e012079ff63a508d8ece0062b0b2c2d55358c
Olya <ola.shy@yahoo.com>
2022-03-25 13:53:29 +0200

    fix

--- src/app/shared/components/city-confirmation/city-confirmation.component.ts
+++ src/app/shared/components/city-confirmation/city-confirmation.component.ts
@@ -1,6 +1,6 @@
 import { Constants } from 'src/app/shared/constants/constants';
 import { Store } from '@ngxs/store';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ConfirmCity } from '../../store/filter.actions';
 import { SetFocusOnCityField } from '../../store/app.actions';
 import { City } from '../../models/city.model';
@@ -10,14 +10,10 @@ import { City } from '../../models/city.model';
   templateUrl: './city-confirmation.component.html',
   styleUrls: ['./city-confirmation.component.scss']
 })
-export class CityConfirmationComponent implements OnInit {
+export class CityConfirmationComponent {
   city: City = JSON.parse(localStorage.getItem('cityConfirmation') as string);
 
   constructor(public store: Store) { }
-
-  ngOnInit() {
-    this.city = JSON.parse(localStorage.getItem('cityConfirmation') as string);
-  }
 
   confirmCity(): void {
     this.city.name !== 'Київ' 
