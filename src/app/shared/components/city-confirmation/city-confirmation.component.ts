03c8eda89029f6d97b8b3fbcbe5a551591dc848a
Olya <ola.shy@yahoo.com>
2022-03-28 13:10:05 +0300

    fix

--- src/app/shared/components/city-confirmation/city-confirmation.component.ts
+++ src/app/shared/components/city-confirmation/city-confirmation.component.ts
@@ -1,6 +1,6 @@
 import { Constants } from 'src/app/shared/constants/constants';
 import { Store } from '@ngxs/store';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ConfirmCity } from '../../store/filter.actions';
 import { SetFocusOnCityField } from '../../store/app.actions';
 import { City } from '../../models/city.model';
@@ -10,16 +10,18 @@ import { City } from '../../models/city.model';
   templateUrl: './city-confirmation.component.html',
   styleUrls: ['./city-confirmation.component.scss']
 })
-export class CityConfirmationComponent {
-  city: City = JSON.parse(localStorage.getItem('cityConfirmation') as string);
+export class CityConfirmationComponent implements OnInit {
+  city: City;
 
   constructor(public store: Store) { }
 
-  confirmCity(): void {
-    this.city.name === 'Київ' 
-      ? localStorage.setItem('cityConfirmation', JSON.stringify(Constants.KIEV)) 
-      : localStorage.setItem('cityConfirmation', JSON.stringify(this.city as City))
-    this.store.dispatch(new ConfirmCity(false));
+  ngOnInit() {
+    this.city = JSON.parse(localStorage.getItem('cityConfirmation') as string);
+  }
+
+  confirmCity(): void {    
+    localStorage.setItem('cityConfirmation', JSON.stringify(this.city));
+    this.store.dispatch(new ConfirmCity(true));
   }
 
   changeCity(): void {
