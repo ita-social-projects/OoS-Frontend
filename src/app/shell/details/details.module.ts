5001c78204c3629e7d96a9faf1b9df5387f17d30
Sasha <alitvinets7@gmail.com>
2022-03-28 11:18:52 +0300

    Update details.module.ts

--- src/app/shell/details/details.module.ts
+++ src/app/shell/details/details.module.ts
@@ -1,19 +1,19 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { DetailsPageComponent } from './workshop-page/details-page.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
 import { DetailsComponent } from './details.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MaterialModule } from 'src/app/shared/modules/material.module';
 import { RouterModule } from '@angular/router';
-import { WorkshopAboutComponent } from './workshop-page/workshop-about/workshop-about.component';
-import { ProviderAboutComponent } from './workshop-page/provider-about/provider-about.component';
-import { WorkshopTeachersComponent } from './workshop-page/workshop-teachers/workshop-teachers.component';
-import { ReviewsComponent } from './workshop-page/reviews/reviews.component';
+import { WorkshopAboutComponent } from './details-page/workshop-about/workshop-about.component';
+import { ProviderAboutComponent } from './details-page/provider-about/provider-about.component';
+import { WorkshopTeachersComponent } from './details-page/workshop-teachers/workshop-teachers.component';
+import { ReviewsComponent } from './details-page/reviews/reviews.component';
 import { CommonModule } from '@angular/common';
 import { ActionsComponent } from './side-menu/actions/actions.component';
 import { ContactsComponent } from './side-menu/contacts/contacts.component';
 import { ScheduleComponent } from './side-menu/schedule/schedule.component';
-import { AllProviderWorkshopsComponent } from './workshop-page/all-provider-workshops/all-provider-workshops.component';
+import { AllProviderWorkshopsComponent } from './details-page/all-provider-workshops/all-provider-workshops.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
