02a0af59d9a2acc778198bba63409db87b626d2c
Olya <ola.shy@yahoo.com>
2022-03-29 12:58:34 +0300

    fix

--- src/app/shared/components/city-confirmation/city-confirmation.component.ts
+++ src/app/shared/components/city-confirmation/city-confirmation.component.ts
@@ -1,26 +1,26 @@
 import { Constants } from 'src/app/shared/constants/constants';
-import { Store } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
 import { Component, OnInit } from '@angular/core';
 import { ConfirmCity } from '../../store/filter.actions';
 import { SetFocusOnCityField } from '../../store/app.actions';
 import { City } from '../../models/city.model';
+import { Observable } from 'rxjs';
+import { FilterState } from '../../store/filter.state';
 
 @Component({
   selector: 'app-city-confirmation',
   templateUrl: './city-confirmation.component.html',
   styleUrls: ['./city-confirmation.component.scss']
 })
-export class CityConfirmationComponent implements OnInit {
+export class CityConfirmationComponent {
   city: City;
+
+  @Select(FilterState.city)
+  city$: Observable<City>;
 
   constructor(public store: Store) { }
 
-  ngOnInit() {
-    this.city = JSON.parse(localStorage.getItem('cityConfirmation') as string);
-  }
-
   confirmCity(): void {    
-    localStorage.setItem('cityConfirmation', JSON.stringify(this.city));
     this.store.dispatch(new ConfirmCity(true));
   }
 
