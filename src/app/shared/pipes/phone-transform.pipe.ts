dcdb231d115df01cf3fbf2b160ad12c3748695d9
perepichai <o.perepichai@gmail.com>
2022-04-05 12:18:01 +0300

    feat: phone number pipe

---
+++ src/app/shared/pipes/phone-transform.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'phoneTransform'
+})
+export class PhoneTransformPipe implements PipeTransform {
+
+  transform(phone: string): string {
+
+    return phone.replace(/(\d{2})(\d{3})(\d{3})(\d{2})(\d{2})/, "$1 ($2)$3-$4-$5");
+  }
+
+}
