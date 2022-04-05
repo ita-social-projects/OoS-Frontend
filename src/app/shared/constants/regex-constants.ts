97bc9290773e6ffe47b55a35af3702a6ac6a652a
Olya <ola.shy@yahoo.com>
2022-04-05 13:09:20 +0200

    ['Редагування закладу' page] Incorrect behavior is displayed when entering invalid data in the 'Повна назва організації', 'Скорочена назва організації' fields #919 (#1032)
    * fix
    
    * fix
    
    * fix
    
    * fix

--- src/app/shared/constants/regex-constants.ts
+++ src/app/shared/constants/regex-constants.ts
@@ -2,11 +2,11 @@
  * RegExp Constants
  */
 
-export const TEXT_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ'’\s-]*$/;
+export const TEXT_REGEX: RegExp = /^\S[А-Яа-яЇїІіЄєЁёҐґ'’\s-]*$/;
 export const TEXT_WITH_DIGITS_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ'’.,\s\d\/-]*$/;
 export const DATE_REGEX: RegExp = /[^0-9./-]*/g;
 export const NAME_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ'’-]*$/;
 export const BIRTH_CERTIFICATE_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁёҐґ'’\s\d-№]*$/;
-export const EDRPOUIPN_REGEX: RegExp = /^[A-Za-zА-Яа-я\s\d-№\/-]*$/;
+export const EDRPOUIPN_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁёҐґ'’`.,\s\d-№]*$/;
 export const WEB_INST_FB_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁёҐґ'’.,!@\d\/-]*$/;
 export const FOUNDER_REGEX: RegExp = /^\S[А-Яа-яЇїІіЄєЁёҐґ'’\s/-]*$/;
