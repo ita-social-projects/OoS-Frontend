43fd7a9c7e0bdf652924f51b94e8986a800649ab
Olya <ola.shy@yahoo.com>
2022-04-06 15:22:50 +0200

    fix

--- src/app/shared/constants/regex-constants.ts
+++ src/app/shared/constants/regex-constants.ts
@@ -3,7 +3,7 @@
  */
 
 export const TEXT_REGEX: RegExp = /^\S[А-Яа-яЇїІіЄєЁёҐґ'’\s-]*$/;
-export const TEXT_WITH_DIGITS_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ'’.,\s\d\/-]*$/;
+export const TEXT_WITH_DIGITS_REGEX: RegExp = /^\S[А-Яа-яЇїІіЄєЁёҐґ'’.,\s\d\/-]*$/;
 export const DATE_REGEX: RegExp = /[^0-9./-]*/g;
 export const NAME_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ'’-]*$/;
 export const BIRTH_CERTIFICATE_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁёҐґ'’\s\d-№]*$/;
