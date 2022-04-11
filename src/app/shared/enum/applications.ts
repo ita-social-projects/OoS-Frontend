5b5cf1f9c1c77baa53cabba997c397b350a647f4
perepichai <o.perepichai@gmail.com>
2022-04-11 15:28:32 +0300

    fix: align application statuses with figma design

--- src/app/shared/enum/applications.ts
+++ src/app/shared/enum/applications.ts
@@ -2,13 +2,19 @@ export enum ApplicationStatus {
   Pending = 'Pending',
   AcceptedForSelection = 'AcceptedForSelection',
   Approved = 'Approved',
+  Studying = 'Studying',
+  Finished = 'Finished',
   Rejected = 'Rejected',
   Left = 'Left',
+  Blocked = 'Blocked',
 }
 export enum ApplicationIcons {
   Pending = 'fas fa-user-clock',
   AcceptedForSelection = 'fas fa-user-plus',
   Approved = 'fas fa-user-check',
-  Rejected = 'fas fa-user-times',
-  Left = 'fas fa-user-times',
+  Studying = 'fas fa-user-check',
+  Finished = 'fas fa-user-times',
+  Rejected = 'fas fa-user-slash',
+  Left = 'fas fa-user-slash',
+  Blocked = 'fas fa-user-lock',
 }
