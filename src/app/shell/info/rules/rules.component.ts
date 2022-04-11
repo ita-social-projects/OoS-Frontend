f6a87e171d92af5ccf65c70ceb07b280ff2d44e8
Sasha <alitvinets7@gmail.com>
2022-04-11 17:38:37 +0300

    blank

--- src/app/shell/info/rules/rules.component.ts
+++ src/app/shell/info/rules/rules.component.ts
@@ -1,28 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Store } from '@ngxs/store';
-import { NavBarName } from 'src/app/shared/enum/navigation-bar';
-import { NavigationBarService } from 'src/app/shared/services/navigation-bar/navigation-bar.service';
-import { AddNavPath, DeleteNavPath } from 'src/app/shared/store/navigation.actions';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-rules',
   templateUrl: './rules.component.html',
   styleUrls: ['./rules.component.scss']
 })
-export class RulesComponent implements OnInit, OnDestroy {
+export class RulesComponent {
 
-  constructor(private store: Store, public navigationBarService: NavigationBarService) { }
-
-  ngOnInit(): void {
-    this.store.dispatch(
-      new AddNavPath(this.navigationBarService.creatOneNavPath(
-        { name: NavBarName.Rules, isActive: false, disable: true }
-      )),
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.store.dispatch(new DeleteNavPath());
-  }
+  constructor() { }
 
 }
