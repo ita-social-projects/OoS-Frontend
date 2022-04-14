eaff449d3d0b935d9d4a298c36fb72617f3ce855
LeraKornienko <makarenkolera460@gmail.com>
2022-04-14 19:14:20 +0300

    done
    done

--- src/app/shell/personal-cabinet/provider/create-provider/create-photo-form/create-photo-form.component.ts
+++ src/app/shell/personal-cabinet/provider/create-provider/create-photo-form/create-photo-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -18,44 +18,37 @@ export class CreatePhotoFormComponent implements OnInit {
 
   readonly constants: typeof Constants = Constants;
 
-  
-  @Select(MetaDataState.institutionStatuses)
-  institutionStatuses$: Observable<InstitutionStatus[]>;
   destroy$: Subject<boolean> = new Subject<boolean>();
 
   PhotoFormGroup: FormGroup;
-
+  descriptionLength = 0;
   @Input() provider: Provider;
-  @Input() editMode: boolean;
-  
+
+
   @Output() passPhotoFormGroup = new EventEmitter();
 
-  constructor(private formBuilder: FormBuilder, private store: Store) {
+
+  constructor(private formBuilder: FormBuilder, private store: Store ) {
     this.PhotoFormGroup = this.formBuilder.group({
       image: new FormControl(''),
       description: new FormControl('', Validators.required),
       institutionStatusId: new FormControl(Constants.INSTITUTION_STATUS_ID_ABSENT_VALUE),
-    });
+    }); 
+      this.PhotoFormGroup.get('description').valueChanges.pipe(takeUntil(this.destroy$)).subscribe((value: string)=>this.descriptionLength = value.length);
+
   }
 
   ngOnInit(): void {
     this.store.dispatch(new GetInstitutionStatus());
-    if (this.provider?.institutionStatusId === null) {
-      this.provider.institutionStatusId = 0
-    }
-    this.provider && this.PhotoFormGroup.patchValue(this.provider, { emitEvent: false });
+    this.provider && this.activateEditMode();
     this.passPhotoFormGroup.emit(this.PhotoFormGroup);
-    
-    this.institutionStatuses$
-    .pipe(
-      takeUntil(this.destroy$),
-    ).subscribe(() => {
-      if (this.editMode) {
-        this.provider.institutionStatusId = this.provider.institutionStatusId || Constants.SOCIAL_GROUP_ID_ABSENT_VALUE;
-        this.PhotoFormGroup.patchValue(this.provider, { emitEvent: false });
-      }
-    });
   }
+
+  private activateEditMode(): void {
+  this.PhotoFormGroup.patchValue(this.provider, { emitEvent: false });
+  this.descriptionLength = this.PhotoFormGroup.get('description').value.length;
+  this.provider.institutionStatusId = this.provider.institutionStatusId || Constants.SOCIAL_GROUP_ID_ABSENT_VALUE;
+}
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
