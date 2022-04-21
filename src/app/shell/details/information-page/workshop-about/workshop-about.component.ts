283b43fb719bf6553a21c2d16f42e12bff6fcf72
Olya <ola.shy@yahoo.com>
2022-04-21 13:47:04 +0200

    fix

--- src/app/shell/details/information-page/workshop-about/workshop-about.component.ts
+++ src/app/shell/details/information-page/workshop-about/workshop-about.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { WorkingDaysValues } from 'src/app/shared/constants/constants';
-import { WorkingDays } from 'src/app/shared/enum/enumUA/working-hours';
+import { WorkingDays, WorkingDaysReverse } from 'src/app/shared/enum/enumUA/working-hours';
 import { WorkingDaysToggleValue } from 'src/app/shared/models/workingHours.model';
 import { Workshop } from 'src/app/shared/models/workshop.model';
 
@@ -12,11 +12,10 @@ import { Workshop } from 'src/app/shared/models/workshop.model';
 
 export class WorkshopAboutComponent {
   readonly workingDays = WorkingDays;
+  readonly workingDaysReverse = WorkingDaysReverse;
   @Input() workshop!: Workshop;
-  days: WorkingDaysToggleValue[] = WorkingDaysValues.map((value: WorkingDaysToggleValue) => Object.assign({}, value));
+  days: WorkingDaysToggleValue[] = WorkingDaysValues
+    .map((value: WorkingDaysToggleValue) => Object.assign({}, value));
  
-  isSelected() {
-    let i = this.workshop.dateTimeRanges[0].workdays.map(el => this.workingDays[el.toLowerCase()]);
-    console.log(i);    
-  }
+
 }
