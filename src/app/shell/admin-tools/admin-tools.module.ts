67ae99180afccb15c84083ebfaf2dc0f5ad89e90
Olya <ola.shy@yahoo.com>
2022-05-07 15:00:24 +0200

    fix

--- src/app/shell/admin-tools/admin-tools.module.ts
+++ src/app/shell/admin-tools/admin-tools.module.ts
@@ -5,12 +5,14 @@ import { PlatformComponent } from './platform/platform.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { DirectionsComponent } from './platform/create-direction/directions/directions.component';
+import { ProviderListComponent } from './provider-list/provider-list.component';
 
 
 @NgModule({
   declarations: [
     PlatformComponent,
     DirectionsComponent,
+    ProviderListComponent,
   ],
   imports: [
     CommonModule,
--- src/app/shell/admin-tools/provider-list/provider-list.component.ts
+++ src/app/shell/admin-tools/provider-list/provider-list.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { Actions, Store } from '@ngxs/store';
 
+export interface Tile {  
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-provider-list',
   templateUrl: './provider-list.component.html',
   styleUrls: ['./provider-list.component.scss'],
 })
 export class ProviderListComponent implements OnInit {
-  constructor(store: Store, private actions$: Actions) {}
+  tiles: Tile[] = [
+    { text: 'Назва закладу', cols: 1, rows: 1 },
+    { text: 'Форма власності', cols: 1, rows: 1 },
+    { text: 'Ліцензія №', cols: 1, rows: 1 },
+    { text: 'Населений пункт', cols: 1, rows: 1 },
+    { text: 'Адреса', cols: 1, rows: 1 },
+    { text: 'Директор', cols: 1, rows: 1 },
+    { text: 'Статус', cols: 1, rows: 1 },
+  ];
+
+  constructor() {}
 
   ngOnInit(): void {}
-
-  init(): void {
-  }
 }
