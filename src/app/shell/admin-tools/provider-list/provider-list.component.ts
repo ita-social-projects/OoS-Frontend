1c5b2af17c4250c20fae1d4812322c8dff596deb
Olya <ola.shy@yahoo.com>
2022-05-07 15:29:00 +0200

    fix

--- src/app/shell/admin-tools/provider-list/provider-list.component.ts
+++ src/app/shell/admin-tools/provider-list/provider-list.component.ts
@@ -1,8 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Address } from 'src/app/shared/models/address.model';
+import { Workshop } from 'src/app/shared/models/workshop.model';
 
 export interface Tile {
   text: string;
 }
+
+export interface Provider {
+  fullTitle?: string;
+  edrpouIpn?: string;
+  director?: string;
+  phoneNumber?: string;
+  founder?: string;
+  ownership?: string;
+  type?: number;
+  status?: string;
+  legalAddress?: string;
+  city: string
+}
+
 @Component({
   selector: 'app-provider-list',
   templateUrl: './provider-list.component.html',
@@ -11,21 +27,41 @@ export interface Tile {
 export class ProviderListComponent implements OnInit {
   tiles: Tile[] = [
     { text: 'Назва закладу' },
-    { text: 'Форма власності',  },
-    { text: 'Ліцензія №',  },
-    { text: 'Населений пункт',  },
-    { text: 'Адреса', },
-    { text: 'Директор',  },
-    { text: 'Статус', },
+    { text: 'Форма власності' },
+    { text: 'Ліцензія №' },
+    { text: 'Населений пункт' },
+    { text: 'Адреса' },
+    { text: 'Директор' },
+    { text: 'Статус' },
   ];
-  providers: Tile[] = [
-    { text: 'ДНУ ім.Гончара', },
-    { text: 'Державна', },
-    { text: '3897653', },
-    { text: 'Київ', },
-    { text: 'вул.Рокосовського 1', },
-    { text: 'Антонів', },
-    { text: 'Очікує логування' },
+  providers: Provider[] = [
+    {
+      fullTitle: 'ДНУ ім.Гончара',
+      ownership: 'Державна',
+      edrpouIpn: '3897653',
+      city: 'Київ',
+      legalAddress: 'вул.Рокосовського 1',
+      director: 'Антонів',
+      status: 'Очікує логування',
+    },
+    {
+      fullTitle: 'ДНУ ім.Гончара',
+      ownership: 'Державна',
+      edrpouIpn: '3897653',
+      city: 'Київ',
+      legalAddress: 'вул.Рокосовського 1',
+      director: 'Антонів',
+      status: 'Очікує логування',
+    },
+    {
+      fullTitle: 'ДНУ ім.Гончара',
+      ownership: 'Державна',
+      edrpouIpn: '3897653',
+      city: 'Київ',
+      legalAddress: 'вул.Рокосовського 1',
+      director: 'Антонів',
+      status: 'Очікує логування',
+    },
   ];
 
   constructor() {}
