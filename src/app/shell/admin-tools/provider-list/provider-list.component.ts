541ac5c7ac960dddc808a0daa71dd0e5706939e7
Olya <ola.shy@yahoo.com>
2022-05-12 10:09:08 +0200

    fix

--- src/app/shell/admin-tools/provider-list/provider-list.component.ts
+++ src/app/shell/admin-tools/provider-list/provider-list.component.ts
@@ -27,13 +27,64 @@ export class ProviderListComponent implements OnInit {
   constructor(private store: Store, public providerService: ProviderService) {}
 
   @Select(AdminState.providers)
-  providers: Observable<Provider[]>;
+  providers$: Observable<Provider[]>;
 
   ngOnInit() {
-    this.getAllProviders(); 
+    this.getAllProviders();
   }
 
   getAllProviders() {
     this.store.dispatch(new GetAllProviders());
   }
+
+  providers: Provider[] = [
+    {
+      userId: '1',
+      id: '1',
+      shortTitle: '1',
+      fullTitle: 'ДНУ ім.Гончара',
+      ownership: 'Державна',
+      edrpouIpn: '9897653',
+      legalAddress: {
+        city: 'Київ',
+        street: 'вул.Рокосовського',
+        buildingNumber: '1',
+      },
+      director: 'Антонів',
+      email: '',
+      status: true,
+    },
+    {
+      userId: '3',
+      id: '4',
+      shortTitle: '1',
+      fullTitle: 'Школа бальних танців',
+      ownership: 'Державна',
+      edrpouIpn: '3897653',
+      legalAddress: {
+        city: 'Київ',
+        street: 'вул.Фокосовського',
+        buildingNumber: '5',
+      },
+      director: 'Ярко',
+      email: '',
+      status: true,
+    },
+    {
+      userId: '2',
+      id: '3',
+      shortTitle: '4',
+      fullTitle: 'Школа молодого фотографа',
+      ownership: 'Приватна',
+      edrpouIpn: '2897653',
+      legalAddress: {
+        city: 'Львів',
+        street: 'вул.Вокосовського',
+        buildingNumber: '7',
+      },
+      director: 'Лесів',
+      email: '',
+      status: true,
+    },
+  ];
 }
