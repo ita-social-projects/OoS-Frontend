4445fa56383496cb9b403003bffb289866c5c4e3
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-05-12 19:28:04 +0300

    link fix

--- src/app/shared/constants/regex-constants.ts
+++ src/app/shared/constants/regex-constants.ts
@@ -9,4 +9,4 @@ export const DATE_REGEX: RegExp = /[^0-9./-]*/g;
 export const NAME_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ'’-]*$/;
 export const BIRTH_CERTIFICATE_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁёҐґ'’\s\d-№]*$/;
 export const EDRPOUIPN_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁёҐґ'’`.,\s\d-№]*$/;
-export const WEB_INST_FB_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁёҐґ'’.,!@\d\/-]*$/;
+export const WEB_INST_FB_REGEX: RegExp = /^[A-Za-zА-Яа-яЇїІіЄєЁёҐґ'’.,:!@\d\/-]*$/;
