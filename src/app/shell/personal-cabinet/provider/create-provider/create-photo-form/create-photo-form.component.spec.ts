9fdc6452d169e9749c315fddbad92f90fc6f76eb
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-05-12 20:12:02 +0300

    test fix

--- src/app/shell/personal-cabinet/provider/create-provider/create-photo-form/create-photo-form.component.spec.ts
+++ src/app/shell/personal-cabinet/provider/create-provider/create-photo-form/create-photo-form.component.spec.ts
@@ -56,18 +56,15 @@ describe('CreatePhotoFormComponent', () => {
 });
 
 @Component({
-  selector: 'app-validation-hint-for-input',
+  selector: 'app-validation-hint',
   template: ''
 })
 
 class MockValidationHintForInputComponent {
-  @Input() type: string;
-  @Input() invalid: boolean;
-  @Input() isEmailCheck: boolean;
-  @Input() isEmptyCheck: boolean;
-  @Input() minLength: boolean;
-  @Input() maxLength: boolean;
-  @Input() minCharachters: number; 
-  @Input() maxCharachters: number; 
-  @Input() forbiddenCharacter: string;
+  @Input() validationFormControl: FormControl; 
+  @Input() minCharachters: number;
+  @Input() maxCharachters: number;
+  @Input() minMaxDate: boolean;
+  @Input() allowedCharacters: string;
+  @Input() edrpouIpn: boolean;
 }
