bda10b86ad2eae5cd8ea55881cfe735a3b145e31
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-05-13 14:00:42 +0300

    form address fix

--- src/app/shell/personal-cabinet/provider/create-workshop/create-address/create-address.component.ts
+++ src/app/shell/personal-cabinet/provider/create-workshop/create-address/create-address.component.ts
@@ -6,6 +6,7 @@ import { Address } from 'src/app/shared/models/address.model';
 import { City } from 'src/app/shared/models/city.model';
 import { MetaDataState } from 'src/app/shared/store/meta-data.state';
 import { TEXT_REGEX, TEXT_WITH_DIGITS_REGEX } from 'src/app/shared/constants/regex-constants'
+import { ValidationConstants, ValidationTextField } from 'src/app/shared/constants/validation';
 
 @Component({
   selector: 'app-create-address',
@@ -13,12 +14,13 @@ import { TEXT_REGEX, TEXT_WITH_DIGITS_REGEX } from 'src/app/shared/constants/reg
   styleUrls: ['./create-address.component.scss']
 })
 export class CreateAddressComponent implements OnInit {
-
+  readonly validationConstants = ValidationConstants;
+  readonly validationTextField = ValidationTextField;
+  
   @Input() address: Address;
   @Output() passAddressFormGroup = new EventEmitter();
 
   AddressFormGroup: FormGroup;
-  cityValue: FormControl;
   city: string;
 
   @Select(MetaDataState.cities)
@@ -27,18 +29,34 @@ export class CreateAddressComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder) {
     this.AddressFormGroup = this.formBuilder.group({
-      street: new FormControl('', [Validators.required, Validators.pattern(TEXT_WITH_DIGITS_REGEX)]),
-      buildingNumber: new FormControl('', [Validators.required, Validators.pattern(TEXT_WITH_DIGITS_REGEX)]),
-      city: new FormControl('', [Validators.required, Validators.pattern(TEXT_REGEX)]),
-      latitude: new FormControl(''),
+      street: new FormControl('', [
+        Validators.required, 
+        Validators.pattern(TEXT_WITH_DIGITS_REGEX),
+        Validators.minLength(ValidationConstants.INPUT_LENGTH_1),
+        Validators.maxLength(ValidationConstants.INPUT_LENGTH_30)
+      ]),
+      buildingNumber: new FormControl('', [
+        Validators.required, 
+        Validators.pattern(TEXT_WITH_DIGITS_REGEX),
+        Validators.minLength(ValidationConstants.INPUT_LENGTH_1),
+        Validators.maxLength(ValidationConstants.INPUT_LENGTH_15)
+      ]),
+      city: new FormControl('', [
+        Validators.required,
+        Validators.pattern(TEXT_REGEX),
+        Validators.minLength(ValidationConstants.INPUT_LENGTH_1),
+        Validators.maxLength(ValidationConstants.INPUT_LENGTH_15)
+      ]),
       longitude: new FormControl(''),
     });
   }
 
   ngOnInit(): void {
     this.passAddressFormGroup.emit(this.AddressFormGroup);
-    this.address && (this.city = this.address.city);
-    this.address && this.AddressFormGroup.patchValue(this.address, { emitEvent: false });
+    if(this.address){
+      this.city = this.address.city;
+      this.AddressFormGroup.patchValue(this.address, { emitEvent: false });
+    }
   }
 
   onSelectedCity(event: any): void {
