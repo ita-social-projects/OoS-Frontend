2150ad6a3c8308f44b5eea4fea0aeeac16ae9638
Olya <ola.shy@yahoo.com>
2022-05-21 20:21:57 +0200

    fix

--- src/app/shared/components/provider-info/provider-info.component.ts
+++ src/app/shared/components/provider-info/provider-info.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Constants } from 'src/app/shared/constants/constants';
+import { MatTabChangeEvent } from '@angular/material/tabs';
+import { createProviderSteps, OwnershipType, OwnershipTypeUkr, ProviderType, ProviderTypeUkr } from '../../enum/provider';
+import { Provider } from '../../models/provider.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-provider-info',
   templateUrl: './provider-info.component.html',
-  styleUrls: ['./provider-info.component.scss']
+  styleUrls: ['./provider-info.component.scss'],
 })
 export class ProviderInfoComponent implements OnInit {
+  readonly constants: typeof Constants = Constants;
+  readonly createProviderSteps = createProviderSteps;
+  readonly providerType: typeof ProviderType = ProviderType;
+  readonly ownershipType: typeof OwnershipType = OwnershipType;
+  readonly ownershipTypeUkr = OwnershipTypeUkr;
+  readonly providerTypeUkr = ProviderTypeUkr;
 
-  constructor() { }
+  editLink: string = createProviderSteps[0];
 
-  ngOnInit(): void {
+  @Input() provider: Provider;
+  @Input() institutionStatuses;
+  @Output() tabChanged = new EventEmitter();
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  currentStatus: string;
+  
+  constructor() {}
+
+  ngOnInit(): void {}
+  onTabChanged(tabChangeEvent: MatTabChangeEvent): void {
+    this.tabChanged.emit(tabChangeEvent);
   }
-
 }
