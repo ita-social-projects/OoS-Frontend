c9370bb0b77993b3bcdf80bf0cba34e95aa7a805
Olya <ola.shy@yahoo.com>
2022-05-21 20:56:16 +0200

    fix

--- src/app/shell/personal-cabinet/provider/provider-org-info/provider-org-info.component.ts
+++ src/app/shell/personal-cabinet/provider/provider-org-info/provider-org-info.component.ts
@@ -1,18 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subject } from 'rxjs';
-import {
-  createProviderSteps,
-  OwnershipType,
-  OwnershipTypeUkr,
-  ProviderType,
-  ProviderTypeUkr,
-} from 'src/app/shared/enum/provider';
-
 import { Provider } from 'src/app/shared/models/provider.model';
-import { ActivateEditMode } from 'src/app/shared/store/app.actions';
 import { RegistrationState } from 'src/app/shared/store/registration.state';
-import { Constants } from 'src/app/shared/constants/constants';
 
 @Component({
   selector: 'app-provider-org-info',
@@ -20,19 +10,10 @@ import { Constants } from 'src/app/shared/constants/constants';
   styleUrls: ['./provider-org-info.component.scss'],
 })
 export class ProviderOrgInfoComponent implements OnInit {
-  readonly constants: typeof Constants = Constants;
-  readonly providerType: typeof ProviderType = ProviderType;
-  readonly ownershipType: typeof OwnershipType = OwnershipType;
-  readonly ownershipTypeUkr = OwnershipTypeUkr;
-  readonly providerTypeUkr = ProviderTypeUkr;
-  readonly createProviderSteps = createProviderSteps;
-  editLink: string = createProviderSteps[0];
-
   @Select(RegistrationState.provider)
   provider$: Observable<Provider>;
   destroy$: Subject<boolean> = new Subject<boolean>();
 
-  currentStatus: string;
   constructor(private store: Store) {}
 
   ngOnDestroy(): void {
@@ -41,9 +22,5 @@ export class ProviderOrgInfoComponent implements OnInit {
   }
 
   ngOnInit(): void {}
-
-  ActivateEditMode(): void {
-    this.store.dispatch(new ActivateEditMode(true));
-  }
 
 }
