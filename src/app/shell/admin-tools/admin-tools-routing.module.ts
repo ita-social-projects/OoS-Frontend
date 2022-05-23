0a5c63e7c72b8441b017b9bf1f0f3c75938553e2
Olya <ola.shy@yahoo.com>
2022-05-23 12:54:51 +0200

    fix

--- src/app/shell/admin-tools/admin-tools-routing.module.ts
+++ src/app/shell/admin-tools/admin-tools-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { ProviderOrgInfoComponent } from '../personal-cabinet/provider/provider-org-info/provider-org-info.component';
 import { PlatformComponent } from './platform/platform.component';
 import { ProviderListComponent } from './provider-list/provider-list.component';
 
