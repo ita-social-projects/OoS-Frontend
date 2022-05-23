7807f67140ac837e0b5ba0fd3871ad29db78bb18
perepichai <o.perepichai@gmail.com>
2022-05-23 12:24:50 +0300

    fix test

--- src/app/shell/admin-tools/users/users.component.spec.ts
+++ src/app/shell/admin-tools/users/users.component.spec.ts
@@ -1,6 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTabsModule } from '@angular/material/tabs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -17,7 +20,10 @@ describe('UsersComponent', () => {
       imports: [
         NgxsModule.forRoot([]),
         RouterTestingModule,
+        MatFormFieldModule,
+        MatInputModule,
         BrowserAnimationsModule,
+        ReactiveFormsModule,
         MatTabsModule,
         MatIconModule,
       ],
