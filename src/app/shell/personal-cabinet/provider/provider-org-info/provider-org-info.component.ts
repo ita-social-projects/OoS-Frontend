68c15480c98a9b9affef7e07e9e4097e9da1ed03
Olya <ola.shy@yahoo.com>
2022-05-24 13:44:07 +0200

    fix

--- src/app/shell/personal-cabinet/provider/provider-org-info/provider-org-info.component.ts
+++ src/app/shell/personal-cabinet/provider/provider-org-info/provider-org-info.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Provider } from 'src/app/shared/models/provider.model';
 import { RegistrationState } from 'src/app/shared/store/registration.state';
-import { ActivateEditMode } from 'src/app/shared/store/app.actions';
 
 @Component({
   selector: 'app-provider-org-info',
