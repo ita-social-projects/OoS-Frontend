738a3b3eb30e93df680298c448035b1be7549303
Olya <ola.shy@yahoo.com>
2022-05-29 18:13:45 +0200

    fix

--- src/app/shared/components/users-list/users-list.component.ts
+++ src/app/shared/components/users-list/users-list.component.ts
@@ -1,11 +1,30 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Select } from '@ngxs/store';
+import { Observable } from 'rxjs';
 import { Constants } from 'src/app/shared/constants/constants';
-import { providerAdminRoleUkr, ProviderAdminTitles } from '../../enum/enumUA/provider-admin';
-import { ProviderAdminIcons, ProviderAdminStatus } from '../../enum/provider-admin';
+import {
+  providerAdminRoleUkr,
+  ProviderAdminTitles,
+} from '../../enum/enumUA/provider-admin';
+import {
+  ProviderAdminIcons,
+  ProviderAdminStatus,
+} from '../../enum/provider-admin';
+import { Role } from '../../enum/role';
 import { ProviderAdminTable } from '../../models/providerAdmin.model';
+import { RegistrationState } from '../../store/registration.state';
 
 /**
  * @title Table with sorting
@@ -13,9 +32,11 @@ import { ProviderAdminTable } from '../../models/providerAdmin.model';
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
 })
 export class UsersListComponent implements OnInit, AfterViewInit {
+  @Select(RegistrationState.subrole)
+  subrole$: Observable<string>;
 
   @Input() users: Array<object>;
   @Input() filterValue: string;
@@ -27,6 +48,8 @@ export class UsersListComponent implements OnInit, AfterViewInit {
   readonly providerAdminTitles = ProviderAdminTitles;
   readonly providerAdminStatus = ProviderAdminStatus;
   readonly providerAdminIcons = ProviderAdminIcons;
+  subrole: string;
+  Role = Role;
   displayedColumns: string[];
   dataSource: MatTableDataSource<object> = new MatTableDataSource([{}]);
 
@@ -35,8 +58,17 @@ export class UsersListComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit(): void {
-      this.displayedColumns = ['pib', 'email', 'phone', 'place', 'isDeputy', 'status', 'actions'];
-      this.dataSource = new MatTableDataSource(this.users);
+    this.displayedColumns = [
+      'pib',
+      'email',
+      'phone',
+      'place',
+      'isDeputy',
+      'status',
+      'actions',
+    ];
+    this.dataSource = new MatTableDataSource(this.users);
+    this.subrole$.subscribe((subrole) => (this.subrole = subrole));
   }
 
   ngAfterViewInit() {
@@ -44,15 +76,15 @@ export class UsersListComponent implements OnInit, AfterViewInit {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-      if (changes.filterValue && changes.filterValue.currentValue) {
-        let filter = changes.filterValue.currentValue;
-        this.dataSource.filter = filter.trim().toLowerCase();
-      } else this.dataSource.filter = "";
-      
-      if (changes.users && changes.users.currentValue) {
-        const users = changes.users.currentValue;
-        this.dataSource = new MatTableDataSource(users);
-      }
+    if (changes.filterValue && changes.filterValue.currentValue) {
+      let filter = changes.filterValue.currentValue;
+      this.dataSource.filter = filter.trim().toLowerCase();
+    } else this.dataSource.filter = '';
+
+    if (changes.users && changes.users.currentValue) {
+      const users = changes.users.currentValue;
+      this.dataSource = new MatTableDataSource(users);
+    }
   }
 
   /** Announce the change in sort state for assistive technology. */
