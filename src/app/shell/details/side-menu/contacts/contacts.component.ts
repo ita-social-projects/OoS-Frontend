b6062b1c43430b5c7e486a27f7a16fe6daeb18de
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-06-03 01:27:54 +0300

    Update contacts.component.ts

--- src/app/shell/details/side-menu/contacts/contacts.component.ts
+++ src/app/shell/details/side-menu/contacts/contacts.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Workshop } from 'src/app/shared/models/workshop.model';
 import { Constants } from 'src/app/shared/constants/constants';
+import { Address } from 'src/app/shared/models/address.model';
 
 @Component({
   selector: 'app-contacts',
@@ -9,9 +10,17 @@ import { Constants } from 'src/app/shared/constants/constants';
 })
 export class ContactsComponent implements OnInit {
   readonly constants: typeof Constants = Constants;
-  address = 'https://www.google.com/maps/place/';
 
-  @Input() workshop: Workshop;
+  @Input() address: Address;
+  @Input() contactsData: {
+    phone: string;
+    email: string;
+    facebook: string;
+    instagram: string;
+    website: string;
+  };
+
+  addressLink = 'https://www.google.com/maps/place/';
 
   constructor() { }
 
@@ -20,12 +29,12 @@ export class ContactsComponent implements OnInit {
   /*Detects device and opens map*/
   mapLink(): void {
     if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-      this.address = 'http://maps.apple.com:';
-      window.open(`${this.address} ${this.workshop.address.street},+ ${this.workshop.address.buildingNumber} ,+ ${this.workshop.address.city}`);
+      this.addressLink = 'http://maps.apple.com:';
+      window.open(`${this.addressLink} ${this.address.street},+ ${this.address.buildingNumber} ,+ ${this.address.city}`);
     } else if (/Android/i.test(navigator.userAgent)) {
-      window.open(`${this.address} ${this.workshop.address.street},+ ${this.workshop.address.buildingNumber} ,+ ${this.workshop.address.city}`);
+      window.open(`${this.addressLink} ${this.address.street},+ ${this.address.buildingNumber} ,+ ${this.address.city}`);
     } else {
-      window.open(`${this.address} ${this.workshop.address.street},+ ${this.workshop.address.buildingNumber} ,+ ${this.workshop.address.city}`, '_blank');
+      window.open(`${this.addressLink} ${this.address.street},+ ${this.address.buildingNumber} ,+ ${this.address.city}`, '_blank');
     }
   }
 }
