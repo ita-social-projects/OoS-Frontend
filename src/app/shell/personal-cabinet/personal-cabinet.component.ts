3728674c1b35b0580ba951063b0b2a3f80f87a9c
Olya <ola.shy@yahoo.com>
2022-06-05 11:46:12 +0200

    fix

--- src/app/shell/personal-cabinet/personal-cabinet.component.ts
+++ src/app/shell/personal-cabinet/personal-cabinet.component.ts
@@ -1,26 +1,22 @@
 import { NavigationBarService } from './../../shared/services/navigation-bar/navigation-bar.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Select, Store } from '@ngxs/store';
+import { Store } from '@ngxs/store';
 import { Role, RoleLinks } from 'src/app/shared/enum/role';
 import { NavBarName } from 'src/app/shared/enum/navigation-bar';
 import { User } from 'src/app/shared/models/user.model';
 import { AddNavPath, DeleteNavPath } from 'src/app/shared/store/navigation.actions';
 import { RegistrationState } from 'src/app/shared/store/registration.state';
 import { PersonalCabinetTitle } from 'src/app/shared/enum/enumUA/provider-admin';
-import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-personal-cabinet',
   templateUrl: './personal-cabinet.component.html',
   styleUrls: ['./personal-cabinet.component.scss'],
 })
-export class PersonalCabinetComponent implements OnInit, OnDestroy {  
-
-export class PersonalCabinetComponent implements OnInit {
+export class PersonalCabinetComponent implements OnInit, OnDestroy {
   roles = RoleLinks;
   subrole: string;
   userRole: string;
-  subrole: string;
   Role = Role;
   PersonalCabinetTitle = PersonalCabinetTitle;
 
@@ -49,5 +45,9 @@ export class PersonalCabinetComponent implements OnInit {
         })
       )
     );    
+  }
+
+  ngOnDestroy(): void {
+    this.store.dispatch(new DeleteNavPath());
   }
 }
