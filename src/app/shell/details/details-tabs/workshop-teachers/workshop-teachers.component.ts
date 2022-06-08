6b7f345e24e46457d96ed540259541dce4faf184
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-06-08 12:07:20 +0300

    [Workshop/Provider Details] - Structure Refactoring (#1259)
    * refactoring
    
    * Update side-menu.component.ts
    
    * Update side-menu.component.html
    
    * Update contacts.component.ts
    
    * Update contacts.component.html
    
    * finish
    
    * refactoring finale
    
    * fix
    
    * test fix
    
    * test fix
    
    * Update workshop-details.component.html
    
    * Update details.component.html
    
    Co-authored-by: Dmytro Minochkin <dmytro.minochkin@gmail.com>

--- src/app/shell/details/details-tabs/workshop-teachers/workshop-teachers.component.ts
+++ src/app/shell/details/details-tabs/workshop-teachers/workshop-teachers.component.ts
@@ -1,20 +1,16 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NoResultsTitle } from 'src/app/shared/enum/no-results';
 import { Teacher } from 'src/app/shared/models/teacher.model';
-import { Workshop } from 'src/app/shared/models/workshop.model';
 
 @Component({
   selector: 'app-workshop-teachers',
   templateUrl: './workshop-teachers.component.html',
   styleUrls: ['./workshop-teachers.component.scss']
 })
-export class WorkshopTeachersComponent implements OnInit {
+export class WorkshopTeachersComponent {
+  readonly noResultTeachers = NoResultsTitle.noTeachers;
 
   @Input() teachers: Teacher[];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
