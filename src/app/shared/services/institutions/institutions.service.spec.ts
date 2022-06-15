4bf440ab2f35b4bb777f1a2b58728b0803f2bd25
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-06-15 08:17:33 +0300

    Update institutions.service.spec.ts

--- src/app/shared/services/institutions/institutions.service.spec.ts
+++ src/app/shared/services/institutions/institutions.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { InstitutionsService } from './institutions.service';
