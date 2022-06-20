9a47ae01b319414db8deb5acbb7973633774dffe
perepichai <o.perepichai@gmail.com>
2022-06-20 13:20:36 +0300

    add modal width

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -40,6 +40,7 @@ export class Constants {
   static readonly MAT_TOOL_TIP_POSITION_BELOW = 'below';
   static readonly NO_INFORMATION = `не вказано`;
   static readonly MODAL_SMALL = '500px';
+  static readonly MODAL_MEDIUM = '1024px';
 }
 
 export class PaginationConstants {
