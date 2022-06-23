26d9e46839cd488e95d640f72b2cac528b12dc79
Olya <ola.shy@yahoo.com>
2022-06-23 14:06:38 +0200

    fix tests

--- src/app/shell/personal-cabinet/provider/create-achievement/create-achievement.component.spec.ts
+++ src/app/shell/personal-cabinet/provider/create-achievement/create-achievement.component.spec.ts
@@ -1,4 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsModule } from '@ngxs/store';
 
 import { CreateAchievementComponent } from './create-achievement.component';
 
@@ -8,9 +14,17 @@ describe('CreateAchievementComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateAchievementComponent ]
-    })
-    .compileComponents();
+      imports: [
+        NgxsModule.forRoot([]),
+        MatFormFieldModule,
+        MatSelectModule,
+        MatOptionModule,
+        ReactiveFormsModule,
+        FormsModule,
+        BrowserAnimationsModule,
+      ],
+      declarations: [CreateAchievementComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
