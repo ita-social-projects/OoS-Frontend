158bacbbba5a2d0d2070d52ace4d439b05012f7d
Olya <ola.shy@yahoo.com>
2022-06-23 12:54:26 +0200

    fix

--- src/app/shell/personal-cabinet/provider/create-achievement/create-achievement.component.ts
+++ src/app/shell/personal-cabinet/provider/create-achievement/create-achievement.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-achievement',
   templateUrl: './create-achievement.component.html',
-  styleUrls: ['./create-achievement.component.scss']
+  styleUrls: ['./create-achievement.component.scss'],
 })
 export class CreateAchievementComponent implements OnInit {
+  children$ = [
+    { lastName: 'Тетерукова', firstName: 'Дарина' },
+    { lastName: 'Узумакі', firstName: 'Боруто' },
+    { lastName: 'Малинка', firstName: 'Малина' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
