937be489678567dcff1022e0f059be6c3198563d
Olya <ola.shy@yahoo.com>
2022-06-24 09:52:37 +0200

    fix on comments

--- src/app/shell/details/achievements/achievements.component.ts
+++ src/app/shell/details/achievements/achievements.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NoResultsTitle } from 'src/app/shared/enum/no-results';
+import { Achievement } from 'src/app/shared/models/achievement.model';
 
 @Component({
   selector: 'app-achievements',
   templateUrl: './achievements.component.html',
-  styleUrls: ['./achievements.component.scss']
+  styleUrls: ['./achievements.component.scss'],
 })
 export class AchievementsComponent implements OnInit {
+  readonly noResultTeachers = NoResultsTitle.noAchievements;
+  @Input() achievements: Achievement[] = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
