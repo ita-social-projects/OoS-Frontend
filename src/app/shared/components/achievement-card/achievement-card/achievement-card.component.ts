f3154f47e0e9fd522e9be4f5ece77330ccc71aaa
Olya <ola.shy@yahoo.com>
2022-06-24 10:49:16 +0200

    fix on comments

--- src/app/shared/components/achievement-card/achievement-card/achievement-card.component.ts
+++ src/app/shared/components/achievement-card/achievement-card/achievement-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Achievement } from 'src/app/shared/models/achievement.model';
 
 @Component({
   selector: 'app-achievement-card',
   templateUrl: './achievement-card.component.html',
-  styleUrls: ['./achievement-card.component.scss']
+  styleUrls: ['./achievement-card.component.scss'],
 })
 export class AchievementCardComponent implements OnInit {
+  @Input() achievement: Achievement;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
