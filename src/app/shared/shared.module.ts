089ac647ac0cdbc589af311a7cb5498b00124d50
Illya97 <ovsannikovillya@gmail.com>
2022-06-24 17:13:20 +0300

    import EntityCheckboxDropdownComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -31,7 +31,6 @@ import { ConfirmationModalWindowComponent } from './components/confirmation-moda
 import { PlaceholderFormDirective } from './directives/placeholder-styling.directive';
 import { ValidationMessageStyling } from './directives/validation-message-styling.directive';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
-import { EntityCheckboxDropdownComponent } from './components/entity-checkbox-dropdown/entity-checkbox-dropdown.component';
 import { NavigationMobileBarComponent } from './components/navigation-mobile-bar/navigation-mobile-bar.component';
 import { FullSearchBarComponent } from './components/full-search-bar/full-search-bar.component';
 import { MessageBarComponent } from './components/message-bar/message-bar.component';
@@ -65,6 +64,7 @@ import { ValidationHintComponent } from './components/validation-hint/validation
 import { TrimValueDirective } from './directives/trim-value.directive';
 import { WorkingHoursFormWrapperComponent } from './components/working-hours-form-wrapper/working-hours-form-wrapper.component';
 import { InfoFormComponent } from './components/info-form/info-form.component';
+import { EntityCheckboxDropdownComponent } from './components/entity-checkbox-dropdown/entity-checkbox-dropdown.component';
 
 @NgModule({
   declarations: [
