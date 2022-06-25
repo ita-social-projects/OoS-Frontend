1f37e19b0df7c9d47a1f82873132336abb015d2c
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-06-25 11:59:14 +0300

    test fix

--- src/app/shared/components/institution-hierarchy/institution-hierarchy.component.spec.ts
+++ src/app/shared/components/institution-hierarchy/institution-hierarchy.component.spec.ts
@@ -1,6 +1,12 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InstitutionHierarchyComponent } from './institution-hierarchy.component';
+import { NgxsModule } from '@ngxs/store';
 
 describe('InstitutionHierarchyComponent', () => {
   let component: InstitutionHierarchyComponent;
@@ -8,7 +14,19 @@ describe('InstitutionHierarchyComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InstitutionHierarchyComponent ]
+      imports:[
+        MatOptionModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgxsModule.forRoot([]),
+      ],
+      declarations: [ 
+        InstitutionHierarchyComponent,
+        MockValidationHintHierarchyComponent 
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +34,21 @@ describe('InstitutionHierarchyComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InstitutionHierarchyComponent);
     component = fixture.componentInstance;
+    jest.useFakeTimers();
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  selector: 'app-validation-hint',
+  template: ''
+})
+class MockValidationHintHierarchyComponent {
+  @Input() validationFormControl: FormControl; //required for validation
+  @Input() isTouched: boolean;
+}
