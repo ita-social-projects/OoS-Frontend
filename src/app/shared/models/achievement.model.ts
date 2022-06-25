8d1ce3f28bdc278397cd1d9f5d1f8b688628b2f2
Olya-web7 <ola.shy@yahoo.com>
2022-06-25 15:21:14 +0200

    fix

--- src/app/shared/models/achievement.model.ts
+++ src/app/shared/models/achievement.model.ts
@@ -9,11 +9,7 @@ export class Achievement {
   childrenIDs: string[];
   teachers: string[];
 
-<<<<<<< HEAD
-  constructor(title: string) {
-=======
   constructor(title: string, workshop?: Workshop, teachers?: string[]) {
->>>>>>> d235d115d8a8940e0cb2f30374c723373f8d04f5
     this.title = title;
     // this.workshopId = workshop.id;
     // this.teachers = teachers;
