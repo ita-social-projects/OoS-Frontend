0f46ab5950c0bcf6e5b75bcba6ad09273a116ef3
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-06-27 16:18:31 +0300

    Update validation-hint.component.ts

--- src/app/shared/components/validation-hint/validation-hint.component.ts
+++ src/app/shared/components/validation-hint/validation-hint.component.ts
@@ -70,7 +70,8 @@ export class ValidationHintComponent implements OnInit, OnDestroy, OnChanges {
 
   private checkValidationErrors(errors: ValidationErrors): void {
     this.invalidEmail = !!errors?.email;
-    this.invalidFieldLength = !!(errors?.maxlength && errors?.minlength);
+    this.invalidFieldLength = !!(errors?.maxlength || errors?.minlength);
+    console.log(errors)
     this.invalidPhoneLength = !!errors?.minlength && !errors?.maxlength;
   }
 
