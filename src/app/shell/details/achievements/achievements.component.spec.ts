f37af1578d1c0e95065139c2b7829dfbcf3c46aa
Olya-web7 <ola.shy@yahoo.com>
2022-06-27 22:04:19 +0200

    fix

--- src/app/shell/details/achievements/achievements.component.spec.ts
+++ src/app/shell/details/achievements/achievements.component.spec.ts
@@ -1,24 +1,34 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxsModule, Store } from '@ngxs/store';
 import { NoResultCardComponent } from 'src/app/shared/components/no-result-card/no-result-card.component';
 import { Achievement } from 'src/app/shared/models/achievement.model';
 import { AchievementsComponent } from './achievements.component';
 
+// const MockAchievement = {Achievement: typeof Achievement};
 describe('AchievementsComponent', () => {
   let component: AchievementsComponent;
   let fixture: ComponentFixture<AchievementsComponent>;
+  let store: Store;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({      
+    await TestBed.configureTestingModule({    
+      imports: [
+        NgxsModule.forRoot([]),
+      ],  
       declarations: [
         AchievementsComponent,
         MockAchievementCardComponent,
         NoResultCardComponent,
       ],
+      providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
     }).compileComponents();
   });
 
   beforeEach(() => {
+    store = TestBed.inject(Store);
+    // spyOn(store, 'dispatch').and.returnValue(MockAchievement as Achievement);
     fixture = TestBed.createComponent(AchievementsComponent);
     component = fixture.componentInstance;
     component.achievements = [];
@@ -34,5 +44,5 @@ describe('AchievementsComponent', () => {
   template: '',
 })
 class MockAchievementCardComponent {
-  @Input() achievement: Achievement;
+  @Input() achievements: Achievement[];
 }
