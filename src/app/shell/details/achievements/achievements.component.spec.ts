c3e758c6b72d621c8a149aa844cfea7f2771540e
Olya-web7 <ola.shy@yahoo.com>
2022-06-27 20:47:48 +0200

    fix

--- src/app/shell/details/achievements/achievements.component.spec.ts
+++ src/app/shell/details/achievements/achievements.component.spec.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgxsModule, Store } from '@ngxs/store';
 import { NoResultCardComponent } from 'src/app/shared/components/no-result-card/no-result-card.component';
 import { Achievement } from 'src/app/shared/models/achievement.model';
 import { AchievementsComponent } from './achievements.component';
@@ -8,13 +7,9 @@ import { AchievementsComponent } from './achievements.component';
 describe('AchievementsComponent', () => {
   let component: AchievementsComponent;
   let fixture: ComponentFixture<AchievementsComponent>;
-  let store: Store;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        NgxsModule.forRoot([]),
-      ],
+    await TestBed.configureTestingModule({      
       declarations: [
         AchievementsComponent,
         MockAchievementCardComponent,
@@ -24,10 +19,9 @@ describe('AchievementsComponent', () => {
   });
 
   beforeEach(() => {
-    store = TestBed.inject(Store);
-
     fixture = TestBed.createComponent(AchievementsComponent);
     component = fixture.componentInstance;
+    component.achievements = [];
     fixture.detectChanges();
   });
 
@@ -40,5 +34,5 @@ describe('AchievementsComponent', () => {
   template: '',
 })
 class MockAchievementCardComponent {
-  @Input() achievements: Achievement[];
+  @Input() achievement: Achievement;
 }
