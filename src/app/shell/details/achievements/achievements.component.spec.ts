c5e8484337e554dfd311d05155a3e9d52089fddd
Olya-web7 <ola.shy@yahoo.com>
2022-07-05 22:33:49 +0200

    fix

--- src/app/shell/details/achievements/achievements.component.spec.ts
+++ src/app/shell/details/achievements/achievements.component.spec.ts
@@ -1,15 +1,13 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgxsModule, Store } from '@ngxs/store';
+import { NgxsModule } from '@ngxs/store';
 import { NoResultCardComponent } from 'src/app/shared/components/no-result-card/no-result-card.component';
-import { Achievement } from 'src/app/shared/models/achievement.model';
 import { AchievementsComponent } from './achievements.component';
 
 describe('AchievementsComponent', () => {
   let component: AchievementsComponent;
   let fixture: ComponentFixture<AchievementsComponent>;
-  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({    
@@ -26,10 +24,9 @@ describe('AchievementsComponent', () => {
   });
 
   beforeEach(() => {
-    store = TestBed.inject(Store);
     fixture = TestBed.createComponent(AchievementsComponent);
     component = fixture.componentInstance;
-    component.achievements = [];
+    // component.achievements = [];
     fixture.detectChanges();
   });
 
@@ -41,6 +38,4 @@ describe('AchievementsComponent', () => {
   selector: 'app-achievement-card',
   template: '',
 })
-class MockAchievementCardComponent {
-  @Input() achievements: Achievement[];
-}
+class MockAchievementCardComponent {}
--- src/app/shell/details/achievements/achievements.component.ts
+++ src/app/shell/details/achievements/achievements.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { NoResultsTitle } from 'src/app/shared/enum/no-results';
-import { Achievement } from 'src/app/shared/models/achievement.model';
+// import { Achievement } from 'src/app/shared/models/achievement.model';
 import { Workshop } from 'src/app/shared/models/workshop.model';
 import { GetAchievementsByWorkshopId } from 'src/app/shared/store/user.actions';
 
@@ -14,11 +14,10 @@ export class AchievementsComponent implements OnInit {
   readonly noResultAchievements = NoResultsTitle.noAchievements;
 
   @Input() workshop: Workshop;
-  achievements: Achievement[];
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {    
-    this.store.dispatch(new GetAchievementsByWorkshopId(this.workshop.id));
-  }
+    // this.store.dispatch(new GetAchievementsByWorkshopId(this.workshop.id));
+  }  
 }
