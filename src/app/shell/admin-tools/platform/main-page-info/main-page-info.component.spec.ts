07f2d3c21b1e8d7d1b7132c942321834eed536c2
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-07-09 10:56:50 +0300

    Update main-page-info.component.spec.ts

--- src/app/shell/admin-tools/platform/main-page-info/main-page-info.component.spec.ts
+++ src/app/shell/admin-tools/platform/main-page-info/main-page-info.component.spec.ts
@@ -3,7 +3,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxsModule } from '@ngxs/store';
 import { MainPageInfoComponent } from './main-page-info.component';
 
 describe('MainPageInfoComponent', () => {
