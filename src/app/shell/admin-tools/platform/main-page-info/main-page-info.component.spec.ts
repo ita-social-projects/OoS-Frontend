67a6d9053d8b5cfff010f14d4e83a02ef0f02d70
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-07-09 10:56:40 +0300

    Update main-page-info.component.spec.ts

--- src/app/shell/admin-tools/platform/main-page-info/main-page-info.component.spec.ts
+++ src/app/shell/admin-tools/platform/main-page-info/main-page-info.component.spec.ts
@@ -1,5 +1,9 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxsModule } from '@ngxs/store';
 import { MainPageInfoComponent } from './main-page-info.component';
 
 describe('MainPageInfoComponent', () => {
@@ -8,13 +12,19 @@ describe('MainPageInfoComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports:[
+        MatIconModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+      ],
       declarations: [ MainPageInfoComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MainPageInfoComponent);
+    fixture = TestBed.createComponent(InfoCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
