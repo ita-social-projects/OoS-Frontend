c42e51000b4f9abcc491507a7af2efc6b178b3f4
Olya-web7 <ola.shy@yahoo.com>
2022-07-10 20:19:13 +0200

    fix

--- src/app/shell/details/achievements/achievements.component.spec.ts
+++ src/app/shell/details/achievements/achievements.component.spec.ts
@@ -1,32 +1,31 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { NgxsModule, Store } from '@ngxs/store';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxsModule } from '@ngxs/store';
 import { NoResultCardComponent } from 'src/app/shared/components/no-result-card/no-result-card.component';
-import { Achievement } from 'src/app/shared/models/achievement.model';
 import { AchievementsComponent } from './achievements.component';
 
 describe('AchievementsComponent', () => {
   let component: AchievementsComponent;
   let fixture: ComponentFixture<AchievementsComponent>;
-  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({    
       imports: [
         NgxsModule.forRoot([]),
+        MatIconModule,
+        MatCardModule,
+        RouterTestingModule
       ],  
       declarations: [
         AchievementsComponent,
-        MockAchievementCardComponent,
         NoResultCardComponent,
       ],
-      providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
     }).compileComponents();
   });
 
   beforeEach(() => {
-    store = TestBed.inject(Store);
     fixture = TestBed.createComponent(AchievementsComponent);
     component = fixture.componentInstance;
     component.achievements = [];
@@ -37,10 +36,3 @@ describe('AchievementsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-@Component({
-  selector: 'app-achievement-card',
-  template: '',
-})
-class MockAchievementCardComponent {
-  @Input() achievements: Achievement[];
-}
