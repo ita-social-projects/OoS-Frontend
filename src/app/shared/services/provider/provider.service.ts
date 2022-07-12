40c0bba1697daaccf72504cfdeaa5612e00f3d31
perepichai <o.perepichai@gmail.com>
2022-07-12 22:03:39 +0300

    service v1 v2

--- src/app/shared/services/provider/provider.service.ts
+++ src/app/shared/services/provider/provider.service.ts
@@ -1,14 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
+import { FeaturesList } from '../../models/featuresList.model';
 import { InstitutionStatus } from '../../models/institutionStatus.model';
 import { Provider } from '../../models/provider.model';
+import { MetaDataState } from '../../store/meta-data.state';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProviderService {
-  constructor(private http: HttpClient) {}
+  isRelease2: boolean;
+  constructor(private http: HttpClient,
+    private store: Store) {}
 
   /**
    * This method get Providers from the database
@@ -31,6 +36,13 @@ export class ProviderService {
    * @param Provider
    */
   createProvider(provider: Provider): Observable<object> {
+    this.isRelease2 = this.store.selectSnapshot<FeaturesList>(MetaDataState.featuresList).release2;
+    return this.isRelease2 ? this.createProviderV2(provider) : this.createProviderV1(provider);
+  }
+  createProviderV1(provider: Provider): Observable<object> {
+    return this.http.post('/api/v1/Provider/Create', provider);
+  }
+  createProviderV2(provider: Provider): Observable<object> {
     const formData = Provider.createFormData(provider);
     return this.http.post('/api/v2/Provider/Create', formData);
   }
@@ -47,6 +59,13 @@ export class ProviderService {
    * @param Provider
    */
   updateProvider(provider: Provider): Observable<object> {
+    this.isRelease2 = this.store.selectSnapshot<FeaturesList>(MetaDataState.featuresList).release2;
+    return this.isRelease2 ? this.updateProviderV1(provider) : this.updateProviderV2(provider);
+  }
+  updateProviderV1(provider: Provider): Observable<object> {
+    return this.http.put('/api/v1/Provider/Update', provider);
+  }
+  updateProviderV2(provider: Provider): Observable<object> {
     const formData = Provider.createFormData(provider);
     return this.http.put('/api/v2/Provider/Update', formData);
   }
