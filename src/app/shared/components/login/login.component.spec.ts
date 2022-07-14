14a8030c89eed76e61a05260eb4863bab14d2181
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-07-14 11:19:33 +0300

    Update login.component.spec.ts

--- src/app/shared/components/login/login.component.spec.ts
+++ src/app/shared/components/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgxsModule } from '@ngxs/store';
 import { LoginComponent } from './login.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -9,7 +10,9 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [NgxsModule.forRoot([])],
+      providers: [
+        RouterTestingModule,
+        NgxsModule.forRoot([])],
     }).compileComponents();
   });
 
