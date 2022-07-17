acda253196ad453be8137735f92e555a9626af20
Olya <ola.shy@yahoo.com>
2022-07-17 11:05:47 +0200

    fix achievements cards

--- src/app/shared/components/achievement-card/achievement-card.component.ts
+++ src/app/shared/components/achievement-card/achievement-card.component.ts
@@ -9,33 +9,38 @@ import { RegistrationState } from '../../store/registration.state';
 import { GetWorkshopsByProviderId } from '../../store/user.actions';
 import { UserState } from '../../store/user.state';
 import { Provider } from 'src/app/shared/models/provider.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-achievement-card',
   templateUrl: './achievement-card.component.html',
-  styleUrls: ['./achievement-card.component.scss']
+  styleUrls: ['./achievement-card.component.scss'],
 })
 export class AchievementCardComponent implements OnInit {
   @Select(UserState.workshops) workshops: Observable<Workshop[]>;
-  destroy$: Subject<boolean> = new Subject<boolean>();  
+  destroy$: Subject<boolean> = new Subject<boolean>();
   @Input() achievement: Achievement;
   @Input() workshop: Workshop;
   readonly achievementsTitle = AchievementsTitle;
   showMore = false;
   auth: boolean;
   provider: Provider;
+  workshopId: string;
 
-  constructor(private store: Store) { }
+  constructor(private store: Store, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.workshopId = this.route.snapshot.paramMap.get('id');
+    this.provider = this.store.selectSnapshot<Provider>(
+      RegistrationState.provider
+    );
     this.auth = this.store.selectSnapshot<boolean>(RegistrationState.isAuthorized);
-    this.store.dispatch(new GetWorkshopsByProviderId(this.provider?.id));  
+    this.store.dispatch(new GetWorkshopsByProviderId(this.provider?.id));    
   }
 
   getWorkshopIds(arr): string[] {
-    const res = []
+    const res = [];
     arr.forEach((i) => res.push(i.workshopId));
     return res;
   }
-
 }
