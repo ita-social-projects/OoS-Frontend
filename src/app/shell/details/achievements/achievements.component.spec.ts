8ba14dbb402ad87618eccf8898e76ad807fe8d0b
Olya <ola.shy@yahoo.com>
2022-07-17 12:28:19 +0200

    test achievements

--- src/app/shell/details/achievements/achievements.component.spec.ts
+++ src/app/shell/details/achievements/achievements.component.spec.ts
@@ -1,3 +1,4 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -5,6 +6,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxsModule } from '@ngxs/store';
 import { NoResultCardComponent } from 'src/app/shared/components/no-result-card/no-result-card.component';
+import { Achievement } from 'src/app/shared/models/achievement.model';
 import { AchievementsComponent } from './achievements.component';
 
 describe('AchievementsComponent', () => {
@@ -12,17 +14,18 @@ describe('AchievementsComponent', () => {
   let fixture: ComponentFixture<AchievementsComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({    
+    await TestBed.configureTestingModule({
       imports: [
         NgxsModule.forRoot([]),
         MatIconModule,
         MatCardModule,
         RouterTestingModule,
-        MatDialogModule
-      ],  
+        MatDialogModule,
+      ],
       declarations: [
         AchievementsComponent,
         NoResultCardComponent,
+        MockAchievementCardComponent,
       ],
     }).compileComponents();
   });
@@ -37,3 +40,10 @@ describe('AchievementsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+@Component({
+  selector: 'app-achievement-card',
+  template: '',
+})
+class MockAchievementCardComponent {
+  @Input() achievement: Achievement;
+}
