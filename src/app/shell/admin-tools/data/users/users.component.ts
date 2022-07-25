ca06ee5b68a758ccc81cd4c0fd5bba0b1e66c52c
Illya97 <ovsannikovillya@gmail.com>
2022-07-25 14:28:23 +0300

    some fix

--- src/app/shell/admin-tools/data/users/users.component.ts
+++ src/app/shell/admin-tools/data/users/users.component.ts
@@ -123,9 +123,6 @@ export class UsersComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
-    this.store.dispatch([
-      new GetChildrenForAdmin(), 
-      new PopNavPath()
-    ]);
+    this.store.dispatch(new PopNavPath());
   }
 }
