f51d5123d2e537e079a87daf905149757a7b53ff
Olya-web7 <ola.shy@yahoo.com>
2022-07-25 13:17:00 +0200

    add fix for code

--- src/app/shared/components/achievement-card/achievement-card.component.ts
+++ src/app/shared/components/achievement-card/achievement-card.component.ts
@@ -1,13 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Subject } from '@microsoft/signalr';
-import { Store } from '@ngxs/store';
 import { AchievementsTitle } from '../../constants/constants';
 import { Achievement } from '../../models/achievement.model';
 import { Workshop } from '../../models/workshop.model';
-import { RegistrationState } from '../../store/registration.state';
-import { Provider } from 'src/app/shared/models/provider.model';
-import { ActivatedRoute } from '@angular/router';
-import { Role } from '../../enum/role';
 
 @Component({
   selector: 'app-achievement-card',
@@ -15,10 +9,12 @@ import { Role } from '../../enum/role';
   styleUrls: ['./achievement-card.component.scss'],
 })
 export class AchievementCardComponent implements OnInit {
+  readonly achievementsTitle = AchievementsTitle;
+
   @Input() achievement: Achievement;
   @Input() workshop: Workshop;
   @Input() isAllowedEdit: boolean;
-  readonly achievementsTitle = AchievementsTitle;
+  
   showMore = false;
   
   constructor() {}
