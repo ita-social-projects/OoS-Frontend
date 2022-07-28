16b6061168493e60ff3267a82e3e535187ef7b16
Olya-web7 <ola.shy@yahoo.com>
2022-07-28 21:41:20 +0200

    fix error with route after achievement create

--- src/app/shared/store/user.state.ts
+++ src/app/shared/store/user.state.ts
@@ -745,7 +745,7 @@ export class UserState {
       new ShowMessageBar({ message: 'Досягнення видалено!', type: 'success' }),
       new GetUsersChildren(),
     ]);
-    this.router.navigate(['/details/workshop', payload.body.workshopId]);
+    this.router.navigate(['/details/workshop', payload.workshopId]);
   }
 
   @Action(OnDeleteChildFail)
