8a6b9cccb1514717e5b9bb2a36d05f672bf5dd51
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-08-12 16:38:46 +0300

    view

--- src/app/shared/components/filters-list/city-filter/city-filter.component.ts
+++ src/app/shared/components/filters-list/city-filter/city-filter.component.ts
@@ -1,42 +1,110 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subject } from 'rxjs';
-import { debounce, takeUntil, debounceTime } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, startWith, takeUntil, tap } from 'rxjs/operators';
+import { Constants } from 'src/app/shared/constants/constants';
 import { City } from 'src/app/shared/models/city.model';
+import { Codeficator } from 'src/app/shared/models/codeficator.model';
 import { FilterState } from 'src/app/shared/store/filter.state';
+import { ClearCodeficatorSearch, GetCodeficatorSearch } from 'src/app/shared/store/meta-data.actions';
+import { MetaDataState } from 'src/app/shared/store/meta-data.state';
 import { SetCity } from '../../../store/filter.actions';
 
 @Component({
   selector: 'app-city-filter',
   templateUrl: './city-filter.component.html',
-  styleUrls: ['./city-filter.component.scss']
+  styleUrls: ['./city-filter.component.scss'],
 })
-export class CityFilterComponent implements OnInit, OnDestroy{
+export class CityFilterComponent implements OnInit, OnDestroy {
+  readonly Constants = Constants;
+
   @Select(FilterState.isConfirmCity)
   isConfirmCity$: Observable<boolean>;
-  @Select(FilterState.city)
-  city$: Observable<City>;
+  @Select(MetaDataState.codeficatorSearch)
+  codeficatorSearch$: Observable<Codeficator[]>;
+
   city: City;
 
-  cityFormControl = new FormControl();
+  settlementSearchControl = new FormControl('');
+  settlementControl = new FormControl('');
+
   destroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor(private store: Store) { }
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.city$.pipe(
-      debounceTime(1000),
-      takeUntil(this.destroy$)
-    ).subscribe((city: City) => this.city = city);
+    this.settlementListener();
+    this.codeficatorSearch$.subscribe(val=> console.log(val))
+    // this.city$.pipe(debounceTime(1000), takeUntil(this.destroy$)).subscribe((city: City) => (this.city = city));
+  }
+
+  /**
+   * This method listen input changes and handle search
+   */
+  private settlementListener(): void {
+    this.settlementSearchControl.valueChanges
+      .pipe(
+        takeUntil(this.destroy$),
+        debounceTime(500),
+        distinctUntilChanged(),
+        startWith(''),
+        tap((value: string) => {
+          if (!value?.length) {
+            this.store.dispatch(new ClearCodeficatorSearch());
+          }
+        }),
+        filter((value: string) => value?.length > 2)
+      )
+      .subscribe((value: string) => {
+        console.log(value)
+        this.store.dispatch(new GetCodeficatorSearch(value))}
+      );
   }
 
   onSelectedCity(city: City): void {
     this.store.dispatch(new SetCity(city));
   }
 
-  OnFocusout(): void {
-    this.cityFormControl.setValue(this.city);
+  /**
+   * This method listen input FocusOut event and update search and settlement controls value
+   * @param auto MatAutocomplete
+   * @param controls
+   *  searchControl FormControl
+   *  settlementControl FormControl
+   *  codeficatorIdControl FormControl
+   */
+  onFocusOut(auto: MatAutocomplete): void {
+    console.log(auto)
+    // const codeficator: Codeficator = auto.options.first?.value;
+    // if (!this.settlementSearchControl.value || codeficator?.settlement === Constants.NO_SETTLEMENT) {
+    //   this.settlementSearchControl.setValue(null);
+    // } else {
+    //   this.settlementSearchControl.setValue(this.settlementControl.value?.settlement);
+    // }
+  }
+
+  /**
+   * This method listen mat option select event and save settlement control value
+   * @param event MatAutocompleteSelectedEvent
+   * @param controls
+   *  searchControl FormControl
+   *  settlementControl FormControl
+   *  codeficatorIdControl FormControl
+   */
+  onSelectSettlement(event: MatAutocompleteSelectedEvent): void {
+    this.store.dispatch(new ClearCodeficatorSearch());
+    this.settlementSearchControl.setValue(event.option.value.settlement, { emitEvent: false, onlySelf: true });
+    this.settlementControl.setValue(event.option.value, { emitEvent: false, onlySelf: true });
+  }
+
+  /**
+   * This method handle displayed value for mat-autocomplete dropdown
+   * @param codeficator: Codeficator | string
+   */
+  displaySettlementNameFn(codeficator: Codeficator | string): string {
+    return typeof codeficator === 'string' ? codeficator : codeficator?.settlement;
   }
 
   ngOnDestroy(): void {
