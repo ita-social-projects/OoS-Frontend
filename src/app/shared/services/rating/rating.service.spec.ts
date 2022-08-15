96a5ba71e2cc9b99ffb77fb43b174d45ced6cb7e
Anastasiia Derkach <aderk@softserveinc.com>
2022-08-15 11:43:21 +0300

    fix tests

--- src/app/shared/services/rating/rating.service.spec.ts
+++ src/app/shared/services/rating/rating.service.spec.ts
@@ -2,13 +2,15 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { RatingService } from './rating.service';
+import { NgxsModule } from '@ngxs/store';
 
 describe('RatingService', () => {
   let service: RatingService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule,
+        NgxsModule.forRoot([]), ],
     });
     service = TestBed.inject(RatingService);
   });
