d5bba7cb5acae63ca1deda21cd94d69bb637afc4
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-08-22 23:06:07 +0300

    revert

--- src/app/shared/components/image-cropper-modal/image-cropper-modal.component.ts
+++ src/app/shared/components/image-cropper-modal/image-cropper-modal.component.ts
@@ -36,7 +36,6 @@ export class ImageCropperModalComponent {
 
   imageLoaded(image: LoadedImage): void {
     const { height, width } = image.original.size;   
-    console.log(height, width)
     this.invalidMinRequirements = (height < this.data.cropperConfig.cropperMinHeight || width < this.data.cropperConfig.cropperMinWidth);
   }
 
