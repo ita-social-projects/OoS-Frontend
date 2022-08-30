ec06492bdeab53eeec9690fb8d02a6c1d0b5c0d7
Oleksandra Lytvynets <alitvinets7@gmail.com>
2022-08-30 18:02:37 +0300

    map sync with codeficator search (#1608)

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -23,11 +23,9 @@ import { FilterState } from '../../store/filter.state';
 })
 export class MapComponent implements AfterViewInit, OnDestroy {
   @Input() addressFormGroup: FormGroup;
-  @Input() isCreateWorkShops: boolean;
   @Input() filteredWorkshops$: Observable<WorkshopFilterCard>;
 
-  @Output() wrongMapAddress = new EventEmitter<boolean>();
-  @Output() selectedAddress = new EventEmitter<any>();
+  @Output() addressSelect = new EventEmitter<Geocoder>();
 
   @Select(UserState.selectedWorkshop)
   selectedWorkshop$: Observable<Workshop>;
@@ -108,11 +106,7 @@ export class MapComponent implements AfterViewInit, OnDestroy {
     }
 
     this.addressFormGroup.valueChanges
-      .pipe(
-        debounceTime(500),
-        takeUntil(this.destroy$),
-        filter((address: Geocoder) => !!(address.longitude && address.latitude))
-      )
+      .pipe(debounceTime(500), takeUntil(this.destroy$))
       .subscribe((address: Geocoder) => this.addressDecode(address));
   }
 
@@ -120,14 +114,14 @@ export class MapComponent implements AfterViewInit, OnDestroy {
    * changing position on map
    * @param coords:Coords
    */
-  flyTo(coords: Coords): void {
+  private flyTo(coords: Coords): void {
     this.map?.flyTo(coords, this.zoom);
   }
 
   /**
    * method init start position on map
    */
-  initMap(): void {
+  private initMap(): void {
     this.map = Layer.map('map').setView(this.defaultCoords, this.zoom);
 
     Layer.tileLayer('https://tms{s}.visicom.ua/2.0.0/ua/base/{z}/{x}/{y}.png', {
@@ -141,7 +135,7 @@ export class MapComponent implements AfterViewInit, OnDestroy {
     this.map.on('click', (L: Layer.LeafletMouseEvent) => {
       if (this.workshops) {
         this.unselectMarkers();
-        this.selectedAddress.emit(null);
+        this.addressSelect.emit(null);
       } else {
         this.setMapLocation(L.latlng);
       }
@@ -149,10 +143,10 @@ export class MapComponent implements AfterViewInit, OnDestroy {
   }
 
   /**
-   * uses GoelocationService to translate address into coords and sets marker on efault
+   * uses GoelocationService to translate address into coords and sets marker on default
    * @param address - type Address
    */
-  setAddressLocation(address: Address): void {
+  private setAddressLocation(address: Address): void {
     this.workshops
       ? this.setWorkshopMarkers(address)
       : this.setNewSingleMarker([address.codeficatorAddressDto.latitude, address.codeficatorAddressDto.longitude]);
@@ -170,7 +164,7 @@ export class MapComponent implements AfterViewInit, OnDestroy {
         this.addressFormGroup.reset();
         this.map.removeLayer(this.singleMarker);
       }
-      this.wrongMapAddress.emit(!result);
+      this.addressSelect.emit(result);
     });
   }
 
@@ -178,8 +172,9 @@ export class MapComponent implements AfterViewInit, OnDestroy {
     this.geocoderService.addressDecode(address, (result: Geocoder) => {
       if (result) {
         this.setNewSingleMarker([result.latitude, result.longitude]);
+      } else {
+        this.map.removeLayer(this.singleMarker);
       }
-      this.wrongMapAddress.emit(!result);
     });
   }
 
@@ -198,7 +193,7 @@ export class MapComponent implements AfterViewInit, OnDestroy {
    * This method remove existed marker and set the new marke to the map
    * @param coords - type [number, number]
    */
-  setWorkshopMarkers(address: Address): void {
+  private setWorkshopMarkers(address: Address): void {
     const coords: [number, number] = [address.codeficatorAddressDto.latitude, address.codeficatorAddressDto.longitude];
     const marker = this.createMarker(coords, false);
     this.map.addLayer(marker);
@@ -209,14 +204,14 @@ export class MapComponent implements AfterViewInit, OnDestroy {
       const targetMarker = this.workshopMarkers.find(workshopMarker => workshopMarker.marker === event.target);
       targetMarker.isSelected = true;
       targetMarker.marker.setIcon(this.selectedMarkerIcon);
-      this.selectedAddress.emit({
-        ...address,
-        codeficatorAddressDto: address.codeficatorAddressDto,
-      });
+      // this.selectedAddress.emit({
+      //   ...address,
+      //   codeficatorAddressDto: address.codeficatorAddressDto,
+      // });
     });
   }
 
-  setPrevWorkshopMarker(): void {
+  private setPrevWorkshopMarker(): void {
     this.selectedWorkshop$
       .pipe(
         filter((workshop: Workshop) => !!workshop),
@@ -234,17 +229,17 @@ export class MapComponent implements AfterViewInit, OnDestroy {
           targetMarker.isSelected = true;
           targetMarker.marker.setIcon(this.selectedMarkerIcon);
         });
-        this.selectedAddress.emit({
-          ...workshop.address,
-          codeficatorAddressDto: workshop.address.codeficatorAddressDto,
-        });
+        // this.selectedAddress.emit({
+        //   ...workshop.address,
+        //   codeficatorAddressDto: workshop.address.codeficatorAddressDto,
+        // });
       });
   }
 
   /**
    * This method unselect target Marker
    */
-  unselectMarkers(): void {
+  private unselectMarkers(): void {
     const selectedWorkshopMarker = this.workshopMarkers.filter(
       (workshopMarker: WorkshopMarker) => workshopMarker.isSelected
     );
@@ -262,7 +257,7 @@ export class MapComponent implements AfterViewInit, OnDestroy {
    * @param draggable - type boolean
    * @param address - type Address
    */
-  createMarker(coords: [number, number], draggable: boolean = true): Layer.Marker {
+  private createMarker(coords: [number, number], draggable: boolean = true): Layer.Marker {
     return new Layer.Marker(coords, { draggable, icon: this.unselectedMarkerIcon, riseOnHover: true });
   }
 
