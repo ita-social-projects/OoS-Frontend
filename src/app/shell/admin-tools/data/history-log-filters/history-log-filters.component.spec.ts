e368f6a568c2c756407d10aed6af070a9f9fd938
Anastasiia Derkach <aderk@softserveinc.com>
2022-09-07 15:57:39 +0300

    fix tests

--- src/app/shell/admin-tools/data/history-log-filters/history-log-filters.component.spec.ts
+++ src/app/shell/admin-tools/data/history-log-filters/history-log-filters.component.spec.ts
@@ -1,9 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatNativeDateModule } from '@angular/material/core';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { HistoryLogFiltersComponent } from './history-log-filters.component';
 
 describe('HistoryLogFiltersComponent', () => {
@@ -17,6 +18,8 @@ describe('HistoryLogFiltersComponent', () => {
         MatDatepickerModule,
         MatNativeDateModule,
         MatInputModule,
+        MatOptionModule,
+        MatSelectModule,
         ReactiveFormsModule,
         FormsModule
       ],
