2a6452213c0d40b67e451379f3d67d9182068b42
LeraKornienko <makarenkolera460@gmail.com>
2022-09-07 14:23:53 +0300

    fix

--- src/app/shared/pipes/text-transform.pipe.ts
+++ src/app/shared/pipes/text-transform.pipe.ts
@@ -9,8 +9,8 @@ export class TextTransformPipe implements PipeTransform {
   transform(text: string): string {
     if (text.length > 10 && !text.includes(" ")) {
       const result = []
-      for (let i = 0; i < text.length; i+=90) {
-        result.push(text.slice(i, i + 90));
+      for (let i = 0; i < text.length; i+=97) {
+        result.push(text.slice(i, i + 97));
       }
       const resultString = result.toString().replace(/,/gi, ' ');
       return resultString
