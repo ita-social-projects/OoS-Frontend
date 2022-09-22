d0c9f5b1084cdeb6731f0a4b0d1b6438f594de96
Kolia <patskalWork@gmail.com>
2022-09-22 17:30:26 +0300

    Fixed tests

--- src/app/shared/directives/stretch-table/stretch-table.directive.spec.ts
+++ src/app/shared/directives/stretch-table/stretch-table.directive.spec.ts
@@ -1,8 +1,14 @@
+import { ComponentFactoryResolver, ElementRef, Renderer2, ViewContainerRef } from '@angular/core';
 import { StretchTableDirective } from './stretch-table.directive';
 
 describe('StretchTableDirective', () => {
+
   it('should create an instance', () => {
-    const directive = new StretchTableDirective();
+    let el: ElementRef;
+    let renderer: Renderer2;
+    let cfr: ComponentFactoryResolver;
+    let viewContainerRef: ViewContainerRef;
+    const directive = new StretchTableDirective(el, cfr,renderer, viewContainerRef);
     expect(directive).toBeTruthy();
   });
 });
