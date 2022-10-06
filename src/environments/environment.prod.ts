772fccbda6289081c7cfef9053e344610f185fa4
Y-Taras <ytaras@gmail.com>
2022-10-06 17:34:05 +0300

    add husky pre-comit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,4 @@ export const environment = {
   stsServer: 'https://auth.oos.dmytrominochkin.cloud',
   serverUrl: 'https://api.oos.dmytrominochkin.cloud',
   storageUrl: 'https://storage.googleapis.com/oos-images-63325/',
-}
+};
