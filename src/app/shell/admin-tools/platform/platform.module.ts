6d0f3998535f4aa8c8f33075c447afc64796412a
Kolia <patskalWork@gmail.com>
2022-10-09 13:21:13 +0300

    Removed main-page-info-component
    * Removed the main-page-info-component that was replaced by the  main-info.component

--- src/app/shell/admin-tools/platform/platform.module.ts
+++ src/app/shell/admin-tools/platform/platform.module.ts
@@ -10,7 +10,6 @@ import { RegulationsInfoComponent } from './platform-info/regulations-info.compo
 import { SupportInfoComponent } from './platform-info/support-info.component';
 import { AboutInfoComponent } from './platform-info/about-info.component';
 import { InfoEditComponent } from './platform-info/info-edit/info-edit.component';
-import { MainPageInfoComponent } from './main-page-info/main-page-info.component';
 import { SharedModule } from '../../../shared/shared.module';
 import { MaterialModule } from '../../../shared/modules/material.module';
 import { MainInfoComponent } from './platform-info/main-info.component';
@@ -25,7 +24,6 @@ import { MainInfoComponent } from './platform-info/main-info.component';
     SupportInfoComponent,
     InfoEditComponent,
     PlatformComponent,
-    MainPageInfoComponent,
     MainInfoComponent,
   ],
   imports: [
