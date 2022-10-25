920d3fdab4840843df32b5aa98c21206bc12f7f9
LeraKornienko <makarenkolera460@gmail.com>
2022-10-25 14:25:43 +0300

    mobile size

--- src/app/shared/pipes/text-slice-transform.pipe.ts
+++ src/app/shared/pipes/text-slice-transform.pipe.ts
@@ -3,14 +3,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
   name: 'textSliceTransform'
 })
-export class TextSliceTransformPipe implements PipeTransform {
 
+export class TextSliceTransformPipe implements PipeTransform {
 
   transform(text: string): string {
     if (text.length > 10) {
       const result = [];
-      for (let i = 0; i < text.length; i+=97) {
-        result.push(text.slice(i, i + 97));
+      if (screen.width < 750) {
+        for (let i = 0; i < text.length; i += 47) {
+          result.push(text.slice(i, i + 47 ));
+        }
+      } else {
+        for (let i = 0; i < text.length; i += 97) {
+          result.push(text.slice(i, i + 97));
+        }
       }
       const resultString = result.toString().replace(/,/gi, '- ');
       return resultString;
