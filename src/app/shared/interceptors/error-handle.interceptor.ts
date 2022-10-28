78eb27c74b528de45181dfa73857d2af7aa7e49e
Illya97 <ovsannikovillya@gmail.com>
2022-10-28 13:00:05 +0300

    create errors interceptor'

--- src/app/shared/interceptors/error-handle.interceptor.ts
+++ src/app/shared/interceptors/error-handle.interceptor.ts
@@ -8,10 +8,13 @@ import {
   HttpErrorResponse
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
+import { ShowMessageBar } from '../store/app.actions';
+import { SnackbarText } from '../enum/messageBar';
+import { Store } from '@ngxs/store';
 
 @Injectable()
 export class ErrorHandleInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private store: Store) {}
 
   intercept(
     request: HttpRequest<unknown>,
@@ -21,16 +24,43 @@ export class ErrorHandleInterceptor implements HttpInterceptor {
       catchError((error: HttpErrorResponse) => {
         let errorMsg = '';
         switch (error.status) {
+          case 401:
+            errorMsg = 'Unauthorized';
+            this.store.dispatch(
+              new ShowMessageBar({
+                message: SnackbarText.error401,
+                type: 'error'
+              })
+            );
+            break;
           case 404:
-            errorMsg = 'Not Found';
+            errorMsg = `URL NOT FOUND: ${error.url}`;
+            this.store.dispatch(
+              new ShowMessageBar({
+                message: SnackbarText.error404,
+                type: 'error'
+              })
+            );
             break;
           case 403:
-            errorMsg = 'Forbidden';
-          default:
-            errorMsg = 'Unknown';
+            errorMsg = 'Action is forbidden for this user';
+            this.store.dispatch(
+              new ShowMessageBar({
+                message: SnackbarText.error403,
+                type: 'error'
+              })
+            );
+          case 500:
+            errorMsg = 'Internal Server Error';
+            this.store.dispatch(
+              new ShowMessageBar({
+                message: SnackbarText.error500,
+                type: 'error'
+              })
+            );
             break;
         }
-        return throwError(errorMsg);
+        return throwError(() => new Error(errorMsg));
       })
     );
   }
