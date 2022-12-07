4115c088a26c1df72c7389f20af1d0adc44bb6b4
Mykhailo Khrystenko <khrystenko.m.y@gmail.com>
2022-12-07 12:55:42 +0200

    Localization has been provided for Nav, and for two tabs in personal information

--- src/app/shell/personal-cabinet/personal-cabinet.module.ts
+++ src/app/shell/personal-cabinet/personal-cabinet.module.ts
@@ -8,10 +8,11 @@ import { UserConfigComponent } from './shared-cabinet/user-config/user-config.co
 import { UserConfigEditComponent } from './shared-cabinet/user-config/user-config-edit/user-config-edit.component';
 import { SharedCabinetModule } from './shared-cabinet/shared-cabinet.module';
 import { SharedModule } from '../../shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [UserConfigComponent, UserConfigEditComponent],
-  imports: [CommonModule, PersonalCabinetRoutingModule, FlexLayoutModule, SharedModule, SharedCabinetModule],
+  imports: [CommonModule, PersonalCabinetRoutingModule, FlexLayoutModule, SharedModule, SharedCabinetModule, TranslateModule],
   providers: [ParentGuard, ProviderGuard]
 })
 export class PersonalCabinetModule {}
