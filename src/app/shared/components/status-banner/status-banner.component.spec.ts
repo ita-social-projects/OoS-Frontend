e8ba30ec38fe991fb90ff2d1b46e4b1a1b564a4e
patskal <patskalWork@gmail.com>
2022-12-17 18:06:53 +0200

    Fixed tests

--- src/app/shared/components/status-banner/status-banner.component.spec.ts
+++ src/app/shared/components/status-banner/status-banner.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxsModule } from '@ngxs/store';
 
 import { StatusBannerComponent } from './status-banner.component';
 
@@ -8,6 +9,7 @@ describe('FullWidthBannerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [NgxsModule.forRoot([])],
       declarations: [StatusBannerComponent]
     }).compileComponents();
 
