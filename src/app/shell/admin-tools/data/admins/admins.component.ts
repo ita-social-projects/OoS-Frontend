f2420843af817b3ca5e6da4d6592522d36c06137
patskal <patskalWork@gmail.com>
2022-12-24 12:05:21 +0200

    Comments added

--- src/app/shell/admin-tools/data/admins/admins.component.ts
+++ src/app/shell/admin-tools/data/admins/admins.component.ts
@@ -125,14 +125,21 @@ export class AdminsComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * This method determines whether the user needs to display actions column depending on his role.
+   */
   private setDisplayedColumns(): void {
     const isActionsInList = this.displayedColumns.includes('actions');
 
+    // If the user is a technical administrator and he already has the actions column in the list, exit
     if (this.role === Role.techAdmin && isActionsInList) {
       return;
     }
 
+    // If the user is the ministryAdmin, we check whether he needs to display action column on this table.
     if (this.role === Role.ministryAdmin) {
+      //If a table is selected that the user cannot edit and the actions column is added - delete column
+      //If a table is selected that the user can edit and the actions column is added, exit.
       if (this.adminParams.tabTitle === AdminRoleUkr.ministryAdmin && isActionsInList) {
         this.displayedColumns = this.displayedColumns.filter((value: string) => value !== 'actions');
         return;
@@ -141,6 +148,7 @@ export class AdminsComponent implements OnInit, OnDestroy {
       }
     }
 
+    //In all other cases, add an actions column
     this.displayedColumns.push('actions');
   }
 
