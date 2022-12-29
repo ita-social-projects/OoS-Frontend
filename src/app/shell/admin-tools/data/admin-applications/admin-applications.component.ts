f93e11309f735d7f5fad5c83a98139f4f2e417b9
patskal <patskalWork@gmail.com>
2022-12-29 15:11:20 +0200

    Added a search bar for applications
    * Added a search bar for applications
    * Extended the class of admin applications with the help of the personal cabinet component
    * Minor styles changes

--- src/app/shell/admin-tools/data/admin-applications/admin-applications.component.ts
+++ src/app/shell/admin-tools/data/admin-applications/admin-applications.component.ts
@@ -3,13 +3,17 @@ import { Store } from '@ngxs/store';
 import { GetAllApplications } from '../../../../shared/store/shared-user.actions';
 import { ApplicationEntityType } from '../../../../shared/enum/applications';
 import { ApplicationFilterParameters } from '../../../../shared/models/application.model';
+import { CabinetDataComponent } from '../../../../shell/personal-cabinet/shared-cabinet/cabinet-data.component';
+import { MatDialog } from '@angular/material/dialog';
+import { NavBarName } from '../../../../shared/enum/navigation-bar';
+import { PushNavPath } from '../../../../shared/store/navigation.actions';
 
 @Component({
   selector: 'app-admin-applications',
   templateUrl: './admin-applications.component.html',
   styleUrls: ['./admin-applications.component.scss']
 })
-export class AdminApplicationsComponent implements OnInit {
+export class AdminApplicationsComponent extends CabinetDataComponent implements OnInit {
   applicationParams: ApplicationFilterParameters = {
     property: ApplicationEntityType.provider,
     statuses: [],
@@ -18,13 +22,25 @@ export class AdminApplicationsComponent implements OnInit {
     showBlocked: false
   };
 
-  constructor(private store: Store) {}
+  constructor(protected store: Store, protected matDialog: MatDialog) {
+    super(store, matDialog);
+  }
 
-  ngOnInit(): void {
+  init(): void {
     this.onGetApplications();
   }
 
   onGetApplications(): void {
     this.store.dispatch(new GetAllApplications(this.applicationParams));
+  }
+
+  protected addNavPath(): void {
+    this.store.dispatch(
+      new PushNavPath({
+        name: NavBarName.Applications,
+        isActive: false,
+        disable: true
+      })
+    );
   }
 }
