b4fcaa92e31cee804445cdef8839abb07dac329c
patskal <patskalWork@gmail.com>
2022-12-30 14:09:16 +0200

    Update admin-applications.component.spec.ts

--- src/app/shell/admin-tools/data/admin-applications/admin-applications.component.spec.ts
+++ src/app/shell/admin-tools/data/admin-applications/admin-applications.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { NgxsModule } from '@ngxs/store';
 
 import { AdminApplicationsComponent } from './admin-applications.component';
 
@@ -8,9 +10,9 @@ describe('AdminApplicationsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AdminApplicationsComponent ]
-    })
-    .compileComponents();
+      declarations: [AdminApplicationsComponent],
+      imports: [NgxsModule.forRoot([]), MatDialogModule]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AdminApplicationsComponent);
     component = fixture.componentInstance;
