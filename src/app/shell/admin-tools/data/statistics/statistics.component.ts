e0fd8d0c0d1c6466dd73eb52666732bf39922dd4
patskal <patskalWork@gmail.com>
2023-01-06 13:08:58 +0200

    Added logic to statistics class
    * Added getting information from storage for dropdowns
    * Added form controls for form elements
    * Updated data model Codeficator

--- src/app/shell/admin-tools/data/statistics/statistics.component.ts
+++ src/app/shell/admin-tools/data/statistics/statistics.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Select, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { Codeficator } from 'src/app/shared/models/codeficator.model';
+import { Institution } from 'src/app/shared/models/institution.model';
+import { GetAllInstitutions, GetCodeficatorSearch } from 'src/app/shared/store/meta-data.actions';
+import { MetaDataState } from 'src/app/shared/store/meta-data.state';
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +13,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
+  @Select(MetaDataState.institutions)
+  institutions$: Observable<Institution[]>;
 
-  constructor() { }
+  @Select(MetaDataState.codeficatorSearch)
+  codeficatorSearch$: Observable<Codeficator[]>;
+
+  filtersForm: FormGroup;
+  isReport = false;
+
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
+    this.store.dispatch([new GetAllInstitutions(true), new GetCodeficatorSearch('')]);
+
+    this.filtersForm = this.fb.group({
+      dateFrom: new FormControl(''),
+      dateTo: new FormControl(''),
+      institutions: new FormControl(''),
+      regions: new FormControl(''),
+      otgs: new FormControl('')
+    });
   }
 
+  onGenerateReport(): void {
+    //TODO: Add report creation logic
+  }
 }
