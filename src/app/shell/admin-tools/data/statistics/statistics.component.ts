7d652d5435cc8062fb0cee7cd9bf39cae0b289f0
patskal <patskalWork@gmail.com>
2023-01-11 13:18:21 +0200

    Updated statistics interface
    * Removed unnecessary fields
    * Removed unnecessary scss classes
    * Implemented new fields
    * Removed unnecessary class fields and storage queries

--- src/app/shell/admin-tools/data/statistics/statistics.component.ts
+++ src/app/shell/admin-tools/data/statistics/statistics.component.ts
@@ -2,10 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { Codeficator } from 'src/app/shared/models/codeficator.model';
-import { Institution } from 'src/app/shared/models/institution.model';
-import { GetAllInstitutions, GetCodeficatorSearch } from 'src/app/shared/store/meta-data.actions';
-import { MetaDataState } from 'src/app/shared/store/meta-data.state';
 
 @Component({
   selector: 'app-statistics',
@@ -13,26 +9,15 @@ import { MetaDataState } from 'src/app/shared/store/meta-data.state';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
-  @Select(MetaDataState.institutions)
-  institutions$: Observable<Institution[]>;
-
-  @Select(MetaDataState.codeficatorSearch)
-  codeficatorSearch$: Observable<Codeficator[]>;
-
   filtersForm: FormGroup;
   isReport = false;
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
-    this.store.dispatch([new GetAllInstitutions(true), new GetCodeficatorSearch('')]);
-
     this.filtersForm = this.fb.group({
-      dateFrom: new FormControl(''),
-      dateTo: new FormControl(''),
-      institutions: new FormControl(''),
-      regions: new FormControl(''),
-      otgs: new FormControl('')
+      period: new FormControl('WorkshopsDaily'),
+      format: new FormControl('CSV')
     });
   }
 
