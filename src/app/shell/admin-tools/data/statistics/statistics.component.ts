45cdc781090af4f387726b8ad45337c5dc269310
patskal <patskalWork@gmail.com>
2023-01-12 15:58:11 +0200

    Binding buttons with functionality
    * The button "generate reports" is connected with the service for loading data about reports for the table
    * Binding "download" button with a link to download the report file

--- src/app/shell/admin-tools/data/statistics/statistics.component.ts
+++ src/app/shell/admin-tools/data/statistics/statistics.component.ts
@@ -5,7 +5,9 @@ import { filter, Observable, Subject, takeUntil } from 'rxjs';
 import { SearchResponse } from 'src/app/shared/models/search.model';
 import { StatisticReport } from 'src/app/shared/models/statistic.model';
 import { AdminState } from 'src/app/shared/store/admin.state';
+import { GetStatisticReports } from 'src/app/shared/store/admin.actions';
 import { StatisticPeriodType, StatisticPeriodTitle, StatisticFileFormat } from '../../../../shared/enum/statistics';
+import { StatisticParameters } from 'src/app/shared/models/queryParameters.model';
 
 @Component({
   selector: 'app-statistics',
@@ -62,6 +64,7 @@ export class StatisticsComponent implements OnInit, OnDestroy {
       title: 'звіт'
     }
   ];
+  statisticParameters: StatisticParameters;
   filtersForm: FormGroup;
   displayedColumns = ['title', 'fileFormat', 'date', 'createDate', 'actions'];
   destroy$: Subject<boolean> = new Subject<boolean>();
@@ -73,14 +76,24 @@ export class StatisticsComponent implements OnInit, OnDestroy {
       period: new FormControl(StatisticPeriodType.WorkshopsDaily),
       format: new FormControl(StatisticFileFormat.CSV)
     });
+
+    this.setParams();
   }
 
   onGenerateReport(): void {
-    //TODO: Add report creation logic
+    this.setParams();
+    this.store.dispatch(new GetStatisticReports(this.statisticParameters));
   }
 
   ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
+  }
+
+  private setParams(): void {
+    this.statisticParameters = {
+      ReportDataType: this.filtersForm.get('format').value,
+      ReportType: this.filtersForm.get('period').value
+    };
   }
 }
