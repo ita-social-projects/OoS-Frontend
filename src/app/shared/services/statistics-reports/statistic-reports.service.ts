da3df23cf0601aabd82f6e98ffff9a64ae9e4155
patskal <patskalWork@gmail.com>
2023-01-17 13:08:35 +0200

    Fixed wrong state item

--- src/app/shared/services/statistics-reports/statistic-reports.service.ts
+++ src/app/shared/services/statistics-reports/statistic-reports.service.ts
@@ -21,7 +21,7 @@ export class StatisticReportsService {
 
   private setParams(parameters: StatisticParameters): HttpParams {
     const currentPage = this.store.selectSnapshot(PaginatorState.currentPage) as PaginationElement;
-    const size = parameters.size ? parameters.size : this.store.selectSnapshot(PaginatorState.applicationsPerPage);
+    const size = parameters.size ? parameters.size : this.store.selectSnapshot(PaginatorState.tableItemsPerPage);
     const from = size * (+currentPage.element - 1);
 
     const params = new HttpParams()
