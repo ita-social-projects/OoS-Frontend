469a4566c20d8aa57419b107d1c800af2e511d55
Oleksandra Lytvynets <alitvinets7@gmail.com>
2023-01-25 14:58:47 +0200

    test fix

--- src/app/shell/personal-cabinet/provider/create-provider-admin/create-provider-admin.component.spec.ts
+++ src/app/shell/personal-cabinet/provider/create-provider-admin/create-provider-admin.component.spec.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { NgxsModule } from '@ngxs/store';
 import { Workshop } from '../../../../shared/models/workshop.model';
 import { CreateProviderAdminComponent } from './create-provider-admin.component';
@@ -27,7 +28,8 @@ describe('CreateProviderAdminComponent', () => {
         ReactiveFormsModule,
         RouterTestingModule,
         MatDialogModule,
-        NgxsModule.forRoot([])
+        NgxsModule.forRoot([]),
+        TranslateModule.forRoot()
       ],
       declarations: [CreateProviderAdminComponent, MockValidationHintForInputComponent, MockWorkshopChekcboxDropdownComponent]
     }).compileComponents();
