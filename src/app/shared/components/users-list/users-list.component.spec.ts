131d41a1f7ec80ac1918e04eb8a4c447a6eeb360
Oleksandra Lytvynets <alitvinets7@gmail.com>
2023-01-26 12:02:35 +0200

    test fixes

--- src/app/shared/components/users-list/users-list.component.spec.ts
+++ src/app/shared/components/users-list/users-list.component.spec.ts
@@ -4,6 +4,7 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { NgxsModule } from '@ngxs/store';
 import { UsersListComponent } from './users-list.component';
 
@@ -13,8 +14,16 @@ describe('UsersListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MatTableModule, RouterTestingModule, MatMenuModule, NgxsModule.forRoot([]), MatIconModule, MatTooltipModule],
-      declarations: [UsersListComponent]
+      imports: [
+        MatTableModule,
+        RouterTestingModule,
+        MatMenuModule,
+        NgxsModule.forRoot([]),
+        MatIconModule,
+        MatTooltipModule,
+        TranslateModule.forRoot(),
+      ],
+      declarations: [UsersListComponent],
     }).compileComponents();
   });
 
