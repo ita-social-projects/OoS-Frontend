223085837c53ae58579d6d0a4c9309db89a65df1
Oleksandra Lytvynets <alitvinets7@gmail.com>
2023-02-02 22:37:04 +0200

    empty value transform

--- src/app/shared/pipes/empty-value-transform.pipe.ts
+++ src/app/shared/pipes/empty-value-transform.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class EmptyValueTransformPipe implements PipeTransform {
   transform(value: string): string {
-    return value ? value : 'Не вказано';
+    return value ? value : 'SERVICCE_MESSAGES.NO_INFO';
   }
 }
