7e6d7ff8c66ef338bd8041dac5bf1eee09a29784
patskal <patskalWork@gmail.com>
2023-05-18 12:03:47 +0300

    Added access modificators

--- src/app/shared/components/filters-list/city-filter/city-filter.component.ts
+++ src/app/shared/components/filters-list/city-filter/city-filter.component.ts
@@ -21,28 +21,29 @@ import { MetaDataState } from '../../../store/meta-data.state';
   styleUrls: ['./city-filter.component.scss']
 })
 export class CityFilterComponent implements OnInit, AfterViewInit, OnDestroy {
-  readonly Constants = Constants;
-  readonly sliceLength = 25;
+  public readonly Constants = Constants;
+  public readonly sliceLength = 25;
 
   @Select(FilterState.isConfirmCity)
-  isConfirmCity$: Observable<boolean>;
+  public isConfirmCity$: Observable<boolean>;
   @Select(FilterState.settlement)
-  settlement$: Observable<Codeficator>;
-  settlement: Codeficator;
+  private settlement$: Observable<Codeficator>;
+  public settlement: Codeficator;
   @Select(MetaDataState.codeficatorSearch)
-  codeficatorSearch$: Observable<Codeficator[]>;
-  codeficatorSearch: Codeficator[];
+  private codeficatorSearch$: Observable<Codeficator[]>;
+  public codeficatorSearch: Codeficator[];
 
-  @ViewChild('searchInput') searchInput: ElementRef;
+  @ViewChild('searchInput') private searchInput: ElementRef;
 
-  settlementSearchControl: FormControl = new FormControl('');
-  isDispalyed = true;
-  isTopCities = false;
-  destroy$: Subject<boolean> = new Subject<boolean>();
+  private isTopCities = false;
+  private destroy$: Subject<boolean> = new Subject<boolean>();
+
+  public settlementSearchControl: FormControl = new FormControl('');
+  public isDispalyed = true;
 
   constructor(private store: Store, private actions$: Actions, private geolocationService: GeolocationService) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.settlementListener();
     this.settlement$
       .pipe(
@@ -66,7 +67,7 @@ export class CityFilterComponent implements OnInit, AfterViewInit, OnDestroy {
     });
   }
 
-  ngAfterViewInit(): void {
+  public ngAfterViewInit(): void {
     if (this.geolocationService.isCityInStorage()) {
       this.geolocationService.confirmCity(JSON.parse(localStorage.getItem('cityConfirmation')), true);
     } else {
@@ -80,6 +81,52 @@ export class CityFilterComponent implements OnInit, AfterViewInit, OnDestroy {
         }
       });
     }
+  }
+
+  public onSelectedCity(event: MatAutocompleteSelectedEvent): void {
+    this.geolocationService.confirmCity(event.option.value, true);
+    this.store.dispatch([new SetCoordsByMap({ lat: event.option.value.latitude, lng: event.option.value.longitude }), new FilterChange()]);
+  }
+
+  /**
+   * This method listen input FocusOut event and update search and settlement controls value
+   * @param auto MatAutocomplete
+   */
+  public onFocusOut(): void {
+    if (!this.settlementSearchControl.value) {
+      const settlement = this.store.selectSnapshot(FilterState.settlement);
+      this.settlement = settlement;
+      this.settlementSearchControl.setValue(settlement.settlement, { emitEvent: false });
+      this.codeficatorSearch = null;
+    }
+  }
+
+  /**
+   * This method handle displayed value for mat-autocomplete dropdown
+   * @param codeficator: Codeficator | string
+   */
+  public displaySettlementNameFn(codeficator: Codeficator | string): string {
+    return typeof codeficator === 'string' ? codeficator : codeficator?.settlement;
+  }
+
+  public confirmCity(): void {
+    this.geolocationService.confirmCity(this.settlement, true);
+  }
+
+  public changeCity(): void {
+    this.isDispalyed = false;
+    this.settlementSearchControl.setValue(null);
+    this.settlement = null;
+    this.actions$.pipe(ofActionCompleted(GetCodeficatorSearch), takeUntil(this.destroy$)).subscribe(() => this.setInputFocus());
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  private setInputFocus(): void {
+    this.searchInput.nativeElement.focus();
   }
 
   /**
@@ -101,51 +148,5 @@ export class CityFilterComponent implements OnInit, AfterViewInit, OnDestroy {
             .filter((codeficator: Codeficator) => codeficator.settlement.toLowerCase().startsWith(value.toLowerCase()));
         }
       });
-  }
-
-  onSelectedCity(event: MatAutocompleteSelectedEvent): void {
-    this.geolocationService.confirmCity(event.option.value, true);
-    this.store.dispatch([new SetCoordsByMap({ lat: event.option.value.latitude, lng: event.option.value.longitude }), new FilterChange()]);
-  }
-
-  /**
-   * This method listen input FocusOut event and update search and settlement controls value
-   * @param auto MatAutocomplete
-   */
-  onFocusOut(): void {
-    if (!this.settlementSearchControl.value) {
-      const settlement = this.store.selectSnapshot(FilterState.settlement);
-      this.settlement = settlement;
-      this.settlementSearchControl.setValue(settlement.settlement, { emitEvent: false });
-      this.codeficatorSearch = null;
-    }
-  }
-
-  /**
-   * This method handle displayed value for mat-autocomplete dropdown
-   * @param codeficator: Codeficator | string
-   */
-  displaySettlementNameFn(codeficator: Codeficator | string): string {
-    return typeof codeficator === 'string' ? codeficator : codeficator?.settlement;
-  }
-
-  confirmCity(): void {
-    this.geolocationService.confirmCity(this.settlement, true);
-  }
-
-  changeCity(): void {
-    this.isDispalyed = false;
-    this.settlementSearchControl.setValue(null);
-    this.settlement = null;
-    this.actions$.pipe(ofActionCompleted(GetCodeficatorSearch), takeUntil(this.destroy$)).subscribe(() => this.setInputFocus());
-  }
-
-  setInputFocus(): void {
-    this.searchInput.nativeElement.focus();
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next(true);
-    this.destroy$.unsubscribe();
   }
 }
