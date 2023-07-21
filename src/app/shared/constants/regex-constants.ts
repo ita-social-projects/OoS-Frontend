9a9d653a2e6198216125bdc5a1e56bb183611198
doliinyk <denysoliinyk@outlook.com>
2023-07-21 14:17:08 +0300

    Add email regex and email validator, moved all default Angular validators to created validator. TODO regex

--- src/app/shared/constants/regex-constants.ts
+++ src/app/shared/constants/regex-constants.ts
@@ -1,14 +1,24 @@
 /**
  * RegExp Constants
  */
+
+// Regex for text
 export const TEXT_REGEX: RegExp = /^\S[А-Яа-яЇїІіЄєЁёҐґ'’` \s-]*$/;
+
 // Regex for lastName, firstName, middleName, fullName
 export const NAME_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ''’` \s-]*$/;
+
+// Regex for email
+export const EMAIL_REGEX: RegExp = /^$/; // TODO: Create regex validator
+
 // Regex for non-latin characters
 export const NO_LATIN_REGEX: RegExp = /^[А-ЩЬЮЯҐЄІЇа-щьюяґєії0-9.,_ \-’!@#$%^/&*()+={}\\|<>~`':;"]+$/;
+
 // Regex for date
 export const DATE_REGEX: RegExp = /[^0-9./-]*/g;
+
 // Regex for street name
 export const STREET_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ'’`.\s-]*$/;
+
 // Regex for house number
 export const HOUSE_REGEX: RegExp = /^[0-9]+ *[А-Яа-яЇїІіЄєЁёҐґ]* *$/;
--- src/app/shared/constants/validation.ts
+++ src/app/shared/constants/validation.ts
@@ -1,6 +1,6 @@
 import { ValidatorFn, Validators } from '@angular/forms';
 
-import { HOUSE_REGEX, NO_LATIN_REGEX, STREET_REGEX } from './regex-constants';
+import { EMAIL_REGEX, HOUSE_REGEX, NO_LATIN_REGEX, STREET_REGEX } from "./regex-constants";
 
 export class ValidationConstants {
   // Age
@@ -66,4 +66,6 @@ export class FormValidators {
     Validators.minLength(ValidationConstants.INPUT_LENGTH_3),
     Validators.maxLength(ValidationConstants.INPUT_LENGTH_60)
   ];
+
+  static readonly email: ValidatorFn = Validators.pattern(EMAIL_REGEX);
 }
