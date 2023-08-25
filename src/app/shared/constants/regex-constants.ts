536c777d19f48c19a5b46fa677381330adbce677
Ultrafiolet4 <bicaroman@gmail.com>
2023-08-25 11:19:37 +0300

    Optimised house regular expression

--- src/app/shared/constants/regex-constants.ts
+++ src/app/shared/constants/regex-constants.ts
@@ -24,7 +24,7 @@ export const DATE_REGEX: RegExp = /[^0-9./-]*/g;
 export const STREET_REGEX: RegExp = /^[А-Яа-яЇїІіЄєЁёҐґ'’`\s-]+[А-Яа-яЇїІіЄєЁёҐґ'’`\s(\d)]?$/;
 
 // Regex for house number
-export const HOUSE_REGEX: RegExp = /^\d+(?:[А-Яа-яЇїІіЄєЁёҐґ]*)?(?:[\/\-Firast[\dА-Яа-яЇїІіЄєЁёҐґ]+(?:[А-Яа-яЇїІіЄєЁёҐґ]+)*)?(?:\s+[А-Яа-яЇїІіЄєЁёҐґ]*\.\s*[0-9А-Яа-яЇїІіЄєЁёҐґ]+\s*)?$/;
+export const HOUSE_REGEX: RegExp = /^(?!.*\d[\/\-]$)\d+(?:[А-Яа-яЇїІіЄєЁёҐґ]*)?(?:[\/\-][\dА-Яа-яЇїІіЄєЁёҐґ]+(?:[А-Яа-яЇїІіЄєЁёҐґ]+)*)?(?:\s+[А-Яа-яЇїІіЄєЁёҐґ]*\.\s*[0-9А-Яа-яЇїІіЄєЁёҐґ]+\s*)?$/;
 
 // Regex for section name, it checks that the first char is a letter
 export const SECTION_NAME_REGEX: RegExp = /^(?!`)(?!\^)(?!_)(?!\[)(?!])(?!\\)[А-ЩЬЮЯҐЄІЇа-щьюяґєіїA-Za-z].+/;
