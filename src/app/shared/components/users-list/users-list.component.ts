6b01525e06b4e4181fbd8676070b1be3a5907acb
Serhii Horeslavskyi <77gsa88@gmail.com>
2023-08-25 23:58:57 +0300

    added different column label to region/area table

--- src/app/shared/components/users-list/users-list.component.ts
+++ src/app/shared/components/users-list/users-list.component.ts
@@ -31,6 +31,7 @@ import { UserStatusesTitles } from '../../enum/enumUA/statuses';
 })
 export class UsersListComponent implements OnInit, AfterViewInit, OnChanges {
   @Input() users: Array<object>;
+  @Input() adminType: Role;
   @Input() displayedColumns: string[] = ['pib', 'email', 'phone', 'role', 'region', 'status', 'actions'];
   @Input() isEdit: boolean;
   @Input() statusesTitles: UserStatuses | EmailConfirmationStatuses;
