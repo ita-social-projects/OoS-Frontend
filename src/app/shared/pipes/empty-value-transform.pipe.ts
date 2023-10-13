7ab4365c00ba53d76f59cccdd52edf81b1c1a020
Serhii-Horeslavskyi <77gsa88@gmail.com>
2023-10-13 17:25:08 +0300

    added some data to table admins change log

--- src/app/shared/pipes/empty-value-transform.pipe.ts
+++ src/app/shared/pipes/empty-value-transform.pipe.ts
@@ -1,10 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Constants } from 'shared/constants/constants';
 
 @Pipe({
   name: 'emptyValueTransform'
 })
 export class EmptyValueTransformPipe implements PipeTransform {
-  transform(value: string): string {
+  transform(value: string, args: string): string {
+    if (args === Constants.DASH_VALUE) {
+      return value ? value : '—';
+    }
+
     return value ? value : 'SERVICE_MESSAGES.NO_INFO';
   }
 }
