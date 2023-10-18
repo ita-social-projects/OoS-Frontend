2390524defc9236a0c314e35734aa40dc85783cf
Serhii-Horeslavskyi <77gsa88@gmail.com>
2023-10-18 17:02:22 +0300

    added dash value

--- src/app/shared/constants/constants.ts
+++ src/app/shared/constants/constants.ts
@@ -29,6 +29,7 @@ export class Constants {
 
   static readonly MAIL_FORMAT_PLACEHOLDER = 'example@mail.com';
   static readonly DASH_VALUE = 'dash';
+  static readonly DASH = '—';
 
   static readonly SCROLL_TO_TOP_BUTTON_POS = 300;
   static readonly INSTITUTION_ID_ABSENT_VALUE = 0;
