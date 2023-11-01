9636144fab87a47f009b1897d91d3240dde1154a
Denys Oliinyk <68949838+doliinyk@users.noreply.github.com>
2023-11-01 11:39:08 +0200

    Oliinyk / Backend 1284 Applications fetching after block un block (#2296)
    * Fixed platform guard & add users guard for admins
    
    * - Refactored application service
    - Fixed applications fetching after blocking/unblocking parent
    
    * Made UsersGuard return only Observable

--- src/app/shared/services/applications/application.service.ts
+++ src/app/shared/services/applications/application.service.ts
@@ -3,9 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { ApplicationStatuses } from '../../enum/statuses';
-import {
-  Application, ApplicationFilterParameters, ApplicationUpdate
-} from '../../models/application.model';
+import { Application, ApplicationFilterParameters, ApplicationUpdate } from '../../models/application.model';
 import { SearchResponse } from '../../models/search.model';
 
 @Injectable({
@@ -14,40 +12,15 @@ import { SearchResponse } from '../../models/search.model';
 export class ApplicationService {
   constructor(private http: HttpClient) {}
 
-  private setParams(parameters: ApplicationFilterParameters): HttpParams {
-    let params = new HttpParams();
-
-    if (parameters) {
-      if (parameters.statuses.length) {
-        parameters.statuses.forEach((status: ApplicationStatuses) => (params = params.append('Statuses', status)));
-      }
-
-      if (parameters.workshops?.length) {
-        parameters.workshops.forEach((workshopId: string) => (params = params.append('Workshops', workshopId)));
-      }
-
-      if (parameters.children?.length) {
-        parameters.children.forEach((childrenId: string) => (params = params.append('Children', childrenId)));
-      }
-
-      if (parameters.searchString) {
-        params = params.set('SearchString', parameters.searchString);
-      }
-
-      if ('size' in parameters && 'from' in parameters) {
-        params = params.set('Size', parameters.size.toString()).set('From', parameters.from.toString());
-      }
-      
-      params = params.set('ShowBlocked', parameters.showBlocked.toString());
-    }
-    params = params.set('OrderByDateAscending', 'true').set('OrderByAlphabetically', 'true').set('OrderByStatus', 'true');
-
-    return params;
+  getAllApplications(parameters: ApplicationFilterParameters): Observable<SearchResponse<Application[]>> {
+    const options = { params: this.setParams(parameters) };
+    return this.http.get<SearchResponse<Application[]>>('/api/v1/applications', options);
   }
 
   /**
    * This method get applications by Provider id
    * @param id string
+   * @param parameters ApplicationFilterParameters
    */
   getApplicationsByPropertyId(id: string, parameters: ApplicationFilterParameters): Observable<SearchResponse<Application[]>> {
     const options = { params: this.setParams(parameters) };
@@ -55,9 +28,13 @@ export class ApplicationService {
     return this.http.get<SearchResponse<Application[]>>(`/api/v1/${parameters.property}/${id}/applications`, options);
   }
 
-  getAllApplications(parameters: ApplicationFilterParameters): Observable<SearchResponse<Application[]>> {
-    const options = { params: this.setParams(parameters) };
-    return this.http.get<SearchResponse<Application[]>>('/api/v1/applications', options);
+  /**
+   * This method Check if exists an any application with approve status in workshop for parent
+   * @param parentId string
+   * @param workshopId string
+   */
+  getApplicationsAllowedToReview(parentId: string, workshopId: string): Observable<boolean> {
+    return this.http.get<boolean>(`/api/v1/applications/reviewable/parents/${parentId}/workshops/${workshopId}`);
   }
 
   /**
@@ -70,7 +47,7 @@ export class ApplicationService {
 
   /**
    * This method update Application
-   * @param application: ApplicationUpdate
+   * @param application ApplicationUpdate
    */
   updateApplication(application: ApplicationUpdate): Observable<Application> {
     return this.http.put<Application>('/api/v1/applications', application);
@@ -85,12 +62,33 @@ export class ApplicationService {
     return this.http.get<boolean>(`/api/v1/applications/allowed/workshops/${workshopId}/children/${childId}`);
   }
 
-  /**
-   * This method Check if exists an any application with approve status in workshop for parent
-   * @param parentId string
-   * @param workshopId string
-   */
-  getApplicationsAllowedToReview(parentId: string, workshopId: string): Observable<boolean> {
-    return this.http.get<boolean>(`/api/v1/applications/reviewable/parents/${parentId}/workshops/${workshopId}`);
+  private setParams(parameters: ApplicationFilterParameters): HttpParams {
+    let params = new HttpParams();
+
+    if (parameters) {
+      if (parameters.statuses.length) {
+        parameters.statuses.forEach((status: ApplicationStatuses) => (params = params.append('Statuses', status)));
+      }
+      if (parameters.workshops?.length) {
+        parameters.workshops.forEach((workshopId: string) => (params = params.append('Workshops', workshopId)));
+      }
+      if (parameters.children?.length) {
+        parameters.children.forEach((childrenId: string) => (params = params.append('Children', childrenId)));
+      }
+      if (parameters.searchString) {
+        params = params.set('SearchString', parameters.searchString);
+      }
+      if (parameters.size) {
+        params = params.set('Size', parameters.size.toString());
+      }
+      if (parameters.from) {
+        params = params.set('From', parameters.from.toString());
+      }
+
+      params = params.set('ShowBlocked', parameters.showBlocked.toString());
+    }
+    params = params.set('OrderByDateAscending', 'true').set('OrderByAlphabetically', 'true').set('OrderByStatus', 'true');
+
+    return params;
   }
 }
