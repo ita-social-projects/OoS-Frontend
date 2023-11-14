ac90925ea86f7e9e82d28d472648808d6528e5a9
Serhii-Horeslavskyi <77gsa88@gmail.com>
2023-11-14 13:15:01 +0200

    added unit test to checkValidationErrors method

--- src/app/shared/components/validation-hint/validation-hint.component.spec.ts
+++ src/app/shared/components/validation-hint/validation-hint.component.spec.ts
@@ -1,5 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { FormControl, ValidationErrors } from '@angular/forms';
+import { SimpleChanges } from '@angular/core';
 
 import { ValidationHintComponent } from './validation-hint.component';
 import { HOUSE_REGEX, NAME_REGEX, NO_LATIN_REGEX, SECTION_NAME_REGEX, STREET_REGEX } from 'shared/constants/regex-constants';
@@ -24,7 +25,137 @@ describe('ValidationHintComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // describe('#ngOnInit', () => {
+  //   let control: FormControl;
+  //   let errors: ValidationErrors;
+  //   let delay: number;
+
+  //   beforeEach(() => {
+  //     control = component.validationFormControl;
+  //     errors = component.validationFormControl.errors;
+  //     delay = 420;
+  //   });
+
+  //   it('should mark validationFormControl as touched when status changes', fakeAsync(() => {
+  //     const markAsTouchedSpy = jest.spyOn(component.validationFormControl, 'markAsTouched');
+      
+  //     component.ngOnInit();
+  //     component.validationFormControl.setValue('test');
+  //     tick(delay);
   
+  //     expect(markAsTouchedSpy).toHaveBeenCalled();
+  //   }));
+
+  //   it('should execute checkMatDatePicker when validationFormControl status changes and minMaxDate is TRUE', fakeAsync(() => {
+  //     component.minMaxDate = true;
+  //     const checkMatDatePickerSpy = jest.spyOn(component, 'checkMatDatePicker' as any);
+
+  //     component.ngOnInit();
+  //     component.validationFormControl.setValue('test');
+  //     tick(delay);
+
+  //     expect(checkMatDatePickerSpy).toHaveBeenCalled();
+  //   }));
+
+  //   it('should NOT execute checkMatDatePicker when validationFormControl status changes and minMaxDate is FALSE', fakeAsync(() => {
+  //     component.minMaxDate = false;
+  //     const checkMatDatePickerSpy = jest.spyOn(component, 'checkMatDatePicker' as any);
+
+  //     component.ngOnInit();
+  //     component.validationFormControl.setValue('test');
+  //     tick(delay);
+
+  //     expect(checkMatDatePickerSpy).not.toHaveBeenCalled();
+  //   }));
+
+  //   it('should execute checkValidationErrors when validationFormControl status changes with errors', fakeAsync(() => {
+  //     const checkValidationErrorsSpy = jest.spyOn(component, 'checkValidationErrors' as any);
+  //     errors = { email: true };
+      
+  //     component.ngOnInit();
+  //     component.validationFormControl.setValue('test');
+  //     tick(delay);
+
+  //     expect(checkValidationErrorsSpy).toHaveBeenCalledWith(errors);
+  //   }));
+  // });
+  
+  describe('checkValidationErrors method', () => {
+    let errors: ValidationErrors;
+    
+    it('should assign to invalidEmail TRUE if errors have an email error', () => {
+      errors = { email: true };
+
+      component['checkValidationErrors'](errors);
+      
+      expect(component.invalidEmail).toBeTruthy();
+    });
+    
+    it('should assign to invalidEmail FALSE if errors have NO an email error', () => {
+      errors = { email: false };
+
+      component['checkValidationErrors'](errors);
+      
+      expect(component.invalidEmail).toBeFalsy();
+    });
+
+    // invalidPhoneLength
+    test.each`
+      isPhoneNumber   | isMinlength   | isMaxlength   | expectedInvalidPhoneLength
+      ${true}         | ${true}       | ${false}      | ${true}
+      ${true}         | ${true}       | ${true}       | ${false}
+      ${true}         | ${false}      | ${true}       | ${false}
+      ${false}        | ${false}      | ${false}      | ${undefined}
+    `('should assign to invalidPhoneLength $expectedInvalidPhoneLength when isPhoneNumber equal to $isPhoneNumber, minlength equal to $isMinlength and maxlength equal to $isMaxlength', 
+      ({ isPhoneNumber, isMinlength, isMaxlength, expectedInvalidPhoneLength }) => {
+
+        errors = { minlength: isMinlength, maxlength: isMaxlength };
+        component.isPhoneNumber = isPhoneNumber;
+  
+        component['checkValidationErrors'](errors);
+  
+        expect(component.invalidPhoneLength).toBe(expectedInvalidPhoneLength);
+      }); 
+      
+    // invalidEdrpouIpn
+    test.each`
+      isEdrpouIpn     | isMinlength   | isMaxlength   | expectedInvalidEdrpouIpn
+      ${true}         | ${true}       | ${false}      | ${true}
+      ${true}         | ${true}       | ${true}       | ${false}
+      ${true}         | ${false}      | ${true}       | ${false}
+      ${false}        | ${false}      | ${false}      | ${undefined}
+    `('should assign to invalidEdrpouIpn $expectedInvalidEdrpouIpn when isEdrpouIpn equal to $isEdrpouIpn, minlength equal to $isMinlength and maxlength equal to $isMaxlength', 
+      ({ isEdrpouIpn, isMinlength, isMaxlength, expectedInvalidEdrpouIpn }) => {
+
+        errors = { minlength: isMinlength, maxlength: isMaxlength };
+        component.isEdrpouIpn = isEdrpouIpn;
+
+        component['checkValidationErrors'](errors);
+
+        expect(component.invalidEdrpouIpn).toBe(expectedInvalidEdrpouIpn);
+      });
+
+    // invalidFieldLength
+    test.each`
+      isMinlength   | isMaxlength   | expectedInvalidFieldLength
+      ${true}       | ${true}       | ${true}
+      ${true}       | ${false}      | ${true}
+      ${false}      | ${true}       | ${true}
+      ${false}      | ${false}      | ${false}
+    `('should assign to invalidFieldLength $expectedInvalidFieldLength when isPhoneNumber and isEdrpouIpn equal to false, minlength equal to $isMinlength, maxlength equal to $isMaxlength', 
+      ({ isMinlength, isMaxlength, expectedInvalidFieldLength }) => {
+
+        component.isPhoneNumber = false;
+        component.isEdrpouIpn = false;
+        errors = { minlength: isMinlength, maxlength: isMaxlength };
+
+        component['checkValidationErrors'](errors);
+
+        expect(component.invalidFieldLength).toBe(expectedInvalidFieldLength);
+      });
+  });
+
   describe('checkInvalidText method', () => {
     let errors: ValidationErrors;
 
