bb888cd36046ef091091597870f8c2e5fc30ada3
doliinyk <denysoliinyk@outlook.com>
2023-11-28 15:07:40 +0200

    Fix actions in admins, directions tab and guard

--- src/app/shell/admin-tools/data/data-routing.module.ts
+++ src/app/shell/admin-tools/data/data-routing.module.ts
@@ -10,13 +10,14 @@ import { ProviderListComponent } from './provider-list/provider-list.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { UsersComponent } from './users/users.component';
 import { UsersGuard } from './users/users.guard';
+import { DirectionsWrapperGuard } from './directions-wrapper/directions-wrapper.guard';
 
 const routes: Routes = [
   { path: 'provider-list', component: ProviderListComponent },
   { path: 'users', component: UsersComponent, canLoad: [UsersGuard] },
   { path: 'admins', component: AdminsComponent, canLoad: [AdminsGuard] },
   { path: 'history-log', component: HistoryLogComponent },
-  { path: 'directions', component: DirectionsWrapperComponent, canLoad: [DirectionsWrapperComponent] },
+  { path: 'directions', component: DirectionsWrapperComponent, canLoad: [DirectionsWrapperGuard] },
   { path: 'applications', component: AdminApplicationsComponent },
   { path: 'statistics', component: StatisticsComponent }
 ];
