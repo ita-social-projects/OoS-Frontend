047202089edfe7f95e515c97d59314dee8809d63
doliinyk <denysoliinyk@outlook.com>
2023-12-20 15:27:33 +0200

    Test fix

--- src/app/shared/components/phone-form-control/phone-form-control.component.spec.ts
+++ src/app/shared/components/phone-form-control/phone-form-control.component.spec.ts
@@ -1,16 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { NgxMatIntlTelInputComponent } from 'ngx-mat-intl-tel-input';
 import { PhoneFormControlComponent } from './phone-form-control.component';
 
-describe('PhoneInputComponent', () => {
+describe('PhoneFormControlComponent', () => {
   let component: PhoneFormControlComponent;
   let fixture: ComponentFixture<PhoneFormControlComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PhoneFormControlComponent],
-      imports: [NgxMatIntlTelInputComponent]
+      imports: [NgxMatIntlTelInputComponent, TranslateModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PhoneFormControlComponent);
