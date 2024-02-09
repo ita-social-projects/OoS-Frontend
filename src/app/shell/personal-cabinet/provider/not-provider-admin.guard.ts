1750107fcddfcc6af73181ede825823916bfe8e8
Morozov Vladyslav <vladmorozov32@outlook.com>
2024-02-09 14:47:07 +0200

    fixed

--- src/app/shell/personal-cabinet/provider/not-provider-admin.guard.ts
+++ src/app/shell/personal-cabinet/provider/not-provider-admin.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, UrlTree } from '@angular/router';
 import { Select } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
 
 import { Role, Subrole } from 'shared/enum/role';
@@ -16,12 +16,13 @@ export class NotProviderAdminGuard implements CanActivate {
   @Select(RegistrationState.subrole)
   public subrole$: Observable<string>;
 
-  public canActivate(): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return (
+  public canActivate(): Observable<boolean> {
+    return combineLatest([
       this.role$.pipe(
         filter(Boolean),
         map((role: string) => role === Role.provider)
-      ) && this.subrole$.pipe(map((subrole: string) => subrole !== Subrole.ProviderAdmin))
-    );
+      ),
+      this.subrole$.pipe(map((subrole: string) => subrole !== Subrole.ProviderAdmin))
+    ]).pipe(map(([isProviderRole, isNotProviderAdmin]) => isProviderRole && isNotProviderAdmin));
   }
 }
