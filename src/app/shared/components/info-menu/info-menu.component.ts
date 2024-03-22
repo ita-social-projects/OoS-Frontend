8b7fa812bc7f7be105a47ab3c26bebee876af4b2
Serhii-Horeslavskyi <77gsa88@gmail.com>
2024-03-22 21:28:25 +0200

    more fixing Sonar warnings

--- src/app/shared/components/info-menu/info-menu.component.ts
+++ src/app/shared/components/info-menu/info-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { InfoMenuType } from 'shared/enum/info-menu-type';
 
 @Component({
