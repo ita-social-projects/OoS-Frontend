0fb161007b5d860629ff7589e653eaf9179984cd
MarianS23 <maryanshutov@gmail.com>
2024-03-28 13:12:38 +0200

    install xlsx library

--- src/app/shell/admin-tools/data/import-export/import-providers/import-providers.component.ts
+++ src/app/shell/admin-tools/data/import-export/import-providers/import-providers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx/xlsx.mjs';
 
 @Component({
   selector: 'app-import-providers',
@@ -6,17 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./import-providers.component.scss']
 })
 export class ImportProvidersComponent implements OnInit {
-  public file!: any;
-  public photo!: any;
-  trustedUrl: any;
+  public providers: any[] = []; // set more specific type
   constructor() {}
   ngOnInit(): void {}
-  selectedFile: any = null;
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  public onFileSelected(event: any): void {
-    this.selectedFile = event.target.files[0] ?? null;
-    console.log(event.target.files[0]);
-  }
 
-  public showFile(): void {}
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  public onFileSelected(event: any): any {
+    const file = event.target.files[0];
+    const reader: FileReader = new FileReader();
+
+    reader.readAsArrayBuffer(file);
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    reader.onload = (e: any) => {
+      // upload file
+      const binarystr = new Uint8Array(e.target.result);
+      const wb: XLSX.WorkBook = XLSX.read(binarystr, { type: 'array', raw: true, cellFormula: false });
+      // console.log(wb.Sheets);
+
+      const wsname = wb.SheetNames[0];
+      this.providers = XLSX.utils.sheet_to_json(wb.Sheets[wsname]);
+      console.log(this.providers);
+    };
+  }
+  // selectedFile: any = null;
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  // public onFileSelected(event: any): void {
+  //   this.selectedFile = event.target.files[0] ?? null;
+  //   console.log(event.target.files[0]);
+  // }
 }
