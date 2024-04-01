07efb25a88db7ba29457afbff376a142e4abf978
MarianS23 <maryanshutov@gmail.com>
2024-04-01 22:33:34 +0300

    added validation to input field and compliance of headers with entered data

--- src/app/shell/admin-tools/data/import-export/import-providers/import-providers.component.ts
+++ src/app/shell/admin-tools/data/import-export/import-providers/import-providers.component.ts
@@ -1,12 +1,18 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Component, HostListener, OnInit } from '@angular/core';
 import * as XLSX from 'xlsx/xlsx.mjs';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
+const standartHeaders = [
+  'Назва закладу ',
+  'Форма власності',
+  'ЄДРПОУ/ІПН',
+  'Ліцензія №',
+  'Населений пункт',
+  'Адреса',
+  'Електронна пошта',
+  'Телефон'
+];
 
 @Component({
   selector: 'app-import-providers',
@@ -14,33 +20,10 @@ export interface PeriodicElement {
   styleUrls: ['./import-providers.component.scss']
 })
 export class ImportProvidersComponent implements OnInit {
-  isShow: boolean;
-  topPosToStartShowing = 250;
-
-  @HostListener('window:scroll')
-  checkScroll(): void {
-    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop || 0;
-
-    // console.log('[scroll]', scrollPosition);
-
-    if (scrollPosition >= this.topPosToStartShowing) {
-      this.isShow = true;
-    } else {
-      this.isShow = false;
-    }
-  }
-
-  // TODO: Cross browsing
-  gotoTop(): void {
-    window.scroll({
-      top: 0,
-      left: 0,
-      behavior: 'smooth'
-    });
-  }
   public selectedFile: any = null;
-
-  displayedColumns: string[] = [
+  public isShow: boolean;
+  public readonly topPosToStartShowing: number = 250;
+  public readonly displayedColumns: string[] = [
     'sequence number',
     'provider name',
     'ownership',
@@ -53,27 +36,58 @@ export class ImportProvidersComponent implements OnInit {
   ];
   public dataSource;
 
+  @HostListener('window:scroll')
+  checkScroll(): void {
+    const scrollPosition = document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollPosition >= this.topPosToStartShowing) {
+      this.isShow = true;
+    } else {
+      this.isShow = false;
+    }
+  }
+
+  gotoTop(): void {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
   constructor() {}
   ngOnInit(): void {}
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   public onFileSelected(event: any): any {
+    this.dataSource = null;
     this.selectedFile = event.target.files[0] ?? null;
     const file = event.target.files[0];
     const reader: FileReader = new FileReader();
-
     reader.readAsArrayBuffer(file);
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
     reader.onload = (e: any) => {
       const binarystring = new Uint8Array(e.target.result);
-      const wb: XLSX.WorkBook = XLSX.read(binarystring, { type: 'array', raw: true, cellFormula: false });
+      const wb: XLSX.WorkBook = XLSX.read(binarystring, { type: 'array', WTF: true, raw: true, cellFormula: false });
       const wsname = wb.SheetNames[0];
-      const providers = XLSX.utils.sheet_to_json(wb.Sheets[wsname], {
-        header: ['providerName', 'ownership', 'identifier', 'licenseNumber', 'settlement', 'address', 'email', 'phoneNumber'],
-        range: 1
-      });
-      this.dataSource = providers;
-      console.log(providers);
+      const currentHeaders = XLSX.utils.sheet_to_json(wb.Sheets[wsname], { header: 1 }).shift();
+      if (this.checkHeadersForValid(currentHeaders)) {
+        const providers = XLSX.utils.sheet_to_json(wb.Sheets[wsname], {
+          header: ['providerName', 'ownership', 'identifier', 'licenseNumber', 'settlement', 'address', 'email', 'phoneNumber'],
+          range: 1
+        });
+        this.dataSource = providers;
+      }
     };
+  }
+  public checkHeadersForValid(currentHeaders: string[]): boolean {
+    for (let i = 0; i < standartHeaders.length; i++) {
+      if (currentHeaders[i] !== standartHeaders[i]) {
+        alert(`невідповідність в заголовку "${currentHeaders[i]}",
+
+        Зразок:
+        Назва закладу | Форма власності | ЄДРПОУ/ІПН | Ліцензія № |
+        Населений пункт | Адреса | Електронна пошта | Телефон`);
+        return false;
+      }
+    }
+    return true;
   }
 }
