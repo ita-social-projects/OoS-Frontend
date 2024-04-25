c818e4607ced650c17136d971029b8f06f258802
Max Dvorak <dvorakmax2002@gmail.com>
2024-04-25 14:37:35 +0300

    Role added

--- src/app/shared/components/status-info-card/status-info-card.component.ts
+++ src/app/shared/components/status-info-card/status-info-card.component.ts
@@ -3,6 +3,7 @@ import { ApplicationIcons } from '../../enum/applications';
 import { ApplicationStatusDescription, ApplicationTitles } from '../../enum/enumUA/statuses';
 import { ApplicationStatuses } from '../../enum/statuses';
 import { Application } from '../../models/application.model';
+import { Role } from '../../enum/role';
 
 @Component({
   selector: 'app-status-info-card',
@@ -10,10 +11,13 @@ import { Application } from '../../models/application.model';
   styleUrls: ['./status-info-card.component.scss']
 })
 export class StatusInfoCardComponent {
+  @Input() public role: Role;
+
   public readonly statusTitles = ApplicationTitles;
   public readonly applicationStatusDescription = ApplicationStatusDescription;
   public readonly applicationIcons = ApplicationIcons;
   public readonly statuses = ApplicationStatuses;
+  public readonly Role = Role;
 
   @Input() application: Application = null;
 }
