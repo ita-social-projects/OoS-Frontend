f269dc7652479ab13e268dd1d8a93379b98247ee
Yurii Romanchak <71885527+Kazumen@users.noreply.github.com>
2024-06-11 14:30:08 +0300

    Romanchak/Parent registration update (#2554)
    * Created CreateParentComponent, added guard, translation and routing
    
    * Fixed guard, added new translate for message bar
    
    * Added tests for CreateParentComponent and CreateParentGuard. Added logic for button submit
    
    * Fixed lint
    
    * Fixed tests
    
    * Fixed lint
    
    * Fixed lint
    
    * Added new test for service
    
    * Fixed lint
    
    * Fixed test for personal cabinet guard
    
    * Added exclusions for coverage
    
    * Added payload for Fail and Success create provider and parent
    
    * Fixed message bar for provider and parent. Also added condition for dialog menu from workshop details
    
    * Fixed comments
    
    * Fixed comment
    
    * Fixed lint

--- src/app/shell/personal-cabinet/personal-cabinet.guard.ts
+++ src/app/shell/personal-cabinet/personal-cabinet.guard.ts
@@ -1,25 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Router, UrlTree } from '@angular/router';
 import { Select } from '@ngxs/store';
-import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 import { ModeConstants } from 'shared/constants/constants';
 import { RegistrationState } from 'shared/store/registration.state';
+import { User } from 'shared/models/user.model';
+import { Role } from 'shared/enum/role';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonalCabinetGuard {
-  @Select(RegistrationState.isRegistered)
-  private isRegistered$: Observable<boolean>;
+  @Select(RegistrationState.user)
+  private user$: Observable<User>;
 
   constructor(private router: Router) {}
 
   public canLoad(): Observable<boolean | UrlTree> {
-    return this.isRegistered$.pipe(
-      filter((isRegistered: boolean) => isRegistered !== undefined),
-      map((isRegistered: boolean) => isRegistered || this.router.createUrlTree(['/create-provider', ModeConstants.NEW]))
+    return this.user$.pipe(
+      filter((user: User) => !!user),
+      map((user: User) => {
+        if (user.isRegistered) {
+          return user.isRegistered;
+        } else {
+          return this.router.createUrlTree([user.role === Role.parent ? '/create-parent' : '/create-provider', ModeConstants.NEW]);
+        }
+      })
     );
   }
 }
