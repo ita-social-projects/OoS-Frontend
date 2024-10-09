2fb303e61e976d3570ffe84911390a7a03cf5884
Yaroslav Petryshyn <ya6liszl@gmail.com>
2024-10-09 12:28:22 +0300

    Implement image carousel

--- src/app/shared/components/image-carousel/image-carousel.component.ts
+++ src/app/shared/components/image-carousel/image-carousel.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ImgPath } from 'shared/models/carousel.model';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-image-carousel',
@@ -8,4 +9,15 @@ import { ImgPath } from 'shared/models/carousel.model';
 })
 export class ImageCarouselComponent {
   @Input() public images: ImgPath[] = [];
+
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      }
+    }
+  };
 }
