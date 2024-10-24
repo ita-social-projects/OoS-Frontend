1b9b486532c825265162d146be3be7f6987b34f9
Max Dvorak <dvorakmax2002@gmail.com>
2024-10-24 12:38:30 +0300

    Unnecessary code deleted

--- src/app/shared/services/feature-management/feature-management.service.ts
+++ src/app/shared/services/feature-management/feature-management.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { FeaturesList } from 'shared/models/features-list.model';
 
@@ -15,12 +15,5 @@ export class FeatureManagementService {
    */
   public getFeaturesList(): Observable<FeaturesList> {
     return this.http.get<FeaturesList>('/api/v1/FeatureManagement/Get');
-    // return of({
-    //   release1: true,
-    //   release2: false,
-    //   release3: false,
-    //   images: false,
-    //   showForProduction: false
-    // });
   }
 }
