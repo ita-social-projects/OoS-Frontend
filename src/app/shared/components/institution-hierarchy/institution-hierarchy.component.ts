d75a5231db8a3809f83492f80e3c0e03ac6679b4
Max Dvorak <dvorakmax2002@gmail.com>
2024-11-11 16:26:42 +0200

    Lint error fix

--- src/app/shared/components/institution-hierarchy/institution-hierarchy.component.ts
+++ src/app/shared/components/institution-hierarchy/institution-hierarchy.component.ts
@@ -26,6 +26,7 @@ export class InstitutionHierarchyComponent implements OnInit, OnDestroy {
   @Input() public instituitionHierarchyIdFormControl: AbstractControl;
   @Input() public provider: Provider;
   @Input() public instituitionIdFormControl: AbstractControl;
+  public hierarchyArray: HierarchyElement[] = [];
 
   @Select(MetaDataState.institutions)
   public institutions$: Observable<Institution[]>;
@@ -40,7 +41,6 @@ export class InstitutionHierarchyComponent implements OnInit, OnDestroy {
   private institutionFieldDesc: InstitutionFieldDescription[];
   private readonly destroy$: Subject<boolean> = new Subject<boolean>();
   private isEditMode: boolean;
-  public hierarchyArray: HierarchyElement[] = [];
 
   constructor(
     private readonly store: Store,
