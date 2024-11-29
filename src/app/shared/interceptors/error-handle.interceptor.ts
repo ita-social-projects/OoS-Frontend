47725b07ce0bacefe0a6bbafd1cb12a6e5769bfa
DimaShapoval <dsapoval422@gmail.com>
2024-11-29 23:45:55 +0200

    add handler of error that more than 500

--- src/app/shared/interceptors/error-handle.interceptor.ts
+++ src/app/shared/interceptors/error-handle.interceptor.ts
@@ -1,25 +1,36 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpContextToken, HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Store } from '@ngxs/store';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, finalize } from 'rxjs/operators';
 
 import { ApiErrorTypes } from 'shared/enum/api-error-types';
 import { SnackbarText } from 'shared/enum/enumUA/message-bar';
 import { ApiError } from 'shared/models/error-response.model';
 import { ShowMessageBar } from 'shared/store/app.actions';
 
+export const ERROR_HANDLED = new HttpContextToken<boolean>(() => false);
+
 @Injectable()
 export class ErrorHandleInterceptor implements HttpInterceptor {
   constructor(
     private store: Store,
-    private translateService: TranslateService
+    private translateService: TranslateService,
+    private router: Router
   ) {}
 
   public intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
+        if (error.status >= 501) {
+          this.router.navigate(['/server-error']);
+          request.clone({
+            context: request.context.set(ERROR_HANDLED, true)
+          });
+          return throwError(() => error);
+        }
         if (error.error?.apiErrorResponse) {
           const message = this.buildApiErrorMessage(error.error.apiErrorResponse.apiErrors);
           this.displayErrorMessageBar(message, 10000);
@@ -42,6 +53,11 @@ export class ErrorHandleInterceptor implements HttpInterceptor {
           }
         }
         return throwError(() => error);
+      }),
+      finalize(() => {
+        request.clone({
+          context: request.context.set(ERROR_HANDLED, false)
+        });
       })
     );
   }
