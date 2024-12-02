df80ec91e82b628c99df95862795bdf45db14906
DimaShapoval <dsapoval422@gmail.com>
2024-12-02 13:58:10 +0200

    edit imports in test

--- src/app/shared/services/server-error/server-error.service.spec.ts
+++ src/app/shared/services/server-error/server-error.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxsModule } from '@ngxs/store';
 import { ServerErrorService } from './server-error.service';
 
 describe('ServerErrorService', () => {
   let service: ServerErrorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, NgxsModule.forRoot([])]
+    });
     service = TestBed.inject(ServerErrorService);
   });
 
