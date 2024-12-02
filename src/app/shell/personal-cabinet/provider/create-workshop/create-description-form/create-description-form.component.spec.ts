3a844656b0e38549fb4efa722b2e7771f90de6fd
MaxZhylka <dexhonesta@gmail.com>
2024-12-02 21:20:20 +0200

    removed behavior subject, added function to change keyword input state, fixed remarks

--- src/app/shell/personal-cabinet/provider/create-workshop/create-description-form/create-description-form.component.spec.ts
+++ src/app/shell/personal-cabinet/provider/create-workshop/create-description-form/create-description-form.component.spec.ts
@@ -70,31 +70,32 @@ describe('CreateDescriptionFormComponent', () => {
 
     component.onKeyWordsInput();
 
-    expect(component.keyWords$.value).toEqual(['test']);
+    expect(component.keyWords).toEqual(['test']);
     expect(component.keyWordsCtrl.value).toBe('');
   });
 
   it('should disable input if keyword limit is reached', () => {
-    component.keyWords$.next(['one', 'two', 'three', 'four', 'five']);
+    component.keyWords = ['one', 'two', 'three', 'four'];
+    component.keyWordsCtrl.setValue('Test');
 
-    expect(component.disabledKeyWordsInput).toBeTruthy();
+    component.onKeyWordsInput();
+
     expect(component.keyWordsCtrl.disabled).toBeTruthy();
   });
 
   it('should enable input if keyword limit is less than 5', () => {
-    component.keyWords$.next(['one', 'two', 'three', 'four', 'five']);
+    component.keyWords = ['one', 'two', 'three', 'four', 'five'];
 
-    component.keyWords$.next(['one', 'two', 'three', 'four']);
+    component.onRemoveKeyWord('one');
 
-    expect(component.disabledKeyWordsInput).toBeFalsy();
     expect(component.keyWordsCtrl.disabled).toBeFalsy();
   });
   it('should remove keyword', () => {
-    component.keyWords$.next(['one', 'two', 'three', 'four', 'five']);
+    component.keyWords = ['one', 'two', 'three', 'four', 'five'];
 
     component.onRemoveKeyWord('five');
 
-    expect(component.keyWords$.value.length).toBe(4);
+    expect(component.keyWords.length).toBe(4);
   });
 });
 
--- src/app/shell/personal-cabinet/provider/create-workshop/create-description-form/create-description-form.component.ts
+++ src/app/shell/personal-cabinet/provider/create-workshop/create-description-form/create-description-form.component.ts
@@ -1,8 +1,8 @@
 import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { Subject, BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { CropperConfigurationConstants } from 'shared/constants/constants';
 import { MUST_CONTAIN_LETTERS } from 'shared/constants/regex-constants';
 import { ValidationConstants } from 'shared/constants/validation';
@@ -40,13 +40,12 @@ export class CreateDescriptionFormComponent implements OnInit, OnDestroy {
   public SectionItemsFormArray = new FormArray([]);
   public keyWordsCtrl: FormControl = new FormControl('', Validators.required);
 
-  public keyWords$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([]);
+  public keyWords: string[] = [];
   public keyWord: string;
 
   public disabilityOptionRadioBtn: FormControl = new FormControl(false);
-  public disabledKeyWordsInput = false;
 
-  public separatorKeysCodes = [13, 188]; // Enter and Comma
+  public separatorKeysCodes = [COMMA, ENTER];
   private destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(private formBuilder: FormBuilder) {
@@ -75,15 +74,6 @@ export class CreateDescriptionFormComponent implements OnInit, OnDestroy {
 
     this.passDescriptionFormGroup.emit(this.DescriptionFormGroup);
     this.keyWordsListener();
-
-    this.keyWords$.pipe(takeUntil(this.destroy$)).subscribe((keyWords) => {
-      this.disabledKeyWordsInput = keyWords.length >= 5;
-      if (this.disabledKeyWordsInput) {
-        this.keyWordsCtrl.disable({ emitEvent: false });
-      } else {
-        this.keyWordsCtrl.enable({ emitEvent: false });
-      }
-    });
   }
 
   /**
@@ -91,13 +81,11 @@ export class CreateDescriptionFormComponent implements OnInit, OnDestroy {
    * @param word
    */
   public onRemoveKeyWord(word: string): void {
-    const keyWords = this.keyWords$.value;
-    const index = keyWords.indexOf(word);
-    if (index >= 0) {
-      const updatedKeyWords = keyWords.filter((kw) => kw !== word);
-      this.keyWords$.next(updatedKeyWords);
-      if (updatedKeyWords.length) {
-        this.DescriptionFormGroup.get('keyWords').setValue(updatedKeyWords);
+    if (this.keyWords.includes(word)) {
+      this.keyWords = this.keyWords.filter((kw) => kw !== word);
+      this.updateKeywordsInputState();
+      if (this.keyWords.length) {
+        this.DescriptionFormGroup.get('keyWords').setValue(this.keyWords);
       } else {
         this.DescriptionFormGroup.get('keyWords').reset();
       }
@@ -107,11 +95,11 @@ export class CreateDescriptionFormComponent implements OnInit, OnDestroy {
   public onKeyWordsInput(isEditMode: boolean = true): void {
     this.DescriptionFormGroup.get('keyWords').markAsTouched();
     const inputKeyWord = this.keyWordsCtrl.value?.trim().toLowerCase();
-    if (inputKeyWord && !this.keyWords$.value.includes(inputKeyWord)) {
-      if (this.keyWords$.value.length < 5) {
-        const updatedKeyWords = [...this.keyWords$.value, inputKeyWord];
-        this.keyWords$.next(updatedKeyWords);
-        this.DescriptionFormGroup.get('keyWords').setValue(updatedKeyWords, { emitEvent: isEditMode });
+    if (inputKeyWord && !this.keyWords.includes(inputKeyWord)) {
+      if (this.keyWords.length < this.validationConstants.MAX_KEYWORDS_LENGTH) {
+        this.keyWords = [...this.keyWords, inputKeyWord];
+        this.updateKeywordsInputState();
+        this.DescriptionFormGroup.get('keyWords').setValue(this.keyWords, { emitEvent: isEditMode });
         this.keyWordsCtrl.setValue('');
       }
     }
@@ -239,6 +227,14 @@ export class CreateDescriptionFormComponent implements OnInit, OnDestroy {
   private markFormAsDirtyOnUserInteraction(): void {
     if (!this.DescriptionFormGroup.dirty) {
       this.DescriptionFormGroup.markAsDirty({ onlySelf: true });
+    }
+  }
+
+  private updateKeywordsInputState(): void {
+    if (this.keyWords.length >= this.validationConstants.MAX_KEYWORDS_LENGTH) {
+      this.keyWordsCtrl.disable({ emitEvent: false });
+    } else {
+      this.keyWordsCtrl.enable({ emitEvent: false });
     }
   }
 }
