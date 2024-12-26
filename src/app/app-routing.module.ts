695e2c95b0f15b09dea63c62290b1e60b4194cd5
witolDark <witalikspelina@gmail.com>
2024-12-26 20:29:35 +0200

    Changed behavior of navigation to custom, filters now are not scrollable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'data', loadChildren: () => import('./shell/admin-tools/data/data.module').then((m) => m.DataModule) }];
+const routes: Routes = [
+  {
+    path: 'data',
+    loadChildren: () => import('./shell/admin-tools/data/data.module').then((m) => m.DataModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true, scrollPositionRestoration: 'enabled' })],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      anchorScrolling: 'enabled'
+    })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
