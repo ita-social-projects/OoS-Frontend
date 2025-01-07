825c1a42e86de9d2ff84f8ecf66abadacdfcaa66
DmytroShapoval <dsapoval422@gmail.com>
2025-01-07 14:28:52 +0200

    add to app.state action for timer

--- src/app/shared/store/app.state.ts
+++ src/app/shared/store/app.state.ts
@@ -3,12 +3,14 @@ import { Action, Selector, State, StateContext } from '@ngxs/store';
 
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MessageBarComponent } from 'shared/components/message-bar/message-bar.component';
-import { ActivateEditMode, ClearMessageBar, MarkFormDirty, ShowMessageBar, ToggleMobileScreen } from './app.actions';
+import { TimerData } from 'shared/models/server-error';
+import { ActivateEditMode, ClearMessageBar, MarkFormDirty, SetErrorTimerData, ShowMessageBar, ToggleMobileScreen } from './app.actions';
 
 export interface AppStateModel {
   isDirtyForm: boolean;
   isEditMode: boolean;
   isMobileScreen: undefined | boolean;
+  timerErrorTime: TimerData;
 }
 
 @State<AppStateModel>({
@@ -16,7 +18,8 @@ export interface AppStateModel {
   defaults: {
     isDirtyForm: false,
     isEditMode: false,
-    isMobileScreen: undefined
+    isMobileScreen: undefined,
+    timerErrorTime: { timerValue: 1000, time: 0 }
   }
 })
 @Injectable()
@@ -36,6 +39,11 @@ export class AppState {
   @Selector()
   static isEditMode(state: AppStateModel): boolean {
     return state.isEditMode;
+  }
+
+  @Selector()
+  static timerErrorTime(state: AppStateModel): TimerData {
+    return state.timerErrorTime;
   }
 
   @Action(MarkFormDirty)
@@ -67,5 +75,18 @@ export class AppState {
   @Action(ToggleMobileScreen)
   ToggleMobileScreen({ patchState }: StateContext<AppStateModel>, { payload }: ActivateEditMode): void {
     patchState({ isMobileScreen: payload });
+  }
+
+  @Action(SetErrorTimerData)
+  setErrorTimerData({ patchState }: StateContext<AppStateModel>, { payload }: SetErrorTimerData): void {
+    const isTimeExpired = payload.time && Date.now() - payload.time >= 10 * 60 * 1000;
+    // check if 10 minutes have passed since the last click and change timer values
+    const newData: TimerData = isTimeExpired
+      ? { timerValue: 1000, time: 0 } // default values
+      : {
+          timerValue: payload.timerValue / 1000 >= 32 ? 60 * 1000 : payload.timerValue * 2,
+          time: payload.timerValue === 1000 ? Date.now() : payload.time
+        };
+    patchState({ timerErrorTime: newData });
   }
 }
