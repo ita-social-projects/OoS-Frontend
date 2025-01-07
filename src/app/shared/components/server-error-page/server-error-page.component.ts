1cdabc84358fd37e288210236d86c4cc09921db3
DmytroShapoval <dsapoval422@gmail.com>
2025-01-07 14:30:53 +0200

    control button disabling in component and create countdown for timer

--- src/app/shared/components/server-error-page/server-error-page.component.ts
+++ src/app/shared/components/server-error-page/server-error-page.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Select, Store } from '@ngxs/store';
+import { interval, Observable, take, takeWhile, tap } from 'rxjs';
+import { TimerData } from 'shared/models/server-error';
 import { ServerErrorService } from 'shared/services/server-error/server-error.service';
+import { SetErrorTimerData } from 'shared/store/app.actions';
+import { AppState } from 'shared/store/app.state';
 
 @Component({
   selector: 'app-server-error-page',
@@ -8,31 +13,54 @@ import { ServerErrorService } from 'shared/services/server-error/server-error.se
   styleUrls: ['./server-error-page.component.scss']
 })
 export class ServerErrorPageComponent implements OnInit {
+  @Select(AppState.timerErrorTime)
+  public timerTime$: Observable<TimerData>;
+
   public isDisabled!: boolean;
-  public disabledTime: number = 0;
+  public timerData!: TimerData; // static values from app state
+  public disabledTime: number; // time of button disabling
 
   constructor(
     private readonly errorService: ServerErrorService,
-    private readonly router: Router
+    private readonly router: Router,
+    private readonly store: Store
   ) {}
 
   public ngOnInit(): void {
-    this.errorService.isButtonDisabled$.subscribe((res: boolean) => {
-      this.isDisabled = res;
+    this.timerTime$.subscribe((res: TimerData) => {
+      this.timerData = res;
     });
   }
 
-  public async checkServerAvailable(): Promise<any> {
-    try {
-      const { status, timerValue } = this.errorService.serverIsAvailable();
-      this.disabledTime = timerValue / 1000;
-      if ((await status).status === 'Healthy') {
-        this.router.navigate(['/']);
-      }
-    } catch (err) {
-      if (err) {
-        throw err;
-      }
-    }
+  public checkServerAvailable(): void {
+    this.isDisabled = true;
+    this.disabledTime = this.timerData.timerValue / 1000;
+    this.errorService
+      .checkHealth()
+      .pipe(take(1))
+      .subscribe((result) => {
+        const { status } = result;
+        this.setTimerData();
+        if (status === 'Healthy') {
+          this.router.navigate(['/']);
+        }
+      });
+  }
+
+  /**
+   * This method set data for disabled button timer
+   */
+  public setTimerData(): void {
+    interval(1000)
+      .pipe(
+        takeWhile(() => this.disabledTime > 0),
+        tap(() => this.disabledTime--)
+      )
+      .subscribe({
+        complete: () => {
+          this.store.dispatch(new SetErrorTimerData(this.timerData));
+          this.isDisabled = false;
+        }
+      });
   }
 }
