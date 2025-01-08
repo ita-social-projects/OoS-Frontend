dfcfc6cbf88e75a4891de10b0a5c80d1a49ed054
witolDark <witalikspelina@gmail.com>
2025-01-08 19:23:32 +0200

    Added to top button

--- src/app/shell/result/workshop-cards-list/workshop-cards-list.component.ts
+++ src/app/shell/result/workshop-cards-list/workshop-cards-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
@@ -34,21 +34,8 @@ export class WorkshopCardsListComponent implements OnInit, OnDestroy {
   public parent: boolean;
   public workshops: SearchResponse<WorkshopCard[]>;
   public destroy$: Subject<boolean> = new Subject<boolean>();
-  private workshopList: ElementRef;
-  private observer!: IntersectionObserver;
-  @Output() private isListInView = new EventEmitter<boolean>();
 
   constructor(public store: Store) {}
-
-  @ViewChild('workshopCardsList')
-  private set content(content: ElementRef | null) {
-    if (!content) {
-      return;
-    }
-
-    this.workshopList = content;
-    this.listenForListInView();
-  }
 
   public ngOnInit(): void {
     this.workshops$
@@ -75,24 +62,11 @@ export class WorkshopCardsListComponent implements OnInit, OnDestroy {
   public ngOnDestroy(): void {
     this.destroy$.next(true);
     this.destroy$.unsubscribe();
-    this.observer?.unobserve(this.workshopList?.nativeElement);
   }
 
   private getWorkshops(): void {
     Util.setFromPaginationParam(this.paginationParameters, this.currentPage, this.workshops?.totalAmount);
     this.store.dispatch([new SetFilterPagination(this.paginationParameters), new GetFilteredWorkshops()]);
-  }
-
-  private listenForListInView(): void {
-    this.observer = new IntersectionObserver(this.onIntersection.bind(this), { threshold: [0] });
-
-    if (this.workshopList) {
-      this.observer.observe(this.workshopList.nativeElement);
-    }
-  }
-
-  private onIntersection(entries: any): void {
-    this.isListInView.emit(entries[0].isIntersecting);
   }
 
   private scrollToTop(): void {
