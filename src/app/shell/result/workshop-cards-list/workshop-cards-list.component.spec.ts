f87bf9796c1ed3036dd34508f797f89077c4ef9d
witolDark <witalikspelina@gmail.com>
2025-02-13 13:21:57 +0200

    tests

--- src/app/shell/result/workshop-cards-list/workshop-cards-list.component.spec.ts
+++ src/app/shell/result/workshop-cards-list/workshop-cards-list.component.spec.ts
@@ -46,7 +46,7 @@ describe('WorkshopCardsListComponentt', () => {
   });
 
   it('should scroll to top after paginator page change event', () => {
-    jest.spyOn(Util, 'scrollToTop').mockImplementation(() => {});
+    jest.spyOn(window, 'scrollTo').mockImplementation(() => {});
     component.paginationParameters = { from: 1, size: 8 };
     component.onPageChange({ element: 1, isActive: true });
     expect(window.scrollTo).toHaveBeenCalledWith({ top: 0, behavior: 'smooth' });
