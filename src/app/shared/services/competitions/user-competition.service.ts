811169f6a46fc4212b7f3b41ec28212b125fe685
DmytroShapoval <dsapoval422@gmail.com>
2025-02-16 15:10:08 +0200

    edit creating formData in competition service

--- src/app/shared/services/competitions/user-competition.service.ts
+++ src/app/shared/services/competitions/user-competition.service.ts
@@ -86,7 +86,7 @@ export class UserCompetitionService {
 
   private createFormData(competition: Competition): FormData {
     const formData = new FormData();
-    const formNames = ['address'];
+    const formNames = ['contacts'];
     const imageFiles = ['imageFiles', 'coverImage'];
     const judges = 'judges';
 
