50658ebaddc3933f54c37e7732b92524b1a398bf
DmytroShapoval <dsapoval422@gmail.com>
2025-02-19 16:32:44 +0200

    added test for competition-about tab

--- src/app/shell/details/details-tabs/competition-about/competition-about.component.spec.ts
+++ src/app/shell/details/details-tabs/competition-about/competition-about.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { CompetitionAboutComponent } from './competition-about.component';
 
 describe('CompetitionAboutComponent', () => {
@@ -6,7 +7,10 @@ describe('CompetitionAboutComponent', () => {
   let fixture: ComponentFixture<CompetitionAboutComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({}).compileComponents();
+    TestBed.configureTestingModule({
+      declarations: [CompetitionAboutComponent],
+      imports: [TranslateModule.forRoot()]
+    }).compileComponents();
     fixture = TestBed.createComponent(CompetitionAboutComponent);
     component = fixture.componentInstance;
   });
