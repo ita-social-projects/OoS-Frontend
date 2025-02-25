2ef1b0ccd900aea015f5da209e8cde9966d8dc48
MarianS23 <maryanshutov@gmail.com>
2025-02-25 16:25:54 +0200

    complex PR that contain #2756, #2753, #2748 issues

--- src/app/shared/base-components/upload-excel/upload-excel.component.ts
+++ src/app/shared/base-components/upload-excel/upload-excel.component.ts
@@ -97,7 +97,6 @@ export class UploadExcelComponent<DataSource extends { errors: ValidationError; 
     }
     const deletedTemporaryKeys = this.dataSource.map(({ errors, sequenceNumber, ...rest }) => rest);
     const changedItems = this.renamingKeys(deletedTemporaryKeys);
-    console.log(changedItems);
     this.employeeUploadProcessor
       .uploadEmployeesList(changedItems, currentId)
       .pipe(
--- src/app/shell/personal-cabinet/provider/provider-routing.module.ts
+++ src/app/shell/personal-cabinet/provider/provider-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { EmployeesComponent } from './employees/employees.component';
 import { ProviderApplicationsComponent } from './provider-applications/provider-applications.component';
 import { ProviderOrgInfoComponent } from './provider-org-info/provider-org-info.component';
 import { ProviderWorkshopsComponent } from './provider-workshops/provider-workshops.component';
@@ -11,7 +10,6 @@ import { ProviderEmployeesComponent } from './provider-employees/provider-employ
 
 const routes: Routes = [
   { path: 'info', component: ProviderOrgInfoComponent },
-  { path: 'administration', component: EmployeesComponent, canActivate: [NotEmployeeGuard] },
   { path: 'workshops', component: ProviderWorkshopsComponent },
   { path: 'applications', component: ProviderApplicationsComponent },
   { path: 'provider-employees', component: ProviderEmployeesComponent },
