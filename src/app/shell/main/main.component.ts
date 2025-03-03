f3644f272bdd98a332954e026fb10eeb33ee12a8
Dariia <132905554+darynkka@users.noreply.github.com>
2025-03-03 21:57:51 +0200

    litviniuk/top directions & workshops rendering hotfix (#2764)
    * top directions & workshops rendering hotfix
    
    * keyword input rendering hotfix
    
    * fix comment

--- src/app/shell/main/main.component.ts
+++ src/app/shell/main/main.component.ts
@@ -138,11 +138,13 @@ export class MainComponent implements OnInit, OnDestroy {
           take(1),
           filter((favorite: Favorite[]) => !!favorite?.length || favorite === null)
         )
-        .subscribe((favorite: Favorite[]) => this.getMainPageData());
+        .subscribe(() => this.getMainPageData());
+
+      return;
     } else if (role !== Role.unauthorized) {
       this.store.dispatch(new GetUnfinishedWorkshop());
-      this.getMainPageData();
     }
+    this.getMainPageData();
   }
 
   private getMainPageData(): void {
