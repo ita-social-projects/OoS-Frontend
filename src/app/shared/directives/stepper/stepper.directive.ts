fcb90b68ec07ce5c6f7d0b712ddfb039962c507c
witolDark <witalikspelina@gmail.com>
2025-03-10 18:42:57 +0200

    fixed bug with scroll to working hours

--- src/app/shared/directives/stepper/stepper.directive.ts
+++ src/app/shared/directives/stepper/stepper.directive.ts
@@ -4,7 +4,6 @@ import { MatStepper } from '@angular/material/stepper';
 import { Store } from '@ngxs/store';
 import { TranslateService } from '@ngx-translate/core';
 import { WINDOW } from 'ngx-window-token';
-import { asyncScheduler } from 'rxjs';
 
 import { ShowMessageBar } from 'shared/store/app.actions';
 
@@ -24,7 +23,7 @@ export class StepperDirective {
   ) {}
 
   @HostListener('click', ['$event'])
-  public onClick(event: Event): void {
+  public onClick(): void {
     if (!this.form || !this.stepper) {
       return;
     }
@@ -46,15 +45,19 @@ export class StepperDirective {
     this.form.markAllAsTouched();
     this.form.updateValueAndValidity();
 
-    const invalidFields = this.stepElement.querySelectorAll(
-      'input.ng-invalid, select.ng-invalid, textarea.ng-invalid, .days-toggle-invalid, mat-select.ng-invalid'
-    ); // add selector for a specific non-input type element
-    if (invalidFields.length) {
-      invalidFields[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
-      this.translateService.get('SERVICE_MESSAGES.SNACK_BAR_TEXT.REQUIRED_FIELDS_EMPTY').subscribe((text) => {
-        this.store.dispatch(new ShowMessageBar({ message: text, type: 'error' }));
-        asyncScheduler.schedule(() => (invalidFields[0] as HTMLElement).focus(), 1000);
-      });
-    }
+    requestAnimationFrame(() => {
+      const invalidFields = this.stepElement.querySelectorAll(
+        'input.ng-invalid, select.ng-invalid, textarea.ng-invalid, .days-toggle-invalid, mat-select.ng-invalid'
+      ); // add selector for a specific non-input type element
+
+      if (invalidFields.length) {
+        this.translateService.get('SERVICE_MESSAGES.SNACK_BAR_TEXT.REQUIRED_FIELDS_EMPTY').subscribe((text) => {
+          this.store.dispatch(new ShowMessageBar({ message: text, type: 'error' }));
+
+          invalidFields[0].scrollIntoView({ behavior: 'smooth', block: 'center' });
+          (invalidFields[0] as HTMLElement).focus({ preventScroll: true });
+        });
+      }
+    });
   }
 }
