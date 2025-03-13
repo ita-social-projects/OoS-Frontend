dd6ac998386bbcb44571cb82b955a51e8c9a4f0c
DmytroShapoval <dsapoval422@gmail.com>
2025-03-13 16:04:13 +0200

    added new features from #2778

--- src/app/shared/base-components/create-contacts/create-contacts.component.ts
+++ src/app/shared/base-components/create-contacts/create-contacts.component.ts
@@ -17,6 +17,7 @@ import { Codeficator } from 'shared/models/codeficator.model';
   styleUrls: ['./create-contacts.component.scss']
 })
 export class CreateContactsComponent implements OnInit, OnDestroy {
+  @Input() public address: Address;
   @Input() public contacts: Contacts[];
 
   @Output() public passContactsFormArray = new EventEmitter();
