11a2a423fc0393ae96a377f09c72db744151e21a
Dmytro M <dmytro.minochkin@gmail.com>
2025-04-03 12:51:00 +0300

    update storage URL for test env

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   stsServer: 'https://pozashkillia-test.iea.gov.ua/auth',
   serverUrl: 'https://pozashkillia-test.iea.gov.ua/web',
-  storageUrl: 'https://storage.googleapis.com/oos-images-63325/'
+  storageUrl: 'https://minio.pozashkillia-test.iea.gov.ua/outofschool/'
 };
