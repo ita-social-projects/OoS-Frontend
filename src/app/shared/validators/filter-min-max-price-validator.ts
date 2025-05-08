c211bef3ca3f307959a93a8bf3b182f7eade88ba
Oleg Prio <olegprio91@gmail.com>
2025-05-08 17:18:29 +0300

    bug fixes

--- src/app/shared/validators/filter-min-max-price-validator.ts
+++ src/app/shared/validators/filter-min-max-price-validator.ts
@@ -4,14 +4,15 @@ import { MinMaxPriceFilter } from 'shared/models/filter-list.model';
 export function MinMaxPriceValidator(minMax: string, limitMinMaxPrice: MinMaxPriceFilter, currentMinMaxPrice: number): ValidatorFn {
   return (control: AbstractControl): ValidationErrors | null => {
     let error: ValidationErrors | null = null;
+    const { minPrice, maxPrice } = limitMinMaxPrice;
     if (limitMinMaxPrice?.isActiveLimitation) {
       const value = control.value;
       if (minMax === 'min') {
-        if (value < limitMinMaxPrice.minPrice || value > limitMinMaxPrice.maxPrice || value > currentMinMaxPrice) {
+        if (value < minPrice || value > maxPrice || value > currentMinMaxPrice) {
           error = { minPriceFilterError: true };
         }
       } else if (minMax === 'max') {
-        if (value > limitMinMaxPrice.maxPrice || value < limitMinMaxPrice.minPrice || value < currentMinMaxPrice) {
+        if (value > maxPrice || value < minPrice || value < currentMinMaxPrice) {
           error = { maxPriceFilterError: true };
         }
       }
