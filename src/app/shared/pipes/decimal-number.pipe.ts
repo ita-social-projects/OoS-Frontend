1b414bd0605bc149bb26bb412f0398067f18e1e6
Vitalii Spelina <94604123+witolDark@users.noreply.github.com>
2025-06-18 23:37:15 +0300

    fixed pipe for number values, [positions] removed sorting for shortName, description (#2982)

--- src/app/shared/pipes/decimal-number.pipe.ts
+++ src/app/shared/pipes/decimal-number.pipe.ts
@@ -4,12 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'decimal'
 })
 export class DecimalNumberPipe implements PipeTransform {
-  public transform(value: string, digits: number = 2): string {
-    if (value || value.trim() !== '') {
-      const parsed = Number.parseFloat(value);
-      return !isNaN(parsed) ? parsed.toFixed(digits) : value;
+  public transform(value: string | number, digits: number = 2): string {
+    const stringifiedValue = value.toString();
+    if (stringifiedValue || stringifiedValue.trim() !== '') {
+      const parsed = Number.parseFloat(stringifiedValue);
+      return !isNaN(parsed) ? parsed.toFixed(digits) : stringifiedValue;
     }
 
-    return value;
+    return stringifiedValue;
   }
 }
