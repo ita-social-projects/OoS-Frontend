6fb8cbf3969e31f65ca522eba7fdb869c3d475f9
witolDark <witalikspelina@gmail.com>
2025-06-19 16:49:11 +0300

    ngxs logger back

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -83,7 +83,7 @@ registerLocaleData(localeUk);
       disabled: environment.production
     }),
     NgxsLoggerPluginModule.forRoot({
-      disabled: true
+      disabled: environment.production
     }),
     ShellModule,
     RegistrationModule,
