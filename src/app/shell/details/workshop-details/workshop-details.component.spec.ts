d64f31e2aaed90031a60b28a7ca83ebc04a3fcfc
witolDark <witalikspelina@gmail.com>
2025-07-09 12:59:03 +0300

    fixed tests

--- src/app/shell/details/workshop-details/workshop-details.component.spec.ts
+++ src/app/shell/details/workshop-details/workshop-details.component.spec.ts
@@ -27,6 +27,7 @@ import { WorkshopDetailsComponent } from './workshop-details.component';
 describe('WorkshopDetailsComponent', () => {
   let component: WorkshopDetailsComponent;
   let fixture: ComponentFixture<WorkshopDetailsComponent>;
+  let expectingMatDialogData: object;
   let matDialog: MatDialog;
   let matDialogSpy: jest.SpyInstance;
   const mockActivatedRoute = {
