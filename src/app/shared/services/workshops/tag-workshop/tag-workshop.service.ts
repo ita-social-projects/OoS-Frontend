68e2e8e8655a81a8ab0b6f1370e7486b940e02a5
witolDark <witalikspelina@gmail.com>
2025-07-10 18:10:11 +0300

    hidden tags by enableWorkshopTags feature flag fixed workshop/draft/competition contacts form editing

--- src/app/shared/services/workshops/tag-workshop/tag-workshop.service.ts
+++ src/app/shared/services/workshops/tag-workshop/tag-workshop.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
 import { Tag } from 'shared/models/tag.model';
 import { Util } from 'shared/utils/utils';
+import { Store } from '@ngxs/store';
+import { MetaDataState } from 'shared/store/meta-data.state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TagService {
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private store: Store
+  ) {}
 
   public getTags(): Observable<Tag[]> {
-    return this.http.get<Tag[]>('/api/v1/Tag/Get', {
-      params: {
-        localization: Util.getCurrentLocalization()
-      }
-    });
+    const isTagsFeatureEnabled = this.store.selectSnapshot(MetaDataState.featuresList).enableWorkshopTags;
+    return isTagsFeatureEnabled
+      ? this.http.get<Tag[]>('/api/v1/Tag/Get', {
+          params: {
+            localization: Util.getCurrentLocalization()
+          }
+        })
+      : EMPTY;
   }
 }
