453168f7ea963a8b0ef758e81a67ef903ed7f276
witolDark <witalikspelina@gmail.com>
2025-07-31 12:31:32 +0300

    lint

--- src/app/shell/personal-cabinet/provider/create-competition/create-required-form/create-required-form.component.spec.ts
+++ src/app/shell/personal-cabinet/provider/create-competition/create-required-form/create-required-form.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -16,13 +17,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgxsModule } from '@ngxs/store';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
-import { Provider } from 'shared/models/provider.model';
 
+import { Provider } from 'shared/models/provider.model';
 import { ImageFormControlComponent } from 'shared/components/image-form-control/image-form-control.component';
 import { MinMaxDirective } from 'shared/directives/min-max.directive';
 import { InfoMenuType } from 'shared/enum/info-menu-type';
 import { CreateRequiredFormComponent } from './create-required-form.component';
-import { ActivatedRoute } from '@angular/router';
 
 describe('CreateRequiredFormComponent', () => {
   let component: CreateRequiredFormComponent;
