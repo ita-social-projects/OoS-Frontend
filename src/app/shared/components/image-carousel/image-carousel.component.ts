c2bf0e57f6ffdd6abd39f0fd6ff2b49b86611988
witolDark <witalikspelina@gmail.com>
2025-08-29 11:35:41 +0300

    styles fix

--- src/app/shared/components/image-carousel/image-carousel.component.ts
+++ src/app/shared/components/image-carousel/image-carousel.component.ts
@@ -1,21 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Inject, Input, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
 import { ImgPath } from 'shared/models/carousel.model';
 import { DefaultCarouselOptions } from 'shared/configs/carousel.config';
 import { ImagesService } from 'shared/services/images/images.service';
+import { WINDOW } from 'ngx-window-token';
 
 @Component({
   selector: 'app-image-carousel',
   templateUrl: './image-carousel.component.html',
   styleUrls: ['./image-carousel.component.scss']
 })
-export class ImageCarouselComponent implements OnInit {
+export class ImageCarouselComponent implements OnInit, AfterViewInit {
   @Input() public images: ImgPath[];
 
   protected customOptions: OwlOptions = { ...DefaultCarouselOptions };
 
-  constructor(private imageService: ImagesService) {}
+  constructor(
+    private imageService: ImagesService,
+    @Inject(WINDOW) private window: Window
+  ) {}
 
   public ngOnInit(): void {
     if (this.images && this.images.length <= 1) {
@@ -32,5 +36,9 @@ export class ImageCarouselComponent implements OnInit {
   public onImageError(event: Event): void {
     const imgEl = event.target as HTMLImageElement;
     imgEl.src = this.imageService.getDefaultCoverImage();
+  }
+
+  public ngAfterViewInit(): void {
+    setTimeout(() => this.window.dispatchEvent(new Event('resize')));
   }
 }
