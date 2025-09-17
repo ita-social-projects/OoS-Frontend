3b8ae917b7a89515f24dbcd95205d47b0dd00d7c
witolDark <witalikspelina@gmail.com>
2025-09-17 13:49:34 +0300

    fixed editable pending moderation competition on card fixed naming and routing for editing competition drafts

--- src/app/shared/store/provider.state.ts
+++ src/app/shared/store/provider.state.ts
@@ -627,9 +627,16 @@ export class ProviderState {
 
   @Action(providerActions.OnUpdateDraftSuccess)
   onUpdateDraftSuccess({ dispatch }: StateContext<ProviderStateModel>, { payload }: providerActions.OnUpdateDraftSuccess): void {
-    const messageData = Util.getWorkshopMessage(payload, SnackbarText.updateWorkshop);
+    const messageData = Util.getWorkshopMessage(
+      payload,
+      payload instanceof Workshop ? SnackbarText.updateWorkshop : SnackbarText.updateCompetition
+    );
     dispatch([new MarkFormDirty(false), new ShowMessageBar({ message: messageData.message, type: messageData.type })]);
-    this.router.navigate(['/personal-cabinet/provider/drafts']);
+    this.router.navigate(['/personal-cabinet/provider/drafts'], {
+      queryParams: {
+        tab: payload instanceof Workshop ? 'workshops' : 'competitions'
+      }
+    });
   }
 
   @Action(providerActions.DeleteWorkshopDraftById)
