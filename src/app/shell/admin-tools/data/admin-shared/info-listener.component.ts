37a80897e41bcfb3835d327941465dc81c4971da
witolDark <witalikspelina@gmail.com>
2025-10-10 17:52:15 +0300

    abstract class for listening height

---
+++ src/app/shell/admin-tools/data/admin-shared/info-listener.component.ts
@@ -0,0 +1,27 @@
+import { Component, ElementRef, Input, OnDestroy, Renderer2, ViewChild } from '@angular/core';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
+
+@Component({
+  template: ''
+})
+export abstract class InfoListenerComponent implements OnDestroy {
+  @Input() public height$: BehaviorSubject<number> = new BehaviorSubject<number>(400);
+
+  public destroy$: Subject<void> = new Subject();
+
+  protected constructor(protected renderer: Renderer2) {}
+
+  @ViewChild('tabGroup', { read: ElementRef })
+  public set tabGroup(tabGroup: ElementRef) {
+    this.height$.pipe(takeUntil(this.destroy$)).subscribe((height: number) => {
+      const tab = tabGroup.nativeElement.querySelector('.mat-mdc-tab-body-wrapper') as HTMLElement;
+      this.renderer.setStyle(tab, 'max-height', height > 400 ? `${height}px` : '400px');
+      this.renderer.setStyle(tab, 'overflow', 'auto');
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
