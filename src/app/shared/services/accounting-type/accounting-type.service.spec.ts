9dfb817a023ff0a38fe90a046ed23aaf3c21fa79
witolDark <witalikspelina@gmail.com>
2025-11-19 22:22:43 +0200

    tests fix

--- src/app/shared/services/accounting-type/accounting-type.service.spec.ts
+++ src/app/shared/services/accounting-type/accounting-type.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AccountingTypeService } from './accounting-type.service';
 
 describe('AccountingTypeService', () => {
   let service: AccountingTypeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(AccountingTypeService);
   });
 
